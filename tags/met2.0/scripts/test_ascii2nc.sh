c7d17bd14dba42189d86ec28fc1e005bf379ce64
score <score>
2009-09-02 18:07:34 +0000

    John: Move the met2.0 tag over into the trunk/tags area.

--- tags/met2.0/scripts/test_ascii2nc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo
-echo "*** Running ASCII2NC to reformat ASCII point observations into NetCDF ***"
-../bin/ascii2nc \
-   ../data/sample_obs/ascii/sample_ascii_obs.txt \
-   ../out/ascii2nc/sample_ascii.nc \
-   -v 2
