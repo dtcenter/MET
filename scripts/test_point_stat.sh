27164236a95ba2ec7cc582cd29a99b59573165b1
score <score>
2010-05-24 22:43:05 +0000

    John: Rename -ncfile to -point_obs, -valid_beg to -obs_valid_beg, and -evalid_end to -obs_valid_end.

--- scripts/test_point_stat.sh
+++ scripts/test_point_stat.sh
@@ -6,5 +6,5 @@ echo "*** Running Point-Stat on sample NAM data ***"
    ../data/sample_fcst/2007033000/nam.t00z.awip1236.tm00.20070330.grb \
    ../out/pb2nc/sample_pb.nc \
    config/PointStatConfig \
-   -ncfile ../out/ascii2nc/sample_ascii.nc \
+   -point_obs ../out/ascii2nc/sample_ascii.nc \
    -outdir ../out/point_stat -v 2
