b0d7f9097d0c0628d6d417ff6ca1aad90d8653ef
score <score>
2010-08-26 20:28:51 +0000

    John: Enhance read_netcdf_grid routine to be backwards compatible and support METv2.0 grid defintions.

--- lib/vx_met_util/read_netcdf.cc
+++ lib/vx_met_util/read_netcdf.cc
@@ -23,16 +23,51 @@ using namespace std;
 
 
 static void get_att(NcFile *, NcAtt * & att, const char * name);
+static bool has_att(NcFile *, const char * name);
 
-static void get_latlon_data        (NcFile *, LatLonData &,        int verbosity);
-static void get_lambert_data       (NcFile *, LambertData &,       int verbosity);
-static void get_stereographic_data (NcFile *, StereographicData &, int verbosity);
-static void get_mercator_data      (NcFile *, MercatorData &,      int verbosity);
+static void read_netcdf_grid_v3       (NcFile *, Grid &,              int verbosity);
+static void read_netcdf_grid_v2       (NcFile *, Grid &,              int verbosity);
+
+static void get_latlon_data_v3        (NcFile *, LatLonData &,        int verbosity);
+static void get_lambert_data_v3       (NcFile *, LambertData &,       int verbosity);
+static void get_stereographic_data_v3 (NcFile *, StereographicData &, int verbosity);
+static void get_mercator_data_v3      (NcFile *, MercatorData &,      int verbosity);
+
+static void get_latlon_data_v2        (NcFile *, LatLonData &,        int verbosity);
+static void get_lambert_data_v2       (NcFile *, LambertData &,       int verbosity);
+static void get_stereographic_data_v2 (NcFile *, StereographicData &, int verbosity);
+static void get_mercator_data_v2      (NcFile *, MercatorData &,      int verbosity);
 
 
 ///////////////////////////////////////////////////////////////////////////////
 
-void read_netcdf_grid(NcFile * f_in, Grid & gr, int verbosity)
+
+void read_netcdf_grid(NcFile * f_in, Grid & gr, int verbosity) {
+   NcAtt * proj_att = (NcAtt *) 0;
+   bool v3_flag = false;
+
+   // Check for the MET version global attribute
+   if(has_att(f_in, "MET_version")) {
+
+      // Get the version number from the NetCDF file
+      get_att(f_in, proj_att, "MET_version");
+
+      // Check to see if it matches the current version
+      if(strcmp(proj_att->as_string(0), met_version) == 0) v3_flag = true;
+   }
+
+   // Parse the projection information based on the version
+   if(v3_flag) read_netcdf_grid_v3(f_in, gr, verbosity);
+   else        read_netcdf_grid_v2(f_in, gr, verbosity);
+
+   return;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+void read_netcdf_grid_v3(NcFile * f_in, Grid & gr, int verbosity)
 
 {
 
@@ -61,31 +96,104 @@ NcAtt * proj_att = (NcAtt *) 0;
 
    if ( strcmp(proj_att->as_string(0), latlon_proj_type) == 0 )  {
 
-      get_latlon_data(f_in, ll_data, verbosity);
+      get_latlon_data_v3(f_in, ll_data, verbosity);
 
       gr.set(ll_data);
 
    } else if ( strcmp(proj_att->as_string(0), mercator_proj_type) == 0 )  {
 
-      get_mercator_data(f_in, mc_data, verbosity);
+      get_mercator_data_v3(f_in, mc_data, verbosity);
 
       gr.set(mc_data);
 
    } else if ( strcmp(proj_att->as_string(0), lambert_proj_type) == 0 )  {
 
-      get_lambert_data(f_in, lc_data, verbosity);
+      get_lambert_data_v3(f_in, lc_data, verbosity);
 
       gr.set(lc_data);
 
    } else if ( strcmp(proj_att->as_string(0), stereographic_proj_type) == 0 )  {
 
-      get_stereographic_data(f_in, st_data, verbosity);
+      get_stereographic_data_v3(f_in, st_data, verbosity);
 
       gr.set(st_data);
 
    } else {   // Unsupported projection type
 
-      cerr << "\n\nERROR: read_netcdf_grid() -> "
+      cerr << "\n\nERROR: read_netcdf_grid_v3() -> "
+           << "Projection type " << proj_att->as_string(0)
+           << " not currently supported.\n\n"
+           << flush;
+
+      exit(1);
+
+   }
+
+   //
+   //  done
+   //
+
+return;
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+void read_netcdf_grid_v2(NcFile * f_in, Grid & gr, int verbosity)
+
+{
+
+NcAtt * proj_att = (NcAtt *) 0;
+
+   // Structures to store projection info
+   LambertData       lc_data;
+   StereographicData st_data;
+   LatLonData        ll_data;
+   MercatorData      mc_data;
+
+       //
+       // Parse the grid specification out of the global attributes
+       //
+
+   get_att(f_in, proj_att, "Projection");
+
+      //
+      // Parse out the grid specification depending on the projection type
+      // The following 4 Projection types are supported:
+      //    - Lat/Lon
+      //    - Mercator
+      //    - Lambert Conformal
+      //    - Polar Stereographic
+      //
+
+   if ( strcmp(proj_att->as_string(0), latlon_proj_type) == 0 )  {
+
+      get_latlon_data_v2(f_in, ll_data, verbosity);
+
+      gr.set(ll_data);
+
+   } else if ( strcmp(proj_att->as_string(0), mercator_proj_type) == 0 )  {
+
+      get_mercator_data_v2(f_in, mc_data, verbosity);
+
+      gr.set(mc_data);
+
+   } else if ( strcmp(proj_att->as_string(0), lambert_proj_type) == 0 )  {
+
+      get_lambert_data_v2(f_in, lc_data, verbosity);
+
+      gr.set(lc_data);
+
+   } else if ( strcmp(proj_att->as_string(0), stereographic_proj_type) == 0 )  {
+
+      get_stereographic_data_v2(f_in, st_data, verbosity);
+
+      gr.set(st_data);
+
+   } else {   // Unsupported projection type
+
+      cerr << "\n\nERROR: read_netcdf_grid_v2() -> "
            << "Projection type " << proj_att->as_string(0)
            << " not currently supported.\n\n"
            << flush;
@@ -336,7 +444,44 @@ return;
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void get_latlon_data(NcFile * ncfile, LatLonData & data, int verbosity)
+bool has_att(NcFile * ncfile, const char * name)
+
+{
+
+int i, n;
+bool status = false;
+NcAtt *att = (NcAtt *) 0;
+
+n = ncfile->num_atts();
+
+for ( i=0; i<n; i++ )  {
+
+   att = ncfile->get_att(i);
+
+   if ( !att )  {
+
+      cerr << "\n\nERROR: has_att() -> can't read attribute number " << i << ".\n\n" << flush;
+
+      exit ( 1 );
+
+   }
+
+   if ( strcmp(att->as_string(0), name) == 0 )  {
+
+      status = true;
+      break;
+   }
+}
+
+return(status);
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+void get_latlon_data_v3(NcFile * ncfile, LatLonData & data, int verbosity)
 
 {
 
@@ -402,7 +547,7 @@ return;
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void get_lambert_data(NcFile * ncfile, LambertData & data, int verbosity)
+void get_lambert_data_v3(NcFile * ncfile, LambertData & data, int verbosity)
 
 {
 
@@ -494,7 +639,7 @@ return;
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void get_stereographic_data(NcFile * ncfile, StereographicData & data, int verbosity)
+void get_stereographic_data_v3(NcFile * ncfile, StereographicData & data, int verbosity)
 
 {
 
@@ -590,7 +735,7 @@ return;
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void get_mercator_data(NcFile * ncfile, MercatorData & data, int verbosity)
+void get_mercator_data_v3(NcFile * ncfile, MercatorData & data, int verbosity)
 
 {
 
@@ -656,3 +801,314 @@ return;
 ///////////////////////////////////////////////////////////////////////////////
 
 
+void get_latlon_data_v2(NcFile * ncfile, LatLonData & data, int verbosity)
+
+{
+
+NcAtt * att = (NcAtt *) 0;
+
+if ( verbosity > 3 )  {
+
+   cout << "It's a Lat/Lon (PlateCarree or Equidistant Cylindrical) grid...\n" << flush;
+
+}
+
+   // Store the grid name
+data.name = latlon_proj_type;
+
+   // Latitude of the bottom left corner
+get_att(ncfile, att, "lat_ll_deg");
+data.lat_ll = atof(att->as_string(0));
+
+   // Longitude of the bottom left corner
+get_att(ncfile, att, "lon_ll_deg");
+data.lon_ll = atof(att->as_string(0));
+data.lon_ll *= -1.0;
+
+   // Latitude increment
+get_att(ncfile, att, "delta_lat_deg");
+data.delta_lat = atof(att->as_string(0));
+
+   // Longitude increment
+get_att(ncfile, att, "delta_lon_deg");
+data.delta_lon = atof(att->as_string(0));
+
+   // Number of points in the Latitude (y) direction
+get_att(ncfile, att, "Nlat");
+data.Nlat = atoi(att->as_string(0));
+
+   // Number of points in the Longitudinal (x) direction
+get_att(ncfile, att, "Nlon");
+data.Nlon = atoi(att->as_string(0));
+
+
+
+if ( verbosity > 3 )  {
+
+   cout << "Latitude/Longitude Grid Data:\n"
+        << "lat_ll = " << data.lat_ll << "\n"
+        << "lon_ll = " << data.lon_ll << "\n"
+        << "delta_lat = " << data.delta_lat << "\n"
+        << "delta_lon = " << data.delta_lon << "\n"
+        << "Nlat = " << data.Nlat << "\n"
+        << "Nlon = " << data.Nlon << "\n" << flush;
+
+}
+
+   //
+   //  done
+   //
+
+return;
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+void get_lambert_data_v2(NcFile * ncfile, LambertData & data, int verbosity)
+
+{
+
+NcAtt * att = (NcAtt *) 0;
+
+if ( verbosity > 3 )  {
+
+   cout << "It's a Lambert Conformal grid...\n" << flush;
+
+}
+
+   // Store the grid name
+data.name = lambert_proj_type;
+
+   // First scale latitude
+get_att(ncfile, att, "p1_deg");
+data.scale_lat_1 = atof(att->as_string(0));
+
+   // Second scale latitude
+get_att(ncfile, att, "p2_deg");
+data.scale_lat_2 = atof(att->as_string(0));
+
+   // Latitude pin
+get_att(ncfile, att, "p0_deg");
+data.lat_pin = atof(att->as_string(0));
+
+   // Longitude pin
+get_att(ncfile, att, "l0_deg");
+data.lon_pin = atof(att->as_string(0));
+data.lon_pin *= -1.0;
+
+   // X pin
+data.x_pin = 0.0;
+
+   // Y pin
+data.y_pin = 0.0;
+
+   // Orientation longitude
+get_att(ncfile, att, "lcen_deg");
+data.lon_orient = atof(att->as_string(0));
+data.lon_orient *= -1.0;
+
+   // Grid spacing in km
+get_att(ncfile, att, "d_km");
+data.d_km = atof(att->as_string(0));
+
+   // Radius of the earth
+get_att(ncfile, att, "r_km");
+data.r_km = atof(att->as_string(0));
+
+   // Number of points in the x-direction
+get_att(ncfile, att, "nx");
+data.nx = atoi(att->as_string(0));
+
+   // Number of points in the y-direction
+get_att(ncfile, att, "ny");
+data.ny = atoi(att->as_string(0));
+
+
+
+if(verbosity > 3) {
+
+   cout << "Lambert Conformal Grid Data:\n"
+        << "scale_lat_1 = " << data.scale_lat_1 << "\n"
+        << "scale_lat_2 = " << data.scale_lat_2 << "\n"
+        << "lat_pin = " << data.lat_pin << "\n"
+        << "lon_pin = " << data.lon_pin << "\n"
+        << "x_pin = " << data.x_pin << "\n"
+        << "y_pin = " << data.y_pin << "\n"
+        << "lon_orient = " << data.lon_orient << "\n"
+        << "d_km = " << data.d_km << "\n"
+        << "r_km = " << data.r_km << "\n"
+        << "nx = " << data.nx << "\n"
+        << "ny = " << data.ny << "\n" << flush;
+
+}
+
+   //
+   //  done
+   //
+
+return;
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+void get_stereographic_data_v2(NcFile * ncfile, StereographicData & data, int verbosity)
+
+{
+
+NcAtt * att = (NcAtt *) 0;
+const char * c = (const char *) 0;
+
+
+if ( verbosity > 3 )  {
+
+   cout << "It's a Polar Stereographic grid...\n" << flush;
+
+}
+
+   // Store the grid name
+data.name = stereographic_proj_type;
+
+   // Hemisphere, assume northern
+data.hemisphere = 'N';
+
+   // Scale latitude
+get_att(ncfile, att, "p1_deg");
+data.scale_lat = atof(att->as_string(0));
+
+   // Latitude pin
+get_att(ncfile, att, "p0_deg");
+data.lat_pin = atof(att->as_string(0));
+
+   // Longitude pin
+get_att(ncfile, att, "l0_deg");
+data.lon_pin = atof(att->as_string(0));
+data.lon_pin *= -1.0;
+
+   // X pin
+data.x_pin = 0.0;
+
+   // Y pin
+data.y_pin = 0.0;
+
+   // Orientation longitude
+get_att(ncfile, att, "lcen_deg");
+data.lon_orient = atof(att->as_string(0));
+data.lon_orient *= -1.0;
+
+   // Grid spacing in km
+get_att(ncfile, att, "d_km");
+data.d_km = atof(att->as_string(0));
+
+   // Radius of the earth
+get_att(ncfile, att, "r_km");
+data.r_km = atof(att->as_string(0));
+
+   // Number of points in the x-direction
+get_att(ncfile, att, "nx");
+data.nx = atoi(att->as_string(0));
+
+   // Number of points in the y-direction
+get_att(ncfile, att, "ny");
+data.ny = atoi(att->as_string(0));
+
+
+
+if ( verbosity > 3 )  {
+
+   cout << "Stereographic Grid Data:\n"
+        << "hemisphere = " << data.hemisphere << "\n"
+        << "scale_lat = " << data.scale_lat << "\n"
+        << "lat_pin = " << data.lat_pin << "\n"
+        << "lon_pin = " << data.lon_pin << "\n"
+        << "x_pin = " << data.x_pin << "\n"
+        << "y_pin = " << data.y_pin << "\n"
+        << "lon_orient = " << data.lon_orient << "\n"
+        << "d_km = " << data.d_km << "\n"
+        << "r_km = " << data.r_km << "\n"
+        << "nx = " << data.nx << "\n"
+        << "ny = " << data.ny << "\n" << flush;
+
+}
+
+   //
+   //  done
+   //
+
+return;
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+void get_mercator_data_v2(NcFile * ncfile, MercatorData & data, int verbosity)
+
+{
+
+NcAtt * att = (NcAtt *) 0;
+
+if ( verbosity > 3 )  {
+
+   cout << "It's a Mercator grid...\n" << flush;
+
+}
+
+   // Store the grid name
+data.name = mercator_proj_type;
+
+   // Latitude of the bottom left corner
+get_att(ncfile, att, "lat_ll_deg");
+data.lat_ll = atof(att->as_string(0));
+
+   // Longitude of the bottom left corner
+get_att(ncfile, att, "lon_ll_deg");
+data.lon_ll = atof(att->as_string(0));
+data.lon_ll *= -1.0;
+
+   // Latitude of the bottom left corner
+get_att(ncfile, att, "lat_ur_deg");
+data.lat_ur = atof(att->as_string(0));
+
+   // Longitude of the bottom left corner
+get_att(ncfile, att, "lon_ur_deg");
+data.lon_ur = atof(att->as_string(0));
+data.lon_ur *= -1.0;
+
+   // Number of points in the Latitudinal (y) direction
+get_att(ncfile, att, "Nlat");
+data.ny = atoi(att->as_string(0));
+
+   // Number of points in the Longitudinal (x) direction
+get_att(ncfile, att, "Nlon");
+data.nx = atoi(att->as_string(0));
+
+
+if ( verbosity > 3 )  {
+
+   cout << "Mercator Data:\n"
+        << "lat_ll = " << data.lat_ll << "\n"
+        << "lon_ll = " << data.lon_ll << "\n"
+        << "lat_ur = " << data.lat_ur << "\n"
+        << "lon_ur = " << data.lon_ur << "\n"
+        << "ny = " << data.ny << "\n"
+        << "nx = " << data.nx << "\n" << flush;
+
+}
+
+   //
+   //  done
+   //
+
+return;
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
