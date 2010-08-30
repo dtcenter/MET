bd809486334e18563f7845647c6ac21e63bdd1e4
score <score>
2010-08-30 19:23:52 +0000

    John: Edit vx_gdata library to get it to compile with MET.

--- met/lib/vx_gdata/vx_gdata.h
+++ met/lib/vx_gdata/vx_gdata.h
@@ -1,45 +1,25 @@
-
-
 // *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-// ** Copyright UCAR (c) 1992 - 2010
+// ** Copyright UCAR (c) 1992 - 2007
 // ** University Corporation for Atmospheric Research (UCAR)
 // ** National Center for Atmospheric Research (NCAR)
 // ** Research Applications Lab (RAL)
 // ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
 // *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
 
+////////////////////////////////////////////////////////////////////////
 
-
+#ifndef  __VX_GDATA_H__
+#define  __VX_GDATA_H__
 
 ////////////////////////////////////////////////////////////////////////
 
-
-#ifndef  __GET_PINTERP_GRID_H__
-#define  __GET_PINTERP_GRID_H__
-
-
-////////////////////////////////////////////////////////////////////////
-
-
-#include <netcdf.hh>
-
-#include "vx_data_grids/grid.h"
-
+#include "vx_gdata/is_pinterp_file.h"
+#include "vx_gdata/pinterp_file.h"
+#include "vx_gdata/var_info.h"
 
 ////////////////////////////////////////////////////////////////////////
 
-
-extern bool get_pinterp_grid(const char * pinterp_filename, Grid &);
-
-extern bool get_pinterp_grid(NcFile &, Grid &);
-
+#endif   //  __VX_GDATA_H__
 
 ////////////////////////////////////////////////////////////////////////
-
-
-#endif   /*  __GET_PINTERP_GRID_H__  */
-
-
-////////////////////////////////////////////////////////////////////////
-
 
