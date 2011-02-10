a6de8b937ccc9efc80490281cac08fd2d31dc590
Randy Bullock <bullock@ucar.edu>
2011-02-10 18:18:36 +0000

    Deleted the stuff from vx_met_util that I moved over to vx_math

--- src/libcode/vx_met_util/conversions.h
+++ src/libcode/vx_met_util/conversions.h
@@ -19,10 +19,6 @@
 ////////////////////////////////////////////////////////////////////////
 
 extern void   rescale_probability(WrfData &);
-extern double rescale_lon(double);
-extern double rescale_deg(double, double, double);
-extern double angle_between(double, double);
-extern double angle_difference(double, double, double, double);
 
 extern double convert_gpm_to_msl(double, double);
 extern double convert_p_t_z_to_prmsl(double, double, double, double);
@@ -31,10 +27,7 @@ extern double convert_p_w_to_vp(double, double);
 extern double convert_vp_to_dpt(double);
 extern double convert_t_to_svp(double);
 extern double convert_vp_svp_to_rh(double, double);
-extern double convert_u_v_to_wdir(double, double);
-extern double convert_u_v_to_wind(double, double);
-extern void   convert_u_v_to_unit(double, double, double &, double &);
-extern void   convert_u_v_to_unit(double &, double &);
+
 extern double gop_by_lat(double);
 
 ////////////////////////////////////////////////////////////////////////
