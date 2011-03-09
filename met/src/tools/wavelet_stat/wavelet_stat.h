d072cd25519380612a1be951e17a2546d5629cac
John Halley Gotway <johnhg@ucar.edu>
2011-03-09 20:32:50 +0000

    Changes to get wavelet_stat to compile.

--- met/src/tools/wavelet_stat/wavelet_stat.h
+++ met/src/tools/wavelet_stat/wavelet_stat.h
@@ -41,19 +41,21 @@ using namespace std;
 #include <unistd.h>
 
 #include "netcdf.hh"
-#include "vx_grib_classes/grib_classes.h"
 
 #include "wavelet_stat_conf_info.h"
 
-#include "vx_wrfdata/vx_wrfdata.h"
-#include "vx_met_util/vx_met_util.h"
-#include "vx_data_grids/grid.h"
-#include "vx_util/vx_util.h"
-#include "vx_cal/vx_cal.h"
-#include "vx_math/vx_math.h"
-#include "vx_contable/vx_contable.h"
-#include "vx_gsl_prob/vx_gsl_prob.h"
-#include "vx_econfig/result.h"
+#include "vx_wrfdata.h"
+#include "vx_met_util.h"
+#include "grid.h"
+#include "vx_gdata.h"
+#include "vx_grib_classes.h"
+#include "vx_util.h"
+#include "vx_cal.h"
+#include "vx_math.h"
+#include "vx_statistics.h"
+#include "vx_stat_out.h"
+#include "vx_gsl_prob.h"
+#include "result.h"
 
 ////////////////////////////////////////////////////////////////////////
 //
