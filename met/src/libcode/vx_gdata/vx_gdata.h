265af6611a49a543c70cb2b076bfc176a164a0d1
John Halley Gotway <johnhg@ucar.edu>
2011-03-09 16:39:50 +0000

    Changes to get gen_poly_mask to compile.

--- met/src/libcode/vx_gdata/vx_gdata.h
+++ met/src/libcode/vx_gdata/vx_gdata.h
@@ -14,13 +14,18 @@
 ////////////////////////////////////////////////////////////////////////
 
 #include "get_pinterp_grid.h"
+#include "grid_output.h"
 #include "is_met_nc_file.h"
 #include "is_netcdf_file.h"
 #include "is_pinterp_file.h"
 #include "met_file.h"
 #include "pinterp_file.h"
+#include "read_grib.h"
+#include "read_gridded_data.h"
+#include "read_netcdf.h"
 #include "var_info.h"
 #include "vx_gdata_util.h"
+#include "write_netcdf.h"
 
 ////////////////////////////////////////////////////////////////////////
 
