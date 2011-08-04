ba9e4f186bf038cfa244ed59ae787742eb870fff
John Halley Gotway <johnhg@ucar.edu>
2011-08-04 22:12:47 +0000

    Enhance gen_poly_mask to perform masking by lat/lon polyline as well as masking by grid.

--- met/src/tools/gen_poly_mask/gen_poly_mask.cc
+++ met/src/tools/gen_poly_mask/gen_poly_mask.cc
@@ -16,6 +16,9 @@
 //   ----   ----      ----            -----------
 //   000    10/23/08  Halley Gotway   New
 //   001    07/27/10  Halley Gotway  Add lat/lon variables to NetCDF.
+//   002    08/04/11  Halley Gotway  Enhance to allow the masking area
+//                    to be specified as a lat/lon polyline file or
+//                    gridded data file.  Also, add -name option.
 //
 ////////////////////////////////////////////////////////////////////////
 
@@ -57,8 +60,9 @@ static const char * program_name = "gen_poly_mask";
 
 // Input data file, polyline file, and output NetCDF file
 static ConcatString data_file;
-static ConcatString poly_file;
+static ConcatString mask_file;
 static ConcatString out_file;
+static ConcatString mask_name;
 static int i_rec = 1;
 
 // Logging level
@@ -68,13 +72,13 @@ static int verbosity = 2;
 static MaskPoly poly_mask;
 
 // Grid on which the data field resides
-static Grid grid;
+static Grid grid, grid_mask;
 
 ////////////////////////////////////////////////////////////////////////
 
 static void process_command_line(int, char **);
 static void process_data_file();
-static void process_poly_file();
+static void process_mask_file();
 static void write_netcdf();
 static void usage(int, char **);
 
@@ -91,8 +95,8 @@ int main(int argc, char *argv[]) {
    // Process the data file and extract the grid information
    process_data_file();
 
-   // Process the polyline file
-   process_poly_file();
+   // Process the mask file
+   process_mask_file();
 
    // Write out the mask file to NetCDF
    write_netcdf();
@@ -112,13 +116,18 @@ void process_command_line(int argc, char **argv) {
 
    // Store the arguments
    data_file = argv[1];
-   poly_file = argv[2];
+   mask_file = argv[2];
    out_file  = argv[3];
+   mask_name.clear();
 
    // Parse command line arguments
    for(i=0; i<argc; i++) {
 
-      if(strcmp(argv[i], "-rec") == 0) {
+      if(strcmp(argv[i], "-name") == 0) {
+         mask_name << argv[i+1];
+         i++;
+      }
+      else if(strcmp(argv[i], "-rec") == 0) {
          i_rec = atoi(argv[i+1]);
          i++;
       }
@@ -136,8 +145,8 @@ void process_command_line(int argc, char **argv) {
 
    // List the input files
    if(verbosity > 0) {
-      cout << "Input Data File:\t"    << data_file << "\n"
-           << "Input Poly File:\t"    << poly_file << "\n" << flush;
+      cout << "Input Data File:\t" << data_file << "\n"
+           << "Input Mask File:\t" << mask_file << "\n" << flush;
    }
 
    return;
@@ -199,26 +208,97 @@ void process_data_file() {
    }
 
    if(verbosity > 1) {
-      cout << "Parsed Grid:\t\t" << grid.name()
+      cout << "Parsed Input Grid:\t" << grid.name()
            << " (" << grid.nx() << " x " << grid.ny() << ")\n" << flush;
    }
+
+   // Cleanup
+   if(nc_file) { delete nc_file; nc_file = (NcFile *) 0; }
 
    return;
 }
 
 ////////////////////////////////////////////////////////////////////////
 
-void process_poly_file() {
+void process_mask_file() {
+   FileType ftype;
+   GribFile gb_file;
+   GribRecord rec;
+   NcFile *nc_file = (NcFile *) 0;
+   WrfData wd;
 
-   // Parse out the polyline from the file
-   poly_mask.clear();
-   poly_mask.load(poly_file);
+   // Determine the file type for the mask file
+   ftype = get_file_type(mask_file);
+
+   // Handle the supported file types
+   switch(ftype) {
+
+      // GRIB file type
+      case(GbFileType):
+
+         // Open the GRIB file
+         if(!(gb_file.open(mask_file))) {
+            cerr << "\n\nERROR: process_mask_file() -> "
+                 << "can't open GRIB file: "
+                 << mask_file << "\n\n" << flush;
+            exit(1);
+         }
+
+         // Process the data file in GRIB format
+         read_single_grib_record(gb_file, rec, i_rec-1, wd, grid_mask,
+                                 verbosity);
+
+         // If not already specified, retrieve the mask name
+         if(mask_name.length() == 0) mask_name = grid_mask.name();
+         break;
+
+      // NetCDF file type
+      case(NcFileType):
+
+         // Open the NetCDF File
+         nc_file = new NcFile(mask_file);
+         if(!nc_file->is_valid()) {
+            cerr << "\n\nERROR: process_mask_file() -> "
+                 << "can't open NetCDF file: "
+                 << mask_file << "\n\n" << flush;
+            exit(1);
+         }
+
+         // Process the data file in NetCDF format
+         read_netcdf_grid(nc_file, grid_mask, verbosity);
+
+         // If not already specified, retrieve the mask name
+         if(mask_name.length() == 0) mask_name = grid_mask.name();
+         break;
+
+      // Otherwise, assume it's an ASCII polyline file
+      default:
+
+         // Parse out the polyline from the file
+         poly_mask.clear();
+         poly_mask.load(mask_file);
+
+         // If not already specified, retrieve the mask name
+         if(mask_name.length() == 0) mask_name = poly_mask.name();
+         break;
+   }
 
    if(verbosity > 1) {
-      cout << "Parsed Polyline:\t" << poly_mask.name()
-           << " containing " << poly_mask.n_points() << " points\n"
-           << flush;
+
+      if(grid_mask.nx() > 0 && grid_mask.ny() > 0) {
+         cout << "Parsed Masking Grid:\t" << mask_name
+              << " (" << grid_mask.nx() << " x " << grid_mask.ny()
+              << ")\n" << flush;
+      }
+      else {
+         cout << "Parsed Polyline:\t" << mask_name
+              << " containing " << poly_mask.n_points() << " points\n"
+              << flush;
+      }
    }
+
+   // Cleanup
+   if(nc_file) { delete nc_file; nc_file = (NcFile *) 0; }
 
    return;
 }
@@ -227,7 +307,7 @@ void process_poly_file() {
 
 void write_netcdf() {
    int n, x, y, in_count, out_count;
-   double lat, lon;
+   double lat, lon, mask_x, mask_y;
    char var_str[max_str_len];
 
    int *mask_data = (int *)   0;
@@ -265,9 +345,8 @@ void write_netcdf() {
    write_netcdf_latlon(f_out, lat_dim, lon_dim, grid);
 
    // Define Variables
-   sprintf(var_str, "%s", poly_mask.name());
-   mask_var = f_out->add_var(var_str, ncInt, lat_dim, lon_dim);
-   sprintf(var_str, "%s polyline masking region", poly_mask.name());
+   mask_var = f_out->add_var(mask_name, ncInt, lat_dim, lon_dim);
+   sprintf(var_str, "%s polyline masking region", mask_name.text());
    mask_var->add_att("long_name", var_str);
 
    // Allocate memory to store the mask values for each grid point
@@ -280,19 +359,36 @@ void write_netcdf() {
 
          // Lat/Lon values for each grid point
          grid.xy_to_latlon(x, y, lat, lon);
-
          n = two_to_one(grid.nx(), grid.ny(), x, y);
 
-         // Check each of the Lat/Lon points to see if it is inside the
-         // Lat/Lon polyline.  Convert the longitude from degrees_east to
-         // degrees_west.
-         if(poly_mask.latlon_is_inside(lat, (-1.0*lon))) {
-            mask_data[n] = 1;
-            in_count++;
+         // Check if Lat/Lon is inside the masking polyline or grid
+
+         // Grid masking
+         if(grid_mask.nx() > 0 && grid_mask.ny() > 0) {
+
+            grid_mask.latlon_to_xy(lat, lon, mask_x, mask_y);
+            if(mask_x > 0 && mask_x <= grid_mask.nx() &&
+               mask_y > 0 && mask_y <= grid_mask.ny()) {
+               mask_data[n] = 1;
+               in_count++;
+            }
+            else {
+               mask_data[n] = 0;
+               out_count++;
+            }
          }
+         // Polyline masking
          else {
-            mask_data[n] = 0;
-            out_count++;
+
+            //Convert the longitude from degrees_east to degrees_west.
+            if(poly_mask.latlon_is_inside(lat, (-1.0*lon))) {
+               mask_data[n] = 1;
+               in_count++;
+            }
+            else {
+               mask_data[n] = 0;
+               out_count++;
+            }
          }
 
       } // end for y
@@ -333,8 +429,9 @@ void usage(int argc, char *argv[]) {
 
         << "Usage: " << program_name << "\n"
         << "\tdata_file\n"
-        << "\tmask_poly\n"
+        << "\tmask_file\n"
         << "\tnetcdf_file\n"
+        << "\t[-name str]\n"
         << "\t[-rec i]\n"
         << "\t[-v level]\n\n"
 
@@ -342,14 +439,18 @@ void usage(int argc, char *argv[]) {
         << "or NetCDF format from which the grid information is to be "
         << "retrieved (required).\n"
 
-        << "\t\t\"mask_poly\" is an ASCII Lat/Lon polyline file "
-        << "defining the masking region (required).\n"
+        << "\t\t\"mask_file\" is an ASCII Lat/Lon polyline file "
+        << "or gridded data file defining the masking region "
+        << "(required).\n"
 
         << "\t\t\"netcdf_file\" indicates the name of the output NetCDF "
         << "mask file to be written (required).\n"
 
+        << "\t\t\"-name str\" specifies the name to be used for the mask"
+        << " (optional).\n"
+
         << "\t\t\"-rec i\" specifies the GRIB record number from which "
-        << "the grid information is to be extracted. (optional).\n"
+        << "the grid information is to be extracted (optional).\n"
 
         << "\t\t\"-v level\" overrides the default level of logging ("
         << verbosity << ") (optional).\n\n"
