b4a5abec4a2ac568c4428fabf7ba115f6c868127
John Halley Gotway <johnhg@ucar.edu>
2012-01-13 22:23:17 +0000

    Delete contents of the current repository after it's been copied over into the METv3.0.1_final tag.

--- scripts/test_gen_poly_mask.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo
-echo "*** Running Gen-Poly-Mask to generate a polyline mask file for the Continental United States ***"
-../bin/gen_poly_mask \
-   ../data/sample_fcst/2005080700/wrfprs_ruc13_24.tm00_G212 \
-   ../data/poly/CONUS.poly \
-   ../out/gen_poly_mask/CONUS_poly.nc -v 2
--- scripts/test_pb2nc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo
-echo "*** Running PB2NC on a PREPBUFR file ***"
-../bin/pb2nc \
-   ../data/sample_obs/prepbufr/ndas.t00z.prepbufr.tm12.20070401.nr \
-   ../out/pb2nc/sample_pb.nc \
-   config/PB2NCConfig_G212 \
-   -v 2
