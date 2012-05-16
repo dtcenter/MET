689be473acbea5e699a5f8b44ace7a14dea1dd2e
John Halley Gotway <johnhg@ucar.edu>
2012-05-16 16:31:30 +0000

    Changed the version number from 3.1 to 4.0. In the process, needed to tweak the logic when parsing the MET NetCDF grid information. If the "MET_version" global attribute is present, parse the grid information as version 3. If not, parse it as the older version 2 information.

--- met/src/libcode/vx_data2d_nc_met/get_met_grid.cc
+++ met/src/libcode/vx_data2d_nc_met/get_met_grid.cc
@@ -51,18 +51,10 @@ static void get_mercator_data_v2      (NcFile *, MercatorData &);
 
 
 void read_netcdf_grid(NcFile * f_in, Grid & gr) {
-   NcAtt * proj_att = (NcAtt *) 0;
    bool v3_flag = false;
 
    // Check for the MET version global attribute
-   if(has_att(f_in, "MET_version")) {
-
-      // Get the version number from the NetCDF file
-      get_att(f_in, proj_att, "MET_version");
-
-      // Check to see if it matches the current version
-      if(match_met_version(proj_att->as_string(0))) v3_flag = true;
-   }
+   if(has_att(f_in, "MET_version")) v3_flag = true;
 
    // Parse the projection information based on the version
    if(v3_flag) read_netcdf_grid_v3(f_in, gr);
