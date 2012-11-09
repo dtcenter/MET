eb374dc2b5d60e16f103dffd7ca1b2f5f1f36faa
John Halley Gotway <johnhg@ucar.edu>
2012-11-09 16:42:19 +0000

    Add these files I forgot to add before.

---
+++ src/libcode/vx_data2d_factory/is_nccf_file.h
@@ -0,0 +1,35 @@
+
+
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+// ** Copyright UCAR (c) 1992 - 2012
+// ** University Corporation for Atmospheric Research (UCAR)
+// ** National Center for Atmospheric Research (NCAR)
+// ** Research Applications Lab (RAL)
+// ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#ifndef  __IS_NCCF_FILE_H__
+#define  __IS_NCCF_FILE_H__
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+extern bool is_nccf_file(const char * filename);
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#endif   /*  __IS_NCCF_FILE_H__  */
+
+
+////////////////////////////////////////////////////////////////////////
+
+
