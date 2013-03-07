ceff928a0a0b693aa63c668b90606fd90fa98068
John Halley Gotway <johnhg@ucar.edu>
2013-03-07 22:42:50 +0000

    Add basic unit test for the series_analysis tool.  Currently, it just calls the tool once requesting at least one statistic from the FHO, CTC, CTS, MCTC, MCTS, CNT, and SL1L2 lines types.  Could add a second call to request stats from the probabilistic line types.  Also, fixed a cut-and-paste error in the series_analysis tool where we were trying to get the CNT stats from the SL1L2 line.

--- test/bin/unit_test.sh
+++ test/bin/unit_test.sh
@@ -39,6 +39,7 @@ UNIT_XML="unit_ascii2nc.xml \
           unit_plot_point_obs.xml \
           unit_plot_data_plane.xml \
           unit_wwmca_plot.xml \
+          unit_series_analysis.xml \
           unit_tc_dland.xml \
           unit_tc_pairs.xml \
           unit_tc_stat.xml \
