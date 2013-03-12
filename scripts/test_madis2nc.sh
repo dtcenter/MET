ce33f4562580332aa50728a194107896d97ecde4
Julie Prestopnik <jpresto@ucar.edu>
2013-03-12 23:11:49 +0000

    Initial commit of new tarball test scripts
    Former-commit-id: 87ba799b5f5c1b48b310292807ad353006463601

---
+++ scripts/test_madis2nc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo
+echo "*** Running MADIS2NC to reformat MADIS point observations into NetCDF ***"
+../bin/madis2nc \
+  ../data/sample_obs/madis/metar/metar_2012040912_F000.nc \
+  ${TEST_OUT_DIR}/madis2nc/metar_2012040912_F000.nc \
+  -log ${TEST_OUT_DIR}/madis2nc/metar_2012040912_F000.log \
+  -type metar -rec_beg 2000 -rec_end 6000 -v 2
