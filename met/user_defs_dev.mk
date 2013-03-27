67221787e1f33e158a757e52edc830b7f9ebb2d3
Randy Bullock <bullock@ucar.edu>
2013-03-27 22:44:02 +0000

    Finished adding functionality to the modis regridding tool.
    Former-commit-id: cee5152c6e5d7122b5cc14407f3410fe52685e60

--- met/user_defs_dev.mk
+++ met/user_defs_dev.mk
@@ -80,8 +80,8 @@ GRIB2C_LIBS  =
 # Top level directory for the HDF and HDF-EOS Libraries if they're not
 # installed in a standard location.  These need not be specified if
 # you're not building the modis reformatting tool.
-HDF_BASE_DIR      =
-HDF_EOS_BASE_DIR  =
+HDF_BASE_DIR      = $(OTHERLIBS_DIR)/hdf/HDF4.2r3
+HDF_EOS_BASE_DIR  = $(OTHERLIBS_DIR)/hdf_eos/hdfeos
 
 # Optional flags to disable the compilation of MET tools
 # Specify a non-zero value to enable the compilation of the tool
