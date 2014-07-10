2272818f9f943e7c5d642c71c800e286cd2503e7
Randy Bullock <bullock@ucar.edu>
2014-07-10 21:17:05 +0000

    Forgot to add these to the repository.
    Former-commit-id: 5197665b10354fbf792183a0b464add296c3fdfc

---
+++ src/tools/other/mode_graphics/mode_nc_output_file.cc
@@ -0,0 +1,987 @@
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+using namespace std;
+
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cstdio>
+#include <cmath>
+
+#include "mode_nc_output_file.h"
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+   //
+   //  Code for class ModeNcOutputFile
+   //
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+ModeNcOutputFile::ModeNcOutputFile()
+
+{
+
+init_from_scratch();
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+ModeNcOutputFile::~ModeNcOutputFile()
+
+{
+
+close();
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+ModeNcOutputFile::ModeNcOutputFile(const ModeNcOutputFile &)
+
+{
+
+// init_from_scratch();
+// 
+// assign(m);
+
+mlog << Error << "\n\n  ModeNcOutputFile::ModeNcOutputFile(const ModeNcOutputFile &) -> should never be called!\n\n";
+
+exit ( 1 );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+ModeNcOutputFile & ModeNcOutputFile::operator=(const ModeNcOutputFile &)
+
+{
+
+// if ( this == &m )  return ( * this );
+// 
+// assign(m);
+
+mlog << Error << "\n\n  ModeNcOutputFile::operator=(const ModeNcOutputFile &) -> should never be called!\n\n";
+
+exit ( 1 );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void ModeNcOutputFile::init_from_scratch()
+
+{
+
+f = (NcFile *) 0;
+
+_Grid = (Grid *) 0;
+
+close();
+
+return;
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void ModeNcOutputFile::close()
+
+{
+
+if ( f )  { delete f;  f = (NcFile *) 0; }
+
+if ( _Grid )  { delete _Grid;  _Grid = (Grid *) 0; }
+
+FcstObjId  = (NcVar *) 0;
+FcstCompId = (NcVar *) 0;
+
+ObsObjId   = (NcVar *) 0;
+ObsCompId  = (NcVar *) 0;
+
+FcstRaw    = (NcVar *) 0;
+ObsRaw     = (NcVar *) 0;
+
+Nx = Ny = 0;
+
+ValidTime = InitTime = 0;
+
+AccumTime = 0;
+
+fcst_data_range_calculated = false;
+ obs_data_range_calculated = false;
+
+FcstDataMin = FcstDataMax = 0.0;
+ ObsDataMin =  ObsDataMax = 0.0;
+
+// NFcstObjs  = 0;
+// NFcstComps = 0;
+// 
+// NObsObjs   = 0;
+// NObsComps  = 0;
+
+Filename.clear();
+
+return;
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+bool ModeNcOutputFile::open(const char * _filename)
+
+{
+
+// int x, y;
+// int value;
+NcDim * dim = (NcDim *) 0;
+NcAtt * att = (NcAtt *) 0;
+
+
+close();
+
+Filename = _filename;
+
+f = new NcFile(_filename);
+
+if ( !(f->is_valid()) )  {
+
+   close();
+
+   return ( false );
+
+}
+
+   //
+   //  get dimensions
+   //
+
+
+dim = f->get_dim("lon");
+
+Nx = dim->size();
+
+
+dim = f->get_dim("lat");
+
+Ny = dim->size();
+
+
+dim = (NcDim *) 0;
+
+   //
+   //  variables
+   //
+
+FcstRaw    = f->get_var("fcst_raw" );
+ObsRaw     = f->get_var("obs_raw" );
+
+FcstObjId  = f->get_var("fcst_obj_id" );
+// FcstCompId = f->get_var("fcst_comp_id");
+FcstCompId = f->get_var("fcst_clus_id");
+
+ObsObjId   = f->get_var("obs_obj_id" );
+// ObsCompId  = f->get_var("obs_comp_id");
+ObsCompId  = f->get_var("obs_clus_id");
+
+   //
+   //  count objects
+   //
+/*
+for (x=0; x<Nx; ++x)  {
+
+   for (y=0; y<Ny; ++y)  {
+
+      value = fcst_obj_id(x, y);
+
+      if ( value > NFcstObjs )  NFcstObjs = value;
+
+      value = obs_obj_id(x, y);
+
+      if ( value > NObsObjs )  NObsObjs = value;
+
+      value = fcst_comp_id(x, y);
+
+      if ( value > NFcstComps )  NFcstComps = value;
+
+      value = obs_comp_id(x, y);
+
+      if ( value > NObsComps )  NObsComps = value;
+
+   }
+
+}
+*/
+
+   //
+   //  get grid
+   //
+
+_Grid = new Grid;
+
+read_netcdf_grid(f, *_Grid);
+
+   //
+   //  get init time, valid time, lead time from FcstRaw variable attributes
+   //
+
+att = FcstRaw->get_att("init_time_ut");
+
+InitTime = att->as_nclong(0);
+
+// cout << "\n\n  InitTime = " << InitTime << "\n\n" << flush;
+
+att = FcstRaw->get_att("valid_time_ut");
+
+ValidTime = att->as_nclong(0);
+
+// att = FcstRaw->get_att("accum_time_sec");
+// 
+// AccumTime = att->as_nclong(0);
+
+
+   //
+   //  done
+   //
+
+return ( true );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void ModeNcOutputFile::dump(ostream & out) const
+
+{
+
+out << "\n";
+
+if ( f )  out << "File is open\n";
+else      out << "file is closed\n";
+
+out << "\n";
+
+out << "Nx = " << Nx << "\n";
+out << "Ny = " << Ny << "\n";
+
+out << "\n";
+
+if ( f )  {
+
+   int month, day, year, hour, minute, second;
+   char junk[512];
+
+   out << "ValidTime = ";
+   unix_to_mdyhms(ValidTime, month, day, year, hour, minute, second);
+   sprintf(junk, "%s %d %d  %02d:%02d:%02d", short_month_name[month], day, year, hour, minute, second);
+   out << junk << '\n';
+
+   out << "InitTime  = ";
+   unix_to_mdyhms(InitTime, month, day, year, hour, minute, second);
+   sprintf(junk, "%s %d %d  %02d:%02d:%02d", short_month_name[month], day, year, hour, minute, second);
+   out << junk << '\n';
+
+   out << "AccumTime = ";
+   sprintf(junk, "%02d:%02d:%02d", AccumTime/3600, (AccumTime%3600)/60, AccumTime%60);
+   out << junk << '\n';
+
+}
+
+// out << "NFcstObjs  = " << NFcstObjs  << "\n";
+// out << "NObsObjs   = " << NObsObjs   << "\n";
+// 
+// out << "\n";
+// 
+// out << "NFcstComps = " << NFcstComps << "\n";
+// out << "NObsComps  = " << NObsComps  << "\n";
+
+out << "\n";
+
+
+out.flush();
+
+return;
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::n_fcst_simple_objs() const
+
+{
+
+if ( !f )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::n_fcst_simple_objs() const -> no file open!\n\n";
+
+   exit ( 1 );
+
+}
+
+int n;
+
+n = count_objects(FcstObjId);
+
+return ( n );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::n_obs_simple_objs() const
+
+{
+
+if ( !f )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::n_obs_simple_objs() const -> no file open!\n\n";
+
+   exit ( 1 );
+
+}
+
+int n;
+
+n = count_objects(ObsObjId);
+
+return ( n );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::n_fcst_comp_objs() const
+
+{
+
+if ( !f )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::n_fcst_comp_objs() const -> no file open!\n\n";
+
+   exit ( 1 );
+
+}
+
+int n;
+
+n = count_objects(FcstCompId);
+
+return ( n );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::n_obs_comp_objs() const
+
+{
+
+if ( !f )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::n_obs_comp_objs() const -> no file open!\n\n";
+
+   exit ( 1 );
+
+}
+
+int n;
+
+n = count_objects(ObsCompId);
+
+return ( n );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::count_objects(NcVar * var) const
+
+{
+
+int x, y, n, k;
+
+n = 0;
+
+for (x=0; x<Nx; ++x)  {
+
+   for (y=0; y<Ny; ++y)  {
+
+      k = get_int(var, x, y);
+
+      if ( k > n )  n = k;
+
+   }
+
+}
+
+return ( n );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::get_int(NcVar * var, int x, int y) const
+
+{
+
+if ( (x < 0) || (x >= Nx) || (y < 0) || (y >= Ny) )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::get_int() -> range check error ... "
+        << "(" << x << ", " << y << ")"
+        << "\n\n";
+
+   exit ( 1 );
+
+}
+
+int i[2];
+int status;
+
+status = var->set_cur(y, x);   //  NOT (x, y)!
+
+if ( !status )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::get_int() const -> can't set_cur for (" << x << ", " << y << ")!\n\n";
+
+   exit ( 1 );
+
+}
+
+status = var->get(i, 1, 1);
+
+if ( !status )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::get_int() const -> can't get value!\n\n";
+
+   exit ( 1 );
+
+}
+
+return ( i[0] );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+double ModeNcOutputFile::get_float(NcVar * var, int x, int y) const
+
+{
+
+if ( (x < 0) || (x >= Nx) || (y < 0) || (y >= Ny) )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::get_float() -> range check error ... "
+        << "(" << x << ", " << y << ")"
+        << "\n\n";
+
+   exit ( 1 );
+
+}
+
+float ff[2];
+int status;
+
+status = var->set_cur(y, x);   //  NOT (x, y)!
+
+if ( !status )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::get_float() const -> can't set_cur for (" << x << ", " << y << ")!\n\n";
+
+   exit ( 1 );
+
+}
+
+status = var->get(ff, 1, 1);
+
+if ( !status )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::get_float() const -> can't get value!\n\n";
+
+   exit ( 1 );
+
+}
+
+return ( (double) (ff[0]) );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+double ModeNcOutputFile::fcst_raw(int x, int y) const
+
+{
+
+double z;
+
+z = get_float(FcstRaw, x, y);
+
+return ( z );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+double ModeNcOutputFile::obs_raw(int x, int y) const
+
+{
+
+double z;
+
+z = get_float(ObsRaw, x, y);
+
+return ( z );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::fcst_obj_id(int x, int y) const
+
+{
+
+int k;
+
+k = get_int(FcstObjId, x, y);
+
+return ( k );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::fcst_comp_id(int x, int y) const
+
+{
+
+int k;
+
+k = get_int(FcstCompId, x, y);
+
+return ( k );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::obs_obj_id(int x, int y) const
+
+{
+
+int k;
+
+k = get_int(ObsObjId, x, y);
+
+return ( k );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int ModeNcOutputFile::obs_comp_id(int x, int y) const
+
+{
+
+int k;
+
+k = get_int(ObsCompId, x, y);
+
+return ( k );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+DataPlane ModeNcOutputFile::select_obj(ModeObjectField field, int n) const
+
+{
+
+if ( (n == 0) || (n < -1) )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::select_obj() const -> bad value\n\n";
+
+   exit ( 1 );
+
+}
+
+if ( !f )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::select_obj() const -> no data!\n\n";
+
+   exit ( 1 );
+
+}
+
+int x, y;
+int value;
+int count;
+DataPlane fdata;
+
+fdata.set_size(Nx, Ny);
+
+count = 0;
+
+for (x=0; x<Nx; ++x)  {
+
+   for (y=0; y<Ny; ++y)  {
+
+      fdata.set(0.0, x, y);
+
+
+      switch ( field )  {
+
+         case mof_fcst_obj:
+            value = fcst_obj_id(x, y);
+            break;
+
+         case mof_fcst_comp:
+            value = fcst_comp_id(x, y);
+            break;
+
+         case mof_obs_obj:
+            value = obs_obj_id(x, y);
+            break;
+
+         case mof_obs_comp:
+            value = obs_comp_id(x, y);
+            break;
+
+         default:
+            mlog << Error << "\n\n  ModeNcOutputFile::select_obj() const -> bad field\n\n";
+            exit ( 1 );
+            break;
+
+      }   //  switch
+
+
+      if ( value < 0 )  continue;
+
+      if ( n < 0 )  { fdata.set(1.0, x, y);  ++count;  continue; }
+
+      if ( n == value )  { fdata.set(1.0, x, y);  ++count; }
+
+   } 
+
+}
+
+// cout << "ModeNcOutputFile::select_obj() -> count = " << count << "\n" << flush;
+
+return ( fdata );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+DataPlane ModeNcOutputFile::select_fcst_obj  (int n) const
+
+{
+
+DataPlane fdata;
+
+fdata = select_obj(mof_fcst_obj, n);
+
+return ( fdata );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+DataPlane ModeNcOutputFile::select_fcst_comp (int n) const
+
+{
+
+DataPlane fdata;
+
+fdata = select_obj(mof_fcst_comp, n);
+
+return ( fdata );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+DataPlane ModeNcOutputFile::select_obs_obj   (int n) const
+
+{
+
+DataPlane fdata;
+
+fdata = select_obj(mof_obs_obj, n);
+
+return ( fdata );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+DataPlane ModeNcOutputFile::select_obs_comp  (int n) const
+
+{
+
+DataPlane fdata;
+
+fdata = select_obj(mof_obs_comp, n);
+
+return ( fdata );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+bool ModeNcOutputFile::x_line_valid (const int x) const
+
+{
+
+if ( (x < 0) || (x >= Nx) )  return ( false );
+
+int y;
+double v;
+const double bad = -9999.0;
+const double tol = 0.01;
+
+for (y=0; y<Ny; ++y)  {
+
+   v = obs_raw(x, y);
+
+   if ( fabs(v - bad) > tol )  return ( true );
+
+}
+
+return ( false );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+bool ModeNcOutputFile::y_line_valid (const int y) const
+
+{
+
+if ( (y < 0) || (y >= Ny) )  return ( false );
+
+int x;
+double v;
+const double bad = -9999.0;
+const double tol = 0.01;
+
+for (x=0; x<Nx; ++x)  {
+
+   v = obs_raw(x, y);
+
+   if ( fabs(v - bad) > tol )  return ( true );
+
+}
+
+return ( false );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void ModeNcOutputFile::calc_data_range(NcVar *, double & min_value, double & max_value)
+
+{
+
+int x, y;
+int count;
+double value;
+
+min_value =  1.0e30;
+max_value = -1.0e30;
+
+count = 0;
+
+for (x=0; x<Nx; ++x)  {
+
+   for (y=0; y<Ny; ++y)  {
+
+      value = (double) get_float(ObsRaw, x, y);
+
+      if ( value < -9000.0 )  continue;
+
+      ++count;
+
+      if ( value < min_value )  min_value = value;
+      if ( value > max_value )  max_value = value;
+
+   }
+
+}
+
+if ( count == 0 )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::calc_data_range() -> no valid data points!\n\n";
+
+   exit ( 1 );
+
+}
+
+return;
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void ModeNcOutputFile::get_fcst_raw_range(double & min_value, double & max_value)
+
+{
+
+if ( !f )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::get_fcst_raw_range() -> no data file open!\n\n";
+
+   exit ( 1 );
+
+}
+
+if ( fcst_data_range_calculated )  {
+
+   min_value = FcstDataMin;
+   max_value = FcstDataMax;
+
+   return;
+
+}
+
+calc_data_range(FcstRaw, min_value, max_value);
+
+FcstDataMin = min_value;
+FcstDataMax = max_value;
+
+fcst_data_range_calculated = true;
+
+return;
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void ModeNcOutputFile::get_obs_raw_range(double & min_value, double & max_value)
+
+{
+
+if ( !f )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::get_obs_raw_range() -> no data file open!\n\n";
+
+   exit ( 1 );
+
+}
+
+if ( obs_data_range_calculated )  {
+
+   min_value = ObsDataMin;
+   max_value = ObsDataMax;
+
+   return;
+
+}
+
+calc_data_range(ObsRaw, min_value, max_value);
+
+ObsDataMin = min_value;
+ObsDataMax = max_value;
+
+obs_data_range_calculated = true;
+
+return;
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+ConcatString ModeNcOutputFile::short_filename() const
+
+{
+
+if ( Filename.empty() )  {
+
+   mlog << Error << "\n\n  ModeNcOutputFile::short_filename() const -> no filename set!\n\n";
+
+   exit ( 1 );
+
+}
+
+ConcatString s;
+
+s = get_short_name(Filename);
+
+return ( s );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
---
+++ src/tools/other/mode_graphics/mode_nc_output_file.h
@@ -0,0 +1,175 @@
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#ifndef  __MODE_NC_OUTPUT_FILE_H__
+#define  __MODE_NC_OUTPUT_FILE_H__
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#include <netcdf.hh>
+
+#include "data_plane.h"
+#include "vx_grid.h"
+#include "get_met_grid.h"
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+enum ModeObjectField {
+
+   mof_fcst_obj, 
+   mof_fcst_comp, 
+
+   mof_obs_obj, 
+   mof_obs_comp, 
+
+};
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+class ModeNcOutputFile {
+
+   private:
+
+      void init_from_scratch();
+
+      ConcatString Filename;
+
+      int    get_int   (NcVar *, int x, int y) const;
+      double get_float (NcVar *, int x, int y) const;
+
+      int count_objects(NcVar *) const;
+
+      DataPlane select_obj(ModeObjectField, int) const;
+
+      void calc_data_range(NcVar *, double & min_value, double & max_value);
+
+      NcFile * f;   //  allocated
+
+      Grid * _Grid;   //  allocated
+
+      NcVar * FcstObjId;   //  NOT allocated
+      NcVar * FcstCompId;  //  NOT allocated
+
+      NcVar * ObsObjId;    //  NOT allocated
+      NcVar * ObsCompId;   //  NOT allocated
+
+      NcVar * FcstRaw;     //  NOT allocated
+      NcVar * ObsRaw;      //  NOT allocated
+
+      ModeNcOutputFile(const ModeNcOutputFile &);
+      ModeNcOutputFile & operator=(const ModeNcOutputFile &);
+
+      int Nx;
+      int Ny;
+
+      double FcstDataMin;
+      double FcstDataMax;
+
+      double ObsDataMin;
+      double ObsDataMax;
+
+      bool fcst_data_range_calculated;
+      bool  obs_data_range_calculated;
+
+      unixtime ValidTime;
+      unixtime InitTime;
+      int AccumTime;   //  seconds
+
+      // int NFcstObjs;
+      // int NFcstComps;
+
+      // int NObsObjs;
+      // int NObsComps;
+
+   public:
+
+      ModeNcOutputFile();
+     ~ModeNcOutputFile();
+
+      bool open(const char * path);
+
+      void close();
+
+      void dump(ostream &) const;
+
+      ConcatString filename() const;
+      ConcatString short_filename() const;
+
+      int nx() const;
+      int ny() const;
+
+      const Grid & grid() const;
+
+      unixtime valid_time () const;
+      unixtime init_time  () const;
+      int      accum_time () const;   //  seconds
+
+      int n_fcst_simple_objs () const;
+      int n_obs_simple_objs  () const;
+
+      int n_fcst_comp_objs () const;
+      int n_obs_comp_objs  () const;
+
+      double fcst_raw  (int x, int y) const;
+      double  obs_raw  (int x, int y) const;
+
+      int fcst_obj_id  (int x, int y) const;
+      int fcst_comp_id (int x, int y) const;
+
+      int obs_obj_id   (int x, int y) const;
+      int obs_comp_id  (int x, int y) const;
+
+      DataPlane select_fcst_obj  (int) const;   //  -1 for all
+      DataPlane select_fcst_comp (int) const;   //  -1 for all
+
+      DataPlane select_obs_obj   (int) const;   //  -1 for all
+      DataPlane select_obs_comp  (int) const;   //  -1 for all
+
+      bool x_line_valid(const int x) const;
+      bool y_line_valid(const int y) const;
+
+      void get_fcst_raw_range (double & min_value, double & max_value);
+      void get_obs_raw_range  (double & min_value, double & max_value);
+
+};
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+inline int ModeNcOutputFile::nx() const { return ( Nx ); }
+inline int ModeNcOutputFile::ny() const { return ( Ny ); }
+
+inline const Grid & ModeNcOutputFile::grid() const { return ( *_Grid ); }
+
+inline ConcatString ModeNcOutputFile::filename() const { return ( Filename ); }
+
+inline unixtime ModeNcOutputFile::valid_time() const { return ( ValidTime ); }
+inline unixtime ModeNcOutputFile::init_time () const { return (  InitTime ); }
+
+inline int ModeNcOutputFile::accum_time () const { return ( AccumTime ); }
+
+// inline int ModeNcOutputFile::n_fcst_objs () const { return ( NFcstObjs ); }
+// inline int ModeNcOutputFile::n_obs_objs  () const { return ( NObsObjs ); }
+// 
+// inline int ModeNcOutputFile::n_fcst_comps () const { return ( NFcstComps ); }
+// inline int ModeNcOutputFile::n_obs_comps  () const { return ( NObsComps ); }
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#endif   /*  __MODE_NC_OUTPUT_FILE_H__  */
+
+
+////////////////////////////////////////////////////////////////////////
+
+
