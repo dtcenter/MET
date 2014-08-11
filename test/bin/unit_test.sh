574ccc1bffa43b5978a2c9acfd61c2171858ec31
John Halley Gotway <johnhg@ucar.edu>
2014-08-11 20:15:09 +0000

    Add unit test for mode_graphics to the unit tests for the nightly build.
    Former-commit-id: 26bf5b43783331a763e9971c8575db279de53640

--- test/bin/unit_test.sh
+++ test/bin/unit_test.sh
@@ -47,7 +47,8 @@ UNIT_XML="unit_ascii2nc.xml \
           unit_tc_stat.xml \
           unit_met_test_scripts.xml  \
           unit_modis.xml  \
-          unit_ref_config.xml"
+          unit_ref_config.xml \
+          unit_mode_graphics.xml"
 
 # Run each unit test
 for CUR_XML in ${UNIT_XML}; do
