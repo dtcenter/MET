0711a494debf0c32e92cfce7e23c540312569a27
John Halley Gotway <johnhg@ucar.edu>
2014-09-05 17:43:59 +0000

    Update copyright date to 2014.
    Former-commit-id: 32293dbe6a5fb00cecdca9e172532c2ad2082a37

--- met/src/tools/core/mode/mode_exec.cc
+++ met/src/tools/core/mode/mode_exec.cc
@@ -3,7 +3,7 @@
 ///////////////////////////////////////////////////////////////////////
 
 
-// ** Copyright UCAR (c) 1992 - 2013
+// ** Copyright UCAR (c) 1992 - 2014
 // ** University Corporation for Atmospheric Research (UCAR)
 // ** National Center for Atmospheric Research (NCAR)
 // ** Research Applications Lab (RAL)
