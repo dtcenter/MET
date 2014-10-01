9ff7d9bd4c459bb6ec2110ade780b8f3b06cd76b
Nancy Rehak <rehak@ucar.edu>
2014-10-01 15:08:18 +0000

    Added vx_swinging_door library
    Former-commit-id: cac6473ad40e390a449f746c2cabfa8d5eeb34df

---
+++ met/src/libcode/vx_swinging_door/vx_swinging_door.h
@@ -0,0 +1,34 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+// ** Copyright UCAR (c) 1992 - 2013
+// ** University Corporation for Atmospheric Research (UCAR)
+// ** National Center for Atmospheric Research (NCAR)
+// ** Research Applications Lab (RAL)
+// ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#ifndef  __VX_SWINGING_DOOR_H__
+#define  __VX_SWINGING_DOOR_H__
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#include "compute_swinging_door.h"
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#endif   //  __VX_SWINGING_DOOR_H__
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
