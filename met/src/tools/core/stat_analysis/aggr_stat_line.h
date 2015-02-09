fdfd29d323c0db42b1309f497460e796dc1ac2c9
John Halley Gotway <johnhg@ucar.edu>
2015-02-09 20:11:08 +0000

    Define new stat_hdr_info_to_cols() utility function using a StatHdrInfo struct to populate the contents of a StatHdrColumns object.
    Former-commit-id: b3b6ae35254939f4954d60e90a967dedc6a97f49

--- met/src/tools/core/stat_analysis/aggr_stat_line.h
+++ met/src/tools/core/stat_analysis/aggr_stat_line.h
@@ -154,7 +154,8 @@ struct AggrSSVARInfo {
 
 extern void clear_stat_hdr_info(StatHdrInfo &hdr);
 extern void add_stat_hdr_info(StatHdrInfo &hdr, const STATLine &line);
-extern void stat_hdr_info_to_cols(const StatHdrInfo &hdr, StatHdrColumns &shc);
+extern void stat_hdr_info_to_cols(const ConcatString &cur_case,
+                                  const StatHdrInfo &hdr, StatHdrColumns &shc);
 
 extern void aggr_ctc_lines(
                LineDataFile &, STATAnalysisJob &,
