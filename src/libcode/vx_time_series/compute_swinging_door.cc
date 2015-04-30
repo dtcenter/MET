4c54dbc5828b5c79eca420b95b21c0b9fb973cc5
John Halley Gotway <johnhg@ucar.edu>
2015-04-30 20:42:34 +0000

    Messed up some paranthesis in compute_swinging_door.cc
    Former-commit-id: 6ef14b347106fe8206163314a0852a5c6a654f20

--- src/libcode/vx_time_series/compute_swinging_door.cc
+++ src/libcode/vx_time_series/compute_swinging_door.cc
@@ -73,10 +73,9 @@ bool compute_swinging_door_slopes(const TimeArray &valid_times,
       // Otherwise, the end point of the ramp is assigned the previous slope.
 
       if ( (ramp           == ramps.begin()                &&
-            valid_times[i] == ramp->first.getValidTime()) 
+            valid_times[i] == ramp->first.getValidTime())  || 
            (valid_times[i] >  ramp->first.getValidTime()   &&
-            valid_times[i] <= ramp->second.getValidTime()) ||
-           )
+            valid_times[i] <= ramp->second.getValidTime()) )
       {
          int    run_secs = ramp->second.getValidTime() - ramp->first.getValidTime();
          double rise     = ramp->second.getValue() - ramp->first.getValue();
