ff6983ae2de646b05e883688cbc4f22a091e3996
Randy Bullock <bullock@ucar.edu>
2015-05-27 17:22:02 +0000

    Started adding generalized threshold code. Wrote test_thresh program in the internal testing area.
    Former-commit-id: 2d7d5ab87aebabb7761e5e3619179d21600f8ef4

---
+++ met/internal_tests/basic/vx_config/test_thresh.cc
@@ -0,0 +1,124 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+// ** Copyright UCAR (c) 1992 - 2015
+// ** University Corporation for Atmospheric Research (UCAR)
+// ** National Center for Atmospheric Research (NCAR)
+// ** Research Applications Lab (RAL)
+// ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+static const bool debug = false;
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <cmath>
+
+#include "vx_util.h"
+
+#include "config_file.h"
+#include "thresh_node.h"
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+static ConcatString program_name;
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+extern ThreshNode * result;
+
+extern bool test_mode;
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+int main(int argc, char * argv [])
+
+{
+
+program_name = get_short_name(argv[0]);
+
+if ( argc == 1 )  {
+
+   cerr << "\n\n  usage: " << program_name << " thresh_string value\n\n";
+
+   exit ( 1 );
+
+}
+
+MetConfig config;
+
+   //
+   //  redirect stderr to stdout
+   //
+
+if ( dup2(1, 2) < 0 )  {
+
+   cerr << "\n\n  " << program_name << ": dup2 failed\n\n";
+
+   exit ( 1 );
+
+}
+
+config.set_debug(debug);
+
+bool status = false;
+ConcatString thresh_string = argv[1];
+const double value = atof(argv[2]);
+
+test_mode = true;
+
+status = config.read_string(thresh_string);
+
+if ( ! status )  {
+
+   cerr << "\n\n  " << program_name << ": failed to parse string \"" << thresh_string << "\"\n\n";
+
+   exit ( 1 );
+
+}
+
+test_mode = false;
+
+if ( ! result )  {
+
+   cerr << "\n\n  " << program_name << ": no result set!\n\n";
+
+   exit ( 1 );
+
+}
+
+status = result->check(value);
+
+cout << "\n\n   " << (status ? "true" : "false") << "\n\n";
+
+   //
+   //  done
+   //
+
+if ( result )  { delete result;  result = 0; }
+
+return ( 0 );
+
+}
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
