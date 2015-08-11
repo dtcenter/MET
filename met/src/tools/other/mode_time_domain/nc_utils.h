f325be17dd289cf34dafe72b0f37d91a9e334555
Randy Bullock <bullock@ucar.edu>
2015-08-11 16:36:55 +0000

    Added the time domain file classes.
    Former-commit-id: 31222cc174d0c580a2e0b1c7a73814e188e32f94

---
+++ met/src/tools/other/mode_time_domain/nc_utils.h
@@ -0,0 +1,44 @@
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#ifndef  __MTD_NETCDF_UTILS_H__
+#define  __MTD_NETCDF_UTILS_H__
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#include "netcdf.hh"
+
+#include "concat_string.h"
+#include "vx_cal.h"
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+extern const char *  string_att           (const NcFile &, const char * name);
+extern int           string_att_as_int    (const NcFile &, const char * name);
+extern long long     string_att_as_ll     (const NcFile &, const char * name);
+extern double        string_att_as_double (const NcFile &, const char * name);
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+extern unixtime parse_start_time(const char *);
+
+extern ConcatString start_time_string(const unixtime);
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+#endif   /*  __MTD_NETCDF_UTILS_H__  */
+
+
+////////////////////////////////////////////////////////////////////////
+
+
