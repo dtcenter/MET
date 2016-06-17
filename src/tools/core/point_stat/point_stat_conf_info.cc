f2b991ce34b6b55f89b273f70b4c03c8c2353e30
Tatiana Burek <tatiana@ucar.edu>
2016-06-17 16:00:17 +0000

    remove searching and adding a grib code to fcst_info variable field
    Former-commit-id: 21dfd5d0e9d60e329a4af3c8b0f5eb06cfc77984

--- src/tools/core/point_stat/point_stat_conf_info.cc
+++ src/tools/core/point_stat/point_stat_conf_info.cc
@@ -267,7 +267,6 @@ void PointStatConfInfo::process_config(GrdFileType ftype) {
 
       //fill in code data
       vx_pd[i].obs_info->add_grib_code (i_odict);
-      vx_pd[i].fcst_info->add_grib_code (i_fdict);
 
       // Dump the contents of the current VarInfo
       if(mlog.verbosity_level() >= 5) {
