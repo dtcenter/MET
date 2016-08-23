007c88ac240e4310a349503a8f291c3c5cf71ef8
Randy Bullock <bullock@ucar.edu>
2016-08-23 15:10:25 +0000

    Fixed config file bug (MET-555)
    Former-commit-id: dae4e487062a5bb3859c4b10365c7b57621de94d

--- met/scripts/test_mode.sh
+++ met/scripts/test_mode.sh
@@ -2,24 +2,24 @@
 
 echo
 echo "*** Running MODE on APCP using netCDF input for both forecast and observation ***"
-mode \
-   ${TEST_OUT_DIR}/pcp_combine/sample_fcst_12L_2005080712V_12A.nc \
-   ${TEST_OUT_DIR}/pcp_combine/sample_obs_2005080712V_12A.nc \
+../bin/mode \
+   ../out/pcp_combine/sample_fcst_12L_2005080712V_12A.nc \
+   ../out/pcp_combine/sample_obs_2005080712V_12A.nc \
    config/MODEConfig_APCP_12 \
-   -outdir ${TEST_OUT_DIR}/mode -v 2
+   -outdir ../out/mode -v 2
 
 echo
 echo "*** Running MODE on APCP using a GRIB forecast and netCDF observation ***"
-mode \
+../bin/mode \
    ../data/sample_fcst/2005080700/wrfprs_ruc13_24.tm00_G212 \
-   ${TEST_OUT_DIR}/pcp_combine/sample_obs_2005080800V_24A.nc \
+   ../out/pcp_combine/sample_obs_2005080800V_24A.nc \
    config/MODEConfig_APCP_24 \
-   -outdir ${TEST_OUT_DIR}/mode -v 2
+   -outdir ../out/mode -v 2
 
 echo
 echo "*** Running MODE on RH at 500mb using GRIB input for both forecast and observation ***"   
-mode \
+../bin/mode \
    ../data/sample_fcst/2005080700/wrfprs_ruc13_12.tm00_G212 \
    ../data/sample_fcst/2005080712/wrfprs_ruc13_00.tm00_G212 \
    config/MODEConfig_RH \
-   -outdir ${TEST_OUT_DIR}/mode -v 2
+   -outdir ../out/mode -v 2
