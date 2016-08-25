0d87e2d8438170900d2543a8d278849b53b4b8a7
Randy Bullock <bullock@ucar.edu>
2016-08-25 18:15:06 +0000

    Writing Makefile in scripts dir for testing tools
    Former-commit-id: 8f1859ad1f4136a1615a65d2fbe0219859269d8c

---
+++ scripts/mk/regrid_data_plane.mk
@@ -0,0 +1,20 @@
+
+
+
+########################################################################
+
+
+   ##
+   ##  regrid_data_plane
+   ##
+   ##     prerequisites:
+   ##
+
+
+regrid_data_plane: ${OTHER_DIR}/regrid_data_plane/regrid_data_plane
+	touch regrid_data_plane
+
+
+########################################################################
+
+
---
+++ scripts/mk/shift_data_plane.mk
@@ -0,0 +1,20 @@
+
+
+
+########################################################################
+
+
+   ##
+   ##  shift_data_plane
+   ##
+   ##     prerequisites:
+   ##
+
+
+shift_data_plane: ${OTHER_DIR}/shift_data_plane/shift_data_plane
+	touch shift_data_plane
+
+
+########################################################################
+
+
