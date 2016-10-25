7bf2bc98a0a3330b9b91728b9d5d7f2319419bc3
Howard Soh <hsoh@ucar.edu>
2016-10-25 22:13:14 +0000

    Moved some API to nc_utils. Improved madis2nc
    Former-commit-id: 3978f34831f007319c252c4bd6aaabce150ede46

--- met/src/libcode/vx_nc_util/nc_utils.cc
+++ met/src/libcode/vx_nc_util/nc_utils.cc
@@ -56,6 +56,57 @@ bool get_var_att(const NcVar *var, const ConcatString &att_name,
    return(status);
 }
 
+
+////////////////////////////////////////////////////////////////////////
+bool get_var_att(const NcVar *var, const ConcatString &att_name,
+                 int &att_val, bool exit_on_error) {
+   NcAtt *att = (NcAtt *) 0;
+   bool status = true;
+
+   // Initialize
+   att_val = bad_data_int;
+   
+   //
+   // Retrieve the NetCDF variable attribute.
+   //
+   if(!(att = var->get_att(att_name)) || !att->is_valid()) {
+      status = false;
+      mlog << Error << "\nget_var_att(float) -> "
+           << "can't read attribute \"" << att_name
+           << "\" from \"" << var->name() << "\" variable.\n\n";
+      if (exit_on_error) exit(1);
+   }
+   att_val = att->as_int(0);
+
+   return(status);
+}
+
+
+////////////////////////////////////////////////////////////////////////
+bool get_var_att(const NcVar *var, const ConcatString &att_name,
+                 float &att_val, bool exit_on_error) {
+   NcAtt *att = (NcAtt *) 0;
+   bool status = true;
+
+   // Initialize
+   att_val = bad_data_float;
+   
+   //
+   // Retrieve the NetCDF variable attribute.
+   //
+   if(!(att = var->get_att(att_name)) || !att->is_valid()) {
+      status = false;
+      mlog << Error << "\nget_var_att(float) -> "
+           << "can't read attribute \"" << att_name
+           << "\" from \"" << var->name() << "\" variable.\n\n";
+      if (exit_on_error) exit(1);
+   }
+   att_val = att->as_float(0);
+
+   return(status);
+}
+
+
 ////////////////////////////////////////////////////////////////////////
 
 bool get_var_att_double(const NcVar *var, const ConcatString &att_name,
@@ -125,32 +176,361 @@ double get_var_fill_value(const NcVar *var) {
 
 ////////////////////////////////////////////////////////////////////////
 
-int get_int_var(NcFile * nc, const char * var_name, int index) {
-   int k;
+char get_char_val(NcFile * nc, const char * var_name, const int index) {
    NcVar * var = (NcVar *) 0;
 
    var = nc->get_var(var_name);
+   return (get_char_val(var, index));
+}
 
-   var->set_cur(index);
+////////////////////////////////////////////////////////////////////////
 
-   var->get(&k, 1);
+char get_char_val(NcVar *var, const int index) {
+   char k;
+
+   //
+   // Retrieve the character array value from the NetCDF variable.
+   //
+   if(!var->set_cur(index) || !var->get(&k, 1)) {
+      mlog << Error << "\n\tget_char_val() -> "
+           << "can't read data from \"" << var->name()
+           << "\" variable.\n\n";
+      exit(1);
+   }
+
+   return (k);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+ConcatString* get_string_val(NcFile * nc, const char * var_name, const int index,
+                    const int len, ConcatString &tmp_cs) {
+   NcVar * var = (NcVar *) 0;
+
+   var = nc->get_var(var_name);
+   return (get_string_val(var, index, len, tmp_cs));
+}
+
+////////////////////////////////////////////////////////////////////////
+
+ConcatString* get_string_val(NcVar *var, const int index,
+                    const int len, ConcatString &tmp_cs) {
+   char tmp_str[len];
+
+   //
+   // Retrieve the character array value from the NetCDF variable.
+   //
+   if(!var->set_cur(index) || !var->get(&tmp_str[0], 1, len)) {
+      mlog << Error << "\n\tget_string_val(ConcatString) -> "
+           << "can't read data from \"" << var->name()
+           << "\" variable.\n\n";
+      exit(1);
+   }
+
+   //
+   // Store the character array as a ConcatString
+   //
+   tmp_cs = tmp_str;
+
+   return (&tmp_cs);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+int get_int_var(NcFile * nc, const char * var_name, const int index) {
+   NcVar * var = (NcVar *) 0;
+
+   var = nc->get_var(var_name);
+   return(get_int_var(var, index));
+}
+
+////////////////////////////////////////////////////////////////////////
+
+int get_int_var(NcVar * var, const int index) {
+   int k;
+
+   k = bad_data_int;
+   if (var) {
+      if(!var->set_cur(index) || !var->get(&k, 1)) {
+         mlog << Error << "\n\tget_int_var() -> "
+              << "can't read data from \"" << var->name()
+              << "\" variable.\n\n";
+      }
+   }
 
    return(k);
 }
 
 ////////////////////////////////////////////////////////////////////////
 
-double get_double_var(NcFile * nc, const char * var_name, int index) {
-   double k;
+double get_double_var(NcFile * nc, const char * var_name, const int index) {
    NcVar * var = (NcVar *) 0;
 
    var = nc->get_var(var_name);
+   return(get_double_var(var, index));
+}
 
-   var->set_cur(index);
+////////////////////////////////////////////////////////////////////////
 
-   var->get(&k, 1);
+double get_double_var(NcVar * var, const int index) {
+   double k;
+
+   k = bad_data_double;
+   if (var) {
+      if(!var->set_cur(index) || !var->get(&k, 1)) {
+         mlog << Error << "\n\tget_double_var() -> "
+              << "can't read data from \"" << var->name()
+              << "\" variable.\n\n";
+      }
+   }
 
    return(k);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+float get_float_var(NcFile * nc, const char * var_name, const int index) {
+   NcVar * var = (NcVar *) 0;
+
+   var = nc->get_var(var_name);
+   return(get_float_var(var, index));
+}
+
+////////////////////////////////////////////////////////////////////////
+
+float get_float_var(NcVar * var, const int index) {
+   float k;
+
+   k = bad_data_float;
+   if (var) {
+      if(!var->set_cur(index) || !var->get(&k, 1)) {
+         mlog << Error << "\n\tget_float_var() -> "
+              << "can't read data from \"" << var->name()
+              << "\" variable.\n\n";
+      }
+   }
+
+   return(k);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcFile *nc, const char *var_name,
+                 const long *cur, const long *dim, int *data) {
+   
+   //
+   // Retrieve the input variables
+   //
+   NcVar *var    = get_nc_var(nc, var_name);
+   return get_nc_data(var, cur, dim, data);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcVar *var, const long *cur, const long *dim, int *data) {
+   bool return_status = false;
+   //mlog << Debug(3) << "get_nc_data(int) is called\n";
+
+   if (var) {
+      for (int idx1=0; idx1<dim[0]; idx1++) {
+         if (cur[1] > 0) {
+           for (int idx2=0; idx2<dim[1]; idx2++) {
+              data[(idx1*dim[0])+idx2] = bad_data_float;
+           }
+         }
+         else{
+            data[idx1] = bad_data_int;
+         }
+      }
+      
+      //
+      // Retrieve the float value from the NetCDF variable.
+      // Note: missing data was checked here
+      //
+      if(!var->set_cur((long *) cur) || !var->get(data, (long *) dim)) {
+         mlog << Error << "\nget_nc_data(int *) -> "
+              << "can't read data from \"" << var->name()
+              << "\" variable.\n\n";
+         exit(1);
+      }
+      return_status = true;
+   }
+   return(return_status);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcFile *nc, const char *var_name,
+                 const long *cur, const long *dim, float *data) {
+   
+   //
+   // Retrieve the input variables
+   //
+   NcVar *var    = get_nc_var(nc, var_name);
+   return get_nc_data(var, cur, dim, data);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcVar *var, const long *cur, const long *dim, float *data) {
+   bool return_status = false;
+   //mlog << Debug(3) << "get_nc_data(float) is called\n";
+
+   if (var) {
+      for (int idx1=0; idx1<dim[0]; idx1++) {
+         if (cur[1] > 0) {
+           for (int idx2=0; idx2<dim[1]; idx2++) {
+              data[(idx1*dim[0])+idx2] = bad_data_float;
+           }
+         }
+         else{
+            data[idx1] = bad_data_double;
+         }
+      }
+      
+      //
+      // Retrieve the float value from the NetCDF variable.
+      // Note: missing data was checked here
+      //
+      if(!var->set_cur((long *) cur) || !var->get(data, (long *) dim)) {
+         mlog << Error << "\nget_nc_data(float *) -> "
+              << "can't read data from \"" << var->name()
+              << "\" variable.\n\n";
+         exit(1);
+      }
+      return_status = true;
+   }
+   return(return_status);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcFile *nc, const char *var_name,
+                 const long *cur, const long *dim, double *data) {
+   
+   //
+   // Retrieve the input variables
+   //
+   NcVar *var    = get_nc_var(nc, var_name);
+   return get_nc_data(var, cur, dim, data);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcVar *var, const long *cur, const long *dim, double *data) {
+   bool return_status = false;
+
+   if (var) {
+      for (int idx1=0; idx1<dim[0]; idx1++) {
+         if (cur[1] > 0) {
+           for (int idx2=0; idx2<dim[1]; idx2++) {
+              data[(idx1*dim[0])+idx2] = bad_data_double;
+           }
+         }
+         else{
+            data[idx1] = bad_data_double;
+         }
+      }
+      
+      //
+      // Retrieve the float value from the NetCDF variable.
+      // Note: missing data was checked here
+      //
+      if(!var->set_cur((long *) cur) || !var->get(data, (long *) dim)) {
+         mlog << Error << "\nget_nc_data(float *) -> "
+              << "can't read data from \"" << var->name()
+              << "\" variable.\n\n";
+         exit(1);
+      }
+      return_status = true;
+   }
+   return(return_status);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcFile *nc, const char *var_name,
+                 const long *cur, const long *dim, char *data) {
+   
+   //
+   // Retrieve the input variables
+   //
+   NcVar *var    = get_nc_var(nc, var_name);
+   return get_nc_data(var, cur, dim, data);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcVar *var, const long *cur, const long *dim, char *data) {
+   bool return_status = false;
+
+   if (var) {
+      for (int idx1=0; idx1<dim[0]; idx1++) {
+         if (cur[1] > 0) {
+           for (int idx2=0; idx2<dim[1]; idx2++) {
+              data[(idx1*dim[0])+idx2] = bad_data_char;
+           }
+         }
+         else{
+            data[idx1] = bad_data_char;
+         }
+      }
+      
+      //
+      // Retrieve the char value from the NetCDF variable.
+      //
+      if(!var->set_cur((long *) cur) || !var->get(data, (long *) dim)) {
+         mlog << Error << "\nget_nc_data(char **) -> "
+              << "can't read data from \"" << var->name()
+              << "\" variable.\n\n";
+         exit(1);
+      }
+      return_status = true;
+   }
+   return(return_status);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcFile *nc, const char *var_name,
+                 const long *cur, const long *dim, ncbyte *data) {
+   
+   //
+   // Retrieve the input variables
+   //
+   NcVar *var    = get_nc_var(nc, var_name);
+   return get_nc_data(var, cur, dim, data);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+bool get_nc_data(NcVar *var, const long *cur, const long *dim, ncbyte *data) {
+   bool return_status = false;
+
+   if (var) {
+      for (int idx1=0; idx1<dim[0]; idx1++) {
+         if (cur[1] > 0) {
+           for (int idx2=0; idx2<dim[1]; idx2++) {
+              data[(idx1*dim[0])+idx2] = bad_data_char;
+           }
+         }
+         else{
+            data[idx1] = bad_data_char;
+         }
+      }
+      
+      //
+      // Retrieve the char value from the NetCDF variable.
+      //
+      if(!var->set_cur((long *) cur) || !var->get(data, (long *) dim)) {
+         mlog << Error << "\nget_nc_data(char **) -> "
+              << "can't read data from \"" << var->name()
+              << "\" variable.\n\n";
+         exit(1);
+      }
+      return_status = true;
+   }
+   return(return_status);
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -167,6 +547,24 @@ bool args_ok(const LongArray & a) {
 
    return(true);
 }
+
+////////////////////////////////////////////////////////////////////////
+
+NcVar* get_nc_var(NcFile *nc, const char *var_name) {
+   NcVar *var = (NcVar *) 0;
+
+   //
+   // Retrieve the variable from the NetCDF file.
+   //
+   if(!(var = nc->get_var(var_name))) {
+      mlog << Error << "\nget_nc_var() -> "
+           << "can't read \"" << var_name << "\" variable.\n\n";
+      exit(1);
+   }
+
+   return(var);
+}
+
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -299,7 +697,9 @@ bool get_dim(const NcFile *nc, const ConcatString &dim_name,
 }
 
 NcFile* open_ncfile(const char * nc_name, NcFile::FileMode file_mode) {
-    return new NcFile(nc_name, file_mode);
+   //return new NcFile(nc_name, file_mode);
+   cout << " Opening " << nc_name << "\n";
+   return new NcFile(nc_name, file_mode, NULL, 0, NcFile::Netcdf4);
 }
 
 ////////////////////////////////////////////////////////////////////////
