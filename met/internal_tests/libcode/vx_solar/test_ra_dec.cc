6a659c701af59ec8d60fdde71a1e605e6821fe5e
John Halley Gotway <johnhg@ucar.edu>
2017-01-09 18:39:37 +0000

    Update the copyright notice to 2017.
    Former-commit-id: 43b4f8ba4632ee146aa4208c32278e82d0116dc2

--- met/internal_tests/libcode/vx_solar/test_ra_dec.cc
+++ met/internal_tests/libcode/vx_solar/test_ra_dec.cc
@@ -1,3 +1,10 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+// ** Copyright UCAR (c) 1992 - 2017
+// ** University Corporation for Atmospheric Research (UCAR)
+// ** National Center for Atmospheric Research (NCAR)
+// ** Research Applications Lab (RAL)
+// ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
 
 
 ////////////////////////////////////////////////////////////////////////
