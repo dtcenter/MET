10de984d9524aa09e199ab0d66219503001cceb5
John Halley Gotway <johnhg@ucar.edu>
2017-01-12 17:39:03 +0000

    Update the definition of the wwmca_south domain.  After fixing the bug in the southern polar stereographic projection, the wwmca_south grid definition was no longer correct.  Switch the orientation longitude from -100 to the value of 80 degrees which is also used for wwmca_north.
    Former-commit-id: c7a1c6dd87767906132f12db3b3e9fc53eb6bd9f

--- src/tools/other/wwmca_tool/wwmca_grids.h
+++ src/tools/other/wwmca_tool/wwmca_grids.h
@@ -31,7 +31,7 @@
                                                     //       name        Hemisphere   scale_lat    lat_pin   lon_pin    x_pin  y_pin  lon_orient      d_km       r_km    nx    ny
                                                     //   ========        ==========   =========    =======   ========   =====  =====  ==========   ========   =======  ====  ====
 static const StereographicData wwmca_north_data  = {     "wwmca_north",         'N',       60.0,      90.0,    0.0,     511.0, 511.0,       80.0,  23.79848,  6371.20, 1024, 1024 };
-static const StereographicData wwmca_south_data  = {     "wwmca_south",         'S',      -60.0,     -90.0,    0.0,     511.0, 511.0,     -100.0,  23.79848,  6371.20, 1024, 1024 };
+static const StereographicData wwmca_south_data  = {     "wwmca_south",         'S',      -60.0,     -90.0,    0.0,     511.0, 511.0,       80.0,  23.79848,  6371.20, 1024, 1024 };
 
 
 ////////////////////////////////////////////////////////////////////////
