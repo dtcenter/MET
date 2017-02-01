750c394be9a89e0d2091cce233e57db966342cad
Howard Soh <hsoh@ucar.edu>
2017-02-01 23:38:31 +0000

    Added unit_netcdf.xml
    Former-commit-id: 7b2a9a1c5b9de2343fbdcd10060d0ff79f4f1ecc

--- test/bin/unit_test.sh
+++ test/bin/unit_test.sh
@@ -56,7 +56,8 @@ UNIT_XML="unit_ascii2nc.xml \
           unit_mtd.xml \
           unit_climatology.xml \
           unit_test_grib_tables.xml \
-          unit_grid_weight.xml"
+          unit_grid_weight.xml" \
+          unit_netcdf.xml
 
 # Run each unit test
 for CUR_XML in ${UNIT_XML}; do
