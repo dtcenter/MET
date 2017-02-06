cdae5192cd0f3481d2cf2e6d60b08b15eb504898
Randy Bullock <bullock@ucar.edu>
2017-02-06 18:07:16 +0000

    redoing netcdf output of lidar2nc
    Former-commit-id: cf8fe7bf6aefa27afb1208d67203ab5dad4a997e

--- src/tools/other/lidar2nc/lidar2nc.cc
+++ src/tools/other/lidar2nc/lidar2nc.cc
@@ -21,6 +21,29 @@ static const char nc_lon_name         [] = "lon";
 static const char nc_time_name        [] = "time_utc";
 static const char nc_data_name        [] = "layer_base_altitude_km";
 
+static const char na_string           [] = "NA";
+
+static const char mxstr_dim_name      [] = "mxstr";
+static const int  mxstr_dim_size         = 40;
+
+static const char hdr_arr_len_dim_name [] = "hdf_arr_len";
+static const int  hdr_arr_len_dim_size    = 2;
+
+static const char obs_arr_len_dim_name [] = "obs_arr_len";
+static const int  obs_arr_len_dim_size    = 5;
+
+static const char nhdr_dim_name        [] = "nhdr";
+static const char nobs_dim_name        [] = "nobs";
+
+static const char hdr_type_string      [] = "calypso";
+
+static const char hdr_type_var_name    [] = "hdr_type";
+static const char hdr_sid_var_name     [] = "hdr_sid";
+static const char hdr_vld_var_name     [] = "hdr_vld";
+static const char hdr_arr_var_name     [] = "hdr_arr";
+static const char obs_qty_var_name     [] = "obs_qty";
+static const char obs_arr_var_name     [] = "obs_arr";
+
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -71,6 +94,24 @@ static CommandLine cline;
 
 ////////////////////////////////////////////////////////////////////////
 
+struct HdfVarInfo {
+
+   int hdf_index;
+
+   int hdf_id;
+
+   int hdf_rank;
+
+   int hdf_type;
+
+   int hdf_atts;
+
+   int hdf_dimsizes[MAX_VAR_DIMS];
+};
+
+
+////////////////////////////////////////////////////////////////////////
+
    //
    //   Command-line switches
    //
@@ -98,7 +139,12 @@ static void set_verbosity (const StringArray &);
 
 static int sizeof_hdf_type(const int type);
 
+
 static void process_calypso_file (NcFile &, const char * filename);
+
+static int get_lat_size(const int hdf_sd_id, const char * hdf_lat_name);
+
+static void get_hdf_var_info(const int hdf_sd_id, const char * hdf_name, HdfVarInfo &);
 
 static void nc_populate(NcFile &, int hdf_sd_id, 
                         const char * nc_name, const char * hdf_name);
@@ -143,6 +189,7 @@ if ( output_filename.empty() )  usage();
    //
 
 static NcFile ncf(output_filename.text(), NcFile::replace, NcFile::nc4);
+
 
 
    //
@@ -355,6 +402,8 @@ int hdf_lon_index, hdf_lon_id;
 int hdf_time_index, hdf_time_id;
 int hdf_data_index, hdf_data_id;
 int hdf_rank, hdf_atts, hdf_type;
+int n_data;
+int hdf_start[MAX_VAR_DIMS], hdf_stride[MAX_VAR_DIMS], hdf_edge[MAX_VAR_DIMS];
 NcDim nc_dim;
 NcVar nc_lat_var, nc_lon_var, nc_data_var, nc_time_var;
 
@@ -375,13 +424,192 @@ if ( hdf_sd_id < 0 )  {
 
 
    //
-   //  fill in the data
+   //  get number of data points 
+   //
+   //    we'll assume this is the number of latutide points
    //
 
-nc_populate(out, hdf_sd_id, nc_lat_name,   hdf_lat_name);
-nc_populate(out, hdf_sd_id, nc_lon_name,   hdf_lon_name);
-nc_populate(out, hdf_sd_id, nc_time_name,  hdf_time_name);
-nc_populate(out, hdf_sd_id, nc_data_name, hdf_data_name);
+n_data = get_lat_size(hdf_sd_id, hdf_lat_name);
+
+cout << "\n\n  n_data = " << n_data << "\n\n";
+
+const int nhdr_dim_size = n_data;
+const int nobs_dim_size = n_data;
+
+   //
+   //  see how big a buffer we'll need
+   //
+
+const int hdr_type_bytes = nhdr_dim_size*mxstr_dim_size;
+const int hdr_sid_bytes  = nhdr_dim_size*mxstr_dim_size;
+const int hdr_vld_bytes  = nhdr_dim_size*mxstr_dim_size;
+const int hdr_arr_bytes  = nhdr_dim_size*hdr_arr_len_dim_size*sizeof(float);
+const int obs_qty_bytes  = nobs_dim_size*mxstr_dim_size;
+const int obs_arr_bytes  = nobs_dim_size*obs_arr_len_dim_size*sizeof(float);
+
+int buf_size = hdr_type_bytes;
+
+buf_size = max<int>(buf_size, hdr_sid_bytes);
+buf_size = max<int>(buf_size, hdr_vld_bytes);
+buf_size = max<int>(buf_size, hdr_arr_bytes);
+buf_size = max<int>(buf_size, obs_qty_bytes);
+buf_size = max<int>(buf_size, obs_arr_bytes);
+
+
+
+   //
+   //  add some dimensions to the netcdf file
+   //
+
+NcDim mxstr_dim;
+NcDim hdr_arr_len_dim;
+NcDim obs_arr_len_dim;
+NcDim nhdr_dim;
+NcDim nobs_dim;
+
+
+out.addDim(mxstr_dim_name,       mxstr_dim_size);
+out.addDim(hdr_arr_len_dim_name, hdr_arr_len_dim_size);
+out.addDim(obs_arr_len_dim_name, obs_arr_len_dim_size);
+out.addDim(nhdr_dim_name,        n_data);
+out.addDim(nobs_dim_name,        n_data);
+
+mxstr_dim       = out.getDim(mxstr_dim_name);
+hdr_arr_len_dim = out.getDim(hdr_arr_len_dim_name);
+obs_arr_len_dim = out.getDim(obs_arr_len_dim_name);
+nhdr_dim        = out.getDim(nhdr_dim_name);
+nobs_dim        = out.getDim(nobs_dim_name);
+
+   //
+   //  add the variables to the netcdf file
+   //
+
+vector<NcDim> dims;
+NcVar hdr_type_var;
+NcVar hdr_sid_var;
+NcVar hdr_vld_var;
+NcVar hdr_arr_var;
+NcVar obs_qty_var;
+NcVar obs_arr_var;
+
+
+dims.resize(2);
+
+    /////////////////////////////////////
+
+dims.at(0) = nhdr_dim;
+dims.at(1) = mxstr_dim;
+
+out.addVar(hdr_type_var_name, NcType::nc_CHAR, dims);
+
+hdr_type_var = out.getVar(hdr_type_var_name);
+
+    /////////////////////////////////////
+
+out.addVar(hdr_sid_var_name, NcType::nc_CHAR, dims);
+
+hdr_sid_var = out.getVar(hdr_sid_var_name);
+
+    /////////////////////////////////////
+
+out.addVar(hdr_vld_var_name, NcType::nc_CHAR, dims);
+
+hdr_vld_var = out.getVar(hdr_vld_var_name);
+
+    /////////////////////////////////////
+
+dims.at(0) = nhdr_dim;
+dims.at(1) = obs_arr_len_dim;
+
+out.addVar(hdr_arr_var_name, NcType::nc_FLOAT, dims);
+
+hdr_arr_var = out.getVar(hdr_arr_var_name);
+
+    /////////////////////////////////////
+
+dims.at(0) = nobs_dim;
+dims.at(1) = mxstr_dim;
+
+out.addVar(obs_qty_var_name, NcType::nc_CHAR, dims);
+
+obs_qty_var = out.getVar(obs_qty_var_name);
+
+    /////////////////////////////////////
+
+dims.at(0) = nobs_dim;
+dims.at(1) = obs_arr_len_dim;
+
+out.addVar(obs_arr_var_name, NcType::nc_FLOAT, dims);
+
+obs_arr_var = out.getVar(obs_arr_var_name);
+
+   //
+   //  allocate the buffer
+   //
+
+unsigned char * buf = 0;
+
+buf = new unsigned char [buf_size];
+
+char  * cbuf = (char *) buf;
+float * fbuf = (float *) buf;
+
+
+   //
+   //  populate the hdr_typ variable
+   //
+
+// HdfVarInfo info;
+
+
+memset(cbuf, 0, n);
+
+for (j=0; j<n_data; ++j)  {
+
+   strcpy(cbuf + j*mxstr_dim_size, hdr_type_string);
+
+}
+
+hdr_type_var.putVar(cbuf);
+
+   //
+   //  populate the hdr_sid variable
+   //
+
+memset(cbuf, 0, n);
+
+for (j=0; j<n_data; ++j)  {
+
+   strcpy(cbuf + j*mxstr_dim_size, na_string);
+
+}
+
+hdr_sid_var.putVar(cbuf);
+
+   //
+   //  populate the obs_qty variable
+   //
+
+memset(cbuf, 0, n);
+
+for (j=0; j<n_data; ++j)  {
+
+   strcpy(cbuf + j*mxstr_dim_size, na_string);
+
+}
+
+obs_qty_var.putVar(cbuf);
+
+
+
+
+
+
+
+// nc_populate(out, hdf_sd_id, nc_lat_name,   hdf_lat_name);
+// nc_populate(out, hdf_sd_id, nc_lon_name,   hdf_lon_name);
+// nc_populate(out, hdf_sd_id, nc_time_name,  hdf_time_name);
+// nc_populate(out, hdf_sd_id, nc_data_name,  hdf_data_name);
 
    //
    //  close hdf file
@@ -402,6 +630,99 @@ if ( SDend(hdf_sd_id) < 0 )  {
 return;
 
 }
+
+////////////////////////////////////////////////////////////////////////
+
+
+int get_lat_size(const int hdf_sd_id, const char * hdf_lat_name)
+
+{
+
+int n_data;
+int hdf_index, hdf_id;
+int hdf_dimsizes[MAX_VAR_DIMS];
+int hdf_rank, hdf_type, hdf_atts;
+
+
+if ( (hdf_index = SDnametoindex(hdf_sd_id, hdf_lat_name)) < 0 )  {
+
+   cerr << "\n\n  " << program_name << ": failed to get index for \""
+        << hdf_lat_name << "\"\n\n";
+
+   exit ( 1 );
+
+}
+
+if ( (hdf_id = SDselect(hdf_sd_id, hdf_index)) < 0 )  {
+
+   cerr << "\n\n  " << program_name << ": failed to get id for \""
+        << hdf_lat_name << "\"\n\n";
+
+   exit ( 1 );
+
+}
+
+if ( SDgetinfo(hdf_id, 0, &hdf_rank, hdf_dimsizes, &hdf_type, &hdf_atts) < 0 )  {
+
+   cerr << "\n\n  " << program_name << ": SDgetinfo failed\n\n";
+
+   exit ( 1 );
+
+}
+
+
+n_data = max<int>(hdf_dimsizes[0], hdf_dimsizes[1]);
+
+   //
+   //  done
+   //
+
+return ( n_data );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void get_hdf_var_info(const int hdf_sd_id, const char * hdf_name, HdfVarInfo & info)
+
+{
+
+if ( (info.hdf_index = SDnametoindex(hdf_sd_id, hdf_name)) < 0 )  {
+
+   cerr << "\n\n  " << program_name << ": failed to get index for \""
+        << hdf_name << "\"\n\n";
+
+   exit ( 1 );
+
+}
+
+if ( (info.hdf_id = SDselect(hdf_sd_id, info.hdf_index)) < 0 )  {
+
+   cerr << "\n\n  " << program_name << ": failed to get id for \""
+        << hdf_name << "\"\n\n";
+
+   exit ( 1 );
+
+}
+
+if ( SDgetinfo(info.hdf_id, 0, &(info.hdf_rank), info.hdf_dimsizes, &(info.hdf_type), &(info.hdf_atts)) < 0 )  {
+
+   cerr << "\n\n  " << program_name << ": SDgetinfo failed\n\n";
+
+   exit ( 1 );
+
+}
+
+   //
+   //  done
+   //
+
+return;
+
+}
+
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -447,7 +768,7 @@ if ( (hdf_id = SDselect(hdf_sd_id, hdf_index)) < 0 )  {
 }
 
    //
-   //  get hdf info on this data
+   //  get hdf info on this variable
    //
 
 if ( SDgetinfo(hdf_id, 0, &hdf_rank, hdf_dimsizes, &hdf_type, &hdf_atts) < 0 )  {
