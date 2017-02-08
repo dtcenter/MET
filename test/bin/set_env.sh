d23bba3ed910142138c945b6be576665deeb9f66
Julie Prestopnik <jpresto@ucar.edu>
2017-02-08 17:44:09 +0000

    Remove test directory which was inadvertently added to the met-5.2_bugfix directory
    Former-commit-id: a9e5f063a82a0b7272c3b5d4a773c468bcfaf6d5

--- test/bin/set_env.sh
+++
@@ -1,5 +0,0 @@
-export NETCDF=${NETCDF:-/usr/local/netcdf}
-export LDFLAGS=-Wl,-rpath,/usr/local/hdf5-1.8.5patch1/lib:/usr/local/netcdf/lib
-export LD_LIBRARY_PATH=${NETCDF}/lib:/usr/local/hdf5-1.8.5patch1/lib
-export NETCDF_INCLUDE=${NETCDF}/include
-export NETCDF_LIB=${NETCDF}/lib
