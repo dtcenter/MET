c294b3daa53f48a4599e0d77c76c36a442074449
George McCabe <mccabe@ucar.edu>
2017-03-04 00:00:33 +0000

    Removed check of equal obs value when checking for unique obs
    Former-commit-id: 6ffbb00caa4195f2a766781f9c62c7fbe8dd3bb1

--- src/libcode/vx_statistics/pair_base.cc
+++ src/libcode/vx_statistics/pair_base.cc
@@ -258,7 +258,7 @@ bool PairBase::add_obs(const char *sid,
       // if key exists, add new ob to vector, else add new pair
       map<string,station_values_t>::iterator it = map_val.find(sng_key);
    if(it != map_val.end()) {
-     if(check_unique && (*it).second.ut == ut && (*it).second.obs[0].val == o)
+     if(check_unique && (*it).second.ut == ut)
      {
          return false;
      }
@@ -447,8 +447,6 @@ void PairBase::print_duplicate_report(){
 
 
 void PairBase::calc_obs_summary(){
-  if(obs_select == OBS_SUMMARY_NONE) return;
-
   //  iterate over the keys in the unique station id map
   for( map<string,station_values_t>::iterator it_single = map_val.begin();
        it_single != map_val.end(); ++it_single ){
