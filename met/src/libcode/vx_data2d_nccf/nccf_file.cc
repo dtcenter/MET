8af32e4804df6063d9d53a55bb76a43eeda07802
Howard Soh <hsoh@ucar.edu>
2017-03-06 16:00:29 +0000

    MET-571: temporarily turn off latitude swapping
    Former-commit-id: 931708cca21c4e09cd68324d9a5f71679f0e7d73

--- met/src/libcode/vx_data2d_nccf/nccf_file.cc
+++ met/src/libcode/vx_data2d_nccf/nccf_file.cc
@@ -2309,10 +2309,10 @@ bool NcCfFile::get_grid_from_dimensions()
   data.Nlat = _yDim->getSize();
   data.Nlon = _xDim->getSize();
   data.swap_to_north = false;
-  if (dlat < 0) {
-    data.delta_lat = -dlat;
-    data.lat_ll = lat_values[lat_counts-1];
-    data.swap_to_north = true;
+  //if (dlat < 0) {
+  //  data.delta_lat = -dlat;
+  //  data.lat_ll = lat_values[lat_counts-1];
+  //  data.swap_to_north = true;
   }
   grid.set(data);
 
