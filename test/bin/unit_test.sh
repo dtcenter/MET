6b1a15e1cd146dd8ce4081a49ede43258a9ada52
John Halley Gotway <johnhg@ucar.edu>
2017-03-17 16:46:45 +0000

    Tweaks to the unit test for lidar2nc to write it's output to the $MET_TEST_OUTPUT directory.  Also, add it to the list of unit tests for the nightly build.
    Former-commit-id: cb0ddb0bf7719e67dab5093bbf72bf24df68669e

--- test/bin/unit_test.sh
+++ test/bin/unit_test.sh
@@ -60,7 +60,8 @@ UNIT_XML="unit_ascii2nc.xml \
           unit_grid_weight.xml \
           unit_netcdf.xml \
           unit_hira.xml \
-          unit_interp_shape.xml"
+          unit_interp_shape.xml \
+          unit_lidar2nc.xml"
 
 # Run each unit test
 for CUR_XML in ${UNIT_XML}; do
