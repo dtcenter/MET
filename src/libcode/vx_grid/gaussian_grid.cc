ae9d268cc8c909985839213cabc9fd6b619c112c
Randy Bullock <bullock@ucar.edu>
2017-07-05 18:45:20 +0000

    Finished some details on Gaussian grids.
    Former-commit-id: bc05863f63d1ea08fccfb1581098bad99c0d363f

--- src/libcode/vx_grid/gaussian_grid.cc
+++ src/libcode/vx_grid/gaussian_grid.cc
@@ -33,6 +33,18 @@ using namespace std;
 ////////////////////////////////////////////////////////////////////////
 
 
+static const double int_check_tol = 1.0e-5;
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+inline bool is_integer(double x)   { return ( fabs(x - nint(x)) < int_check_tol ); }
+
+
+////////////////////////////////////////////////////////////////////////
+
+
    //
    //  Code for class GaussianGrid
    //
@@ -44,6 +56,8 @@ using namespace std;
 GaussianGrid::GaussianGrid()
 
 {
+
+North_Latitudes = 0;
 
 clear();
 
@@ -75,7 +89,13 @@ Nx = Ny = 0;
 
 Lon_Zero = 0.0;
 
+Delta_Lon = 0.0;
+
 memset(&Data, 0, sizeof(Data));
+
+if ( North_Latitudes )  { delete [] North_Latitudes;  North_Latitudes = 0; }
+
+N_north_lats = 0;
 
 return;
 
@@ -89,17 +109,67 @@ GaussianGrid::GaussianGrid(const GaussianData & data)
 
 {
 
+North_Latitudes = 0;
+
 clear();
+
+Lon_Zero = data.Lon_Zero;
+
+Delta_Lon = 360.0/(Nx - 1.0);
+
+Name = data.name;
+
+Data = data;
 
 Nx = data.Nx;
 
 Ny = data.Ny;
 
-Lon_Zero = data.Lon_Zero;
+   //
+   //  check that Ny is even
+   //
 
-Name = data.name;
+if ( Ny%2 )  {
 
-Data = data;
+   cerr << "\n\n  GaussianGrid::GaussianGrid(const GaussianData &) -> Ny must be even!\n\n";
+
+   exit ( 1 );
+
+}
+
+   //
+   //  get the latitudes
+   //
+
+
+Legendre L;
+int j, k;
+double r, w;
+
+
+N_north_lats = Ny/2;
+
+North_Latitudes = new double [N_north_lats];
+
+L.set_max_degree(N_north_lats);
+
+
+for (j=0; j<N_north_lats; ++j)  {
+
+   k = j + Ny/2;
+
+   k = Ny - 1 - k;
+
+   L.lether_root_weight(k, r, w);
+   // L.d_and_r_root_weight(k, r, w);
+
+   North_Latitudes[j] = r;
+
+}
+
+   //
+   //  done
+   //
 
 }
 
@@ -107,7 +177,40 @@ Data = data;
 ////////////////////////////////////////////////////////////////////////
 
 
-void GaussianGrid::latlon_to_xy(double lat, double lon, double &x, double &y) const
+double GaussianGrid::y_to_lat(int y) const
+
+{
+
+int index;
+double lat;
+
+if ( y < N_north_lats )  {
+
+   index = N_north_lats - 1 - y;
+
+   lat = North_Latitudes[index];
+
+   lat = -lat;
+
+} else {
+
+   index = y - N_north_lats;
+
+   lat = North_Latitudes[index];
+
+}
+
+
+
+return ( lat );
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void GaussianGrid::latlon_to_xy(double lat, double lon, double & x, double & y) const
 
 {
 
@@ -123,13 +226,32 @@ return;
 ////////////////////////////////////////////////////////////////////////
 
 
-void GaussianGrid::xy_to_latlon(double x, double y, double &lat, double &lon) const
+void GaussianGrid::xy_to_latlon(double x, double y, double & lat, double & lon) const
 
 {
 
-mlog << Error << "\n\n  GaussianGrid::xy_to_latlon() -> not yet implemented\n\n";
+const int ix = nint(x);
+const int iy = nint(y);
 
-exit ( 1 );
+if ( (fabs(x - ix) >= int_check_tol) || (fabs(y - iy) >= int_check_tol) )  {
+
+   cerr << "\n\n  GaussianGrid::xy_to_latlon() const -> x and y must be integers\n";
+
+   exit ( 1 );
+
+}
+
+if ( (iy < 0) || (iy >= Ny) )  {
+
+   cerr << "\n\n  GaussianGrid::xy_to_latlon() const -> range check error on y\n\n";
+
+   exit ( 1 );
+
+}
+
+lat = y_to_lat(iy);
+
+lon = Lon_Zero - ix*Delta_Lon;
 
 return;
 
@@ -143,11 +265,19 @@ double GaussianGrid::calc_area(int x, int y) const
 
 {
 
-mlog << Error << "\n\n  GaussianGrid::calc_area() -> not yet implemented\n\n";
+double lat_top, lat_bot;
+double area;
 
-exit ( 1 );
+if ( y == (Ny - 1) )  lat_top = 90.0;
+else                  lat_top = y_to_lat(y + 1);
 
-double area = 0.0;
+lat_bot = y_to_lat(y);
+
+area = rad_per_deg*fabs(Delta_Lon);   //  Delta_Lon in radians
+
+area *= sind(lat_top) - sind(lat_bot);
+
+area *= earth_radius_km*earth_radius_km;
 
 return ( area );
 
