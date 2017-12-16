97272150f138686764730572b0add3b34fe821f6
John Halley Gotway <johnhg@ucar.edu>
2017-12-16 00:17:31 +0000

    Committing big change for Grid-Stat, enabling many config file options to be separately for each verification task, including the vx_mask, bootstrap info, and smoothing options.
    Former-commit-id: 1d3a9cbe53d5a2b9260f9aeec140d233e12ae5aa

--- met/src/tools/core/grid_stat/grid_stat_conf_info.h
+++ met/src/tools/core/grid_stat/grid_stat_conf_info.h
@@ -62,28 +62,19 @@ static const STATLineType txt_file_type[n_txt] = {
 
 ////////////////////////////////////////////////////////////////////////
 
-
 struct GridStatNcOutInfo {
 
    bool do_latlon;
-
    bool do_raw;
-
    bool do_diff;
-
    bool do_climo;
-
    bool do_weight;
-
    bool do_nbrhd;
-
    bool do_fourier;
-
    bool do_gradient;
-
    bool do_apply_mask;
 
-      //////////////////
+      //////////////////////////////////////////////////////////////////
 
    GridStatNcOutInfo();
 
@@ -93,9 +84,108 @@ struct GridStatNcOutInfo {
 
    void set_all_false();
    void set_all_true();
-
 };
 
+////////////////////////////////////////////////////////////////////////
+
+class GridStatVxOpt {
+
+   private:
+
+      void init_from_scratch();
+
+   public:
+
+      GridStatVxOpt();
+     ~GridStatVxOpt();
+
+      //////////////////////////////////////////////////////////////////
+
+      VarInfo *        fcst_info;        // fcst VarInfo pointer (allocated)
+      VarInfo *        obs_info;         // obs VarInfo pointer (allocated)
+
+      RegridInfo       regrid_info;      // Regridding information
+
+      ConcatString     desc;             // Description string
+      ConcatString     var_str;          // nc_pairs_var_str string
+
+      ThreshArray      fcat_ta;          // fcst categorical thresholds
+      ThreshArray      ocat_ta;          // obs categorical thresholds
+
+      ThreshArray      fcnt_ta;          // fcst continuous thresholds
+      ThreshArray      ocnt_ta;          // obs continuous thresholds
+      SetLogic         cnt_logic;        // continuous threshold field logic
+
+      ThreshArray      fwind_ta;         // fcst wind speed thresholds
+      ThreshArray      owind_ta;         // obs wind speed thresholds
+      SetLogic         wind_logic;       // wind speed field logic
+
+      StringArray      mask_grid;        // Masking grid strings
+      StringArray      mask_poly;        // Masking polyline strings
+
+      StringArray      mask_name;        // Masking region names
+
+      NumArray         eclv_points;      // ECLV points
+
+      ThreshArray      climo_cdf_ta;     // Climo CDF thresh array
+
+      NumArray         ci_alpha;         // Alpha value for confidence intervals
+
+      BootInfo         boot_info;        // Bootstrapping information
+      InterpInfo       interp_info;      // Interpolation (smoothing) information
+      NbrhdInfo        nbrhd_info;       // Neighborhood statistics information
+
+      // Fourier Options
+      IntArray         wave_1d_beg;      // Fourier 1-dimensional decomposition
+      IntArray         wave_1d_end;      // beginning and ending wave numbers
+
+      bool             rank_corr_flag;   // Flag for computing rank correlations
+
+      // Output file options
+      STATOutputType    output_flag[n_txt]; // Flag for each output line type
+      GridStatNcOutInfo nc_info;            // Output NetCDF pairs file contents
+
+      //////////////////////////////////////////////////////////////////
+
+      void clear();
+
+      void process_config(GrdFileType, Dictionary &,
+                          GrdFileType, Dictionary &);
+      void parse_nc_info(Dictionary &);
+
+      // Compute the number of output lines for this task
+      int n_txt_row(int i)     const;
+
+      int get_n_mask()         const;
+      int get_n_interp()       const;
+
+      int get_n_cnt_thresh()   const;
+      int get_n_cat_thresh()   const;
+      int get_n_wind_thresh()  const;
+      int get_n_fprob_thresh() const;
+      int get_n_oprob_thresh() const;
+
+      int get_n_eclv_points()  const;
+      int get_n_cdf_bin()      const;
+      int get_n_nbrhd_wdth()   const;
+      int get_n_cov_thresh()   const;
+      int get_n_wave_1d()      const;
+      int get_n_ci_alpha()     const;
+};
+
+////////////////////////////////////////////////////////////////////////
+
+inline int  GridStatVxOpt::get_n_mask()        const { return(mask_name.n_elements());         }
+inline int  GridStatVxOpt::get_n_interp()      const { return(interp_info.n_interp);           }
+inline int  GridStatVxOpt::get_n_cnt_thresh()  const { return(fcnt_ta.n_elements());           }
+inline int  GridStatVxOpt::get_n_cat_thresh()  const { return(fcat_ta.n_elements());           }
+inline int  GridStatVxOpt::get_n_wind_thresh() const { return(fwind_ta.n_elements());          }
+inline int  GridStatVxOpt::get_n_eclv_points() const { return(eclv_points.n_elements());       }
+inline int  GridStatVxOpt::get_n_cdf_bin()     const { return(climo_cdf_ta.n_elements() - 1);  }
+inline int  GridStatVxOpt::get_n_nbrhd_wdth()  const { return(nbrhd_info.width.n_elements());  }
+inline int  GridStatVxOpt::get_n_cov_thresh()  const { return(nbrhd_info.cov_ta.n_elements()); }
+inline int  GridStatVxOpt::get_n_wave_1d()     const { return(wave_1d_beg.n_elements());       }
+inline int  GridStatVxOpt::get_n_ci_alpha()    const { return(ci_alpha.n_elements());          }
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -105,169 +195,77 @@ class GridStatConfInfo {
 
       void init_from_scratch();
 
-      // Counts based on the contents of the config file
-      int n_vx;          // Number of fields to be verified
-      int n_vx_scal;     // Number of scalar fields to be verified
-      int n_vx_vect;     // Number of vector fields to be verified
-      int n_vx_prob;     // Number of probability fields to be verified
+      // Number of verification tasks
+      int n_vx;
 
-      int n_mask;        // Number of masking regions
-      int n_interp;      // Number of interpolation methods
-
-      int max_n_cnt_thresh;   // Maximum number of continuous thresholds
-      int max_n_cat_thresh;   // Maximum number of categorical thresholds
-      int max_n_wind_thresh;  // Maximum number of wind speed thresholds
-      int max_n_fprob_thresh; // Maximum fcst prob thresholds
-      int max_n_oprob_thresh; // Maximum obs prob thresholds
+      // Flags indicating requested output types
+      bool output_ascii_flag;               // Flag for ASCII output
+      bool output_nc_flag;                  // Flag for NetCDF output
 
    public:
+
+      GridStatConfInfo();
+     ~GridStatConfInfo();
+
+      //////////////////////////////////////////////////////////////////
 
       // Grid-Stat configuration object
       MetConfig conf;
 
       // Store data parsed from the Grid-Stat configuration object
-      ConcatString      model;              // Model name
-      ConcatString      obtype;             // Observation type
-      RegridInfo        regrid_info;        // Regridding information
+      ConcatString model;                   // Model name
+      ConcatString obtype;                  // Observation type
 
-      // Settings for each verification task
-      StringArray       desc;               // Array of description strings [n_vx]
-      StringArray       var_str;            // Array of nc_pairs_var_str strings [n_vx]
+      GridStatVxOpt * vx_opt;               // Array of vx task options [n_vx] (allocated)
 
-      VarInfo **        fcst_info;          // Array of pointers for fcst VarInfo [n_vx]
-      VarInfo **        obs_info;           // Array of pointers for obs VarInfo [n_vx]
+      map<ConcatString,DataPlane> mask_map; // Mapping of mask names to DataPlanes
 
-      ThreshArray *     fcat_ta;            // Array for fcst categorical thresholds [n_vx]
-      ThreshArray *     ocat_ta;            // Array for obs categorical thresholds [n_vx]
+      GridWeightType grid_weight_flag;      // Grid weighting flag
+      ConcatString   tmp_dir;               // Directory for temporary files
+      ConcatString   output_prefix;         // String to customize output file name
+      ConcatString   version;               // Config file version
 
-      ThreshArray *     fcnt_ta;            // Array for fcst continuous thresholds [n_vx]
-      ThreshArray *     ocnt_ta;            // Array for obs continuous thresholds [n_vx]
-      SetLogic *        cnt_logic;          // Array of continuous threshold field logic [n_vx]
-
-      ThreshArray *     fwind_ta;           // Array for fcst wind speed thresholds [n_vx]
-      ThreshArray *     owind_ta;           // Array for obs wind speed thresholds [n_vx]
-      SetLogic *        wind_logic;         // Array of wind speed field logic [n_vx]
-
-      NumArray *        eclv_points;        // ECLV points [n_vx]
-
-      ThreshArray       climo_cdf_ta;       // Climo CDF thresh array
-
-      // Settings for all verification tasks
-      StringArray       mask_name;          // Masking region names [n_mask]
-      DataPlane *       mask_dp;            // Array for masking regions [n_mask]
-
-      NumArray          ci_alpha;           // Alpha value for confidence intervals
-      BootIntervalType  boot_interval;      // Bootstrap CI type
-      double            boot_rep_prop;      // Bootstrap replicate proportion
-      int               n_boot_rep;         // Number of bootstrap replicates
-      ConcatString      boot_rng;           // GSL random number generator
-      ConcatString      boot_seed;          // GSL RNG seed value
-
-      FieldType         interp_field;       // How to apply interpolation options
-      double            interp_thresh;      // Proportion of valid data values
-      InterpMthd *      interp_mthd;        // Array for interpolation methods [n_interp]
-      IntArray          interp_wdth;        // Array for interpolation widths [n_interp]
-      GridTemplateFactory::GridTemplates    // Grid shape for Interpolation
-                        interp_shape;
-
-      double            nbrhd_thresh;       // Proportion of valid data values
-      IntArray          nbrhd_wdth;         // Array for neighborhood widths
-      ThreshArray       nbrhd_cov_ta;       // Neighborhood coverage thresholds
-      GridTemplateFactory::GridTemplates    // Grid shape for Interpolation
-                        nbrhd_shape;
-
-      IntArray          wave_1d_beg;        // Fourier 1-dimensional decomposition
-      IntArray          wave_1d_end;        // beginning and ending wave numbers
-
+      // Summary of output file options across all verification tasks
       STATOutputType    output_flag[n_txt]; // Flag for each output line type
       GridStatNcOutInfo nc_info;            // Output NetCDF pairs file contents
-      GridWeightType    grid_weight_flag;   // Grid weighting flag
-      bool              rank_corr_flag;     // Flag for computing rank correlations
 
-      ConcatString      tmp_dir;            // Directory for temporary files
-      ConcatString      output_prefix;      // String to customize output file name
-      ConcatString      version;            // Config file version
-
-      GridStatConfInfo();
-     ~GridStatConfInfo();
-
-      // Setting for generating ASCII output file
-      bool             ascii_output_flag;   // Flag to indicate whether the ASCII text file
-                                            // should be generated
+      //////////////////////////////////////////////////////////////////
 
       void clear();
 
       void read_config   (const char *, const char *);
       void process_config(GrdFileType, GrdFileType);
+      void process_flags ();
       void process_masks (const Grid &);
 
-      void parse_nc_info();
-
       // Dump out the counts
-      int get_n_vx()          const;
-      int get_n_vx_scal()     const;
-      int get_n_vx_vect()     const;
-      int get_n_vx_prob()     const;
-      int get_n_mask()        const;
-      int get_n_cdf_bin()     const;
+      int  get_n_vx()              const;
+      int  get_compression_level();
+      bool get_output_ascii_flag() const;
+      bool get_output_nc_flag()    const;
 
-      int get_n_interp()      const;
-      int get_n_nbrhd_wdth()  const;
-      int get_n_wave_1d()     const;
-      int get_n_cov_thresh()  const;
-      int get_n_ci_alpha()    const;
-      int get_vflag()         const;
-      int get_pflag()         const;
+      // Compute the maximum number of output lines possible based
+      // on the contents of the configuration file
+      int n_txt_row(int i) const;
+      int n_stat_row()     const;
 
+      // Maximum across all verification tasks
       int get_max_n_cat_thresh()   const;
       int get_max_n_cnt_thresh()   const;
       int get_max_n_wind_thresh()  const;
       int get_max_n_fprob_thresh() const;
       int get_max_n_oprob_thresh() const;
+      int get_max_n_eclv_points()  const;
+      int get_max_n_cov_thresh()   const;
 
-      // Compute the maximum number of output lines possible based
-      // on the contents of the configuration file
-      int n_txt_row (int i);
-      int n_stat_row();
-      int get_compression_level();
-
-      // Accessor and mutator for the ascii_output_flag
-      void set_ascii_output_flag(bool val);
-      bool get_ascii_output_flag();
 };
 
 ////////////////////////////////////////////////////////////////////////
 
-inline int GridStatConfInfo::get_n_vx()          const { return(n_vx);                         }
-inline int GridStatConfInfo::get_n_vx_scal()     const { return(n_vx_scal);                    }
-inline int GridStatConfInfo::get_n_vx_vect()     const { return(n_vx_vect);                    }
-inline int GridStatConfInfo::get_n_vx_prob()     const { return(n_vx_prob);                    }
-inline int GridStatConfInfo::get_n_cdf_bin()     const { return(climo_cdf_ta.n_elements() - 1);}
-inline int GridStatConfInfo::get_n_mask()        const { return(n_mask);                       }
-inline int GridStatConfInfo::get_n_interp()      const { return(n_interp);                     }
-inline int GridStatConfInfo::get_n_nbrhd_wdth()  const { return(nbrhd_wdth.n_elements());      }
-inline int GridStatConfInfo::get_n_wave_1d()     const { return(wave_1d_beg.n_elements());     }
-inline int GridStatConfInfo::get_n_cov_thresh()  const { return(nbrhd_cov_ta.n_elements());    }
-inline int GridStatConfInfo::get_n_ci_alpha()    const { return(ci_alpha.n_elements());        }
-inline int GridStatConfInfo::get_vflag()         const { return(n_vx_vect > 0);                }
-inline int GridStatConfInfo::get_pflag()         const { return(n_vx_prob > 0);                }
-inline int GridStatConfInfo::get_compression_level()   { return(conf.nc_compression());        }
-
-inline int GridStatConfInfo::get_max_n_cat_thresh() const {
-   return(max_n_cat_thresh);
-}
-inline int GridStatConfInfo::get_max_n_cnt_thresh() const {
-   return(max_n_cnt_thresh);
-}
-inline int GridStatConfInfo::get_max_n_wind_thresh() const {
-   return(max_n_wind_thresh);
-}
-inline int GridStatConfInfo::get_max_n_fprob_thresh() const {
-   return(max_n_fprob_thresh);
-}
-inline int GridStatConfInfo::get_max_n_oprob_thresh() const {
-   return(max_n_oprob_thresh);
-}
+inline int  GridStatConfInfo::get_n_vx()              const { return(n_vx);                  }
+inline int  GridStatConfInfo::get_compression_level()       { return(conf.nc_compression()); }
+inline bool GridStatConfInfo::get_output_ascii_flag() const { return(output_ascii_flag);     }
+inline bool GridStatConfInfo::get_output_nc_flag()    const { return(output_nc_flag);        }
 
 ////////////////////////////////////////////////////////////////////////
 
