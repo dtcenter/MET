36ece7e95b48367e73688403dc1603ba1b3a8d1d
John Halley Gotway <johnhg@ucar.edu>
2018-03-07 21:57:35 +0000

    Update to handle the presence of the obs_var NetCDF point variable.
    Former-commit-id: d04877a953506b6b8499a7bcd85750a85e13cec4

--- met/scripts/Rscripts/pntnc2ascii.R
+++ met/scripts/Rscripts/pntnc2ascii.R
@@ -4,17 +4,17 @@ usage = function(){
 	cat("\nUsage: pntnc2ascii\n",
 		"        [-sid name]\n",
 		"        [-msg_type name]\n",
-		"        [-code value]\n",
+		"        [-obs_var name]\n",
 		"        nc_file\n\n",
 		"        where   \"-sid name\" prints only observations for that station id (optional).\n",
-		"                \"-msg_typ name \" prints only observations for that message type (optional).\n",
-		"                \"-code value\" prints only observations for that GRIB code (optional).\n",
+		"                \"-msg_typ name\" prints only observations for that message type (optional).\n",
+		"                \"-obs_var name\" prints only observations for that observation variable or GRIB code (optional).\n",
 		"                \"nc_file\" is the netCDF point observation file (required).\n\n",
 		sep="");
 }
 
 # parse and verify command line arguments
-strSid = ""; strMsg = ""; strGc = "";
+strSid = ""; strMsg = ""; strObs = "";
 listArgs = commandArgs(TRUE);
 if( 1 > length(listArgs) ){ usage(); q(); }
 if( 1 < length(listArgs) ){
@@ -23,8 +23,8 @@ if( 1 < length(listArgs) ){
 			strSid = listArgs[i+1];
 		} else if( "-msg_typ" == listArgs[i] ){
 			strMsg = listArgs[i+1];
-		} else if( "-code"  == listArgs[i] ){
-			strGc = listArgs[i+1];
+		} else if( "-obs_var"  == listArgs[i] ){
+			strObs = listArgs[i+1];
 		} else {
 			cat("ERROR: unrecognized input argument '", listArgs[i], "'\n", sep="");
 			usage(); q(status=1);
@@ -60,21 +60,29 @@ if( 0 < length(ncPnt$var[["obs_qty"]]) ) {
 # build the observation data frame
 dfObs = data.frame(
 	hdr_id = ncvar_get(ncPnt, ncPnt$var[["obs_arr"]])[1,],
-	gc     = ncvar_get(ncPnt, ncPnt$var[["obs_arr"]])[2,],
+	var    = ncvar_get(ncPnt, ncPnt$var[["obs_arr"]])[2,],
 	lvl    = ncvar_get(ncPnt, ncPnt$var[["obs_arr"]])[3,],
 	hgt    = ncvar_get(ncPnt, ncPnt$var[["obs_arr"]])[4,],
 	qty    = ObsQty,
 	ob     = signif(ncvar_get(ncPnt, ncPnt$var[["obs_arr"]])[5,], 5)
 );
 
+# check for the obs_var variable and update dfObs$var
+if( 0 < length(ncPnt$var[["obs_var"]]) ) {
+	ObsVar = ncvar_get(ncPnt, ncPnt$var[["obs_var"]]);
+	dfObs$var = ObsVar[dfObs$var+1];
+}
+
 # apply the filtering criteria and merge the headers and obs
 if( strSid != "" ){ dfHdr = dfHdr[dfHdr$sid == strSid,]; }
 if( strMsg != "" ){ dfHdr = dfHdr[dfHdr$typ == strMsg,]; }
-if( strGc  != "" ){ dfObs = dfObs[dfObs$gc  == strGc, ]; }
+if( strObs != "" ){ dfObs = dfObs[dfObs$var == strObs,]; }
 dfSid = merge(dfHdr, dfObs);
 
 # format level column to HHMMSS for accumulation intervals
-ind_accum = ( dfSid$gc == 61 | dfSid$gc == 62 | dfSid$gc == 63 ) & ( dfSid$lvl != -9999 );
+ind_accum = ( dfSid$var == 61 | dfSid$var == "APCP"  |
+              dfSid$var == 62 | dfSid$var == "NCPCP" |
+              dfSid$var == 63 | dfSid$var == "ACPCP" ) & ( dfSid$lvl != -9999 );
 dfSid$lvl[ind_accum] = format(.POSIXct(dfSid$lvl[ind_accum], tz="GMT"), "%H%M%S");
 
 # print the formatted observations
