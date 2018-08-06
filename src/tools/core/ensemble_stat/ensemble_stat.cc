8b7ad3e8864547d5299755462c5d4754b14d97b4
Howard Soh <hsoh@ucar.edu>
2018-08-06 17:20:20 +0000

    Revert back to 5632
    Former-commit-id: 6930bc0ea4e07f0e6a3602621de8b5b6e45b87bc

--- src/tools/core/ensemble_stat/ensemble_stat.cc
+++ src/tools/core/ensemble_stat/ensemble_stat.cc
@@ -73,7 +73,6 @@ using namespace std;
 #include "vx_data2d_nc_met.h"
 #include "vx_regrid.h"
 #include "vx_log.h"
-#include "nc_tools.h"
 
 ////////////////////////////////////////////////////////////////////////
 
