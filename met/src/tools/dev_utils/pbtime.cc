ae2bcc1a6c65071f59bf7e56a6e4b040d2904bd9
John Halley Gotway <johnhg@ucar.edu>
2018-11-26 18:38:02 +0000

    Fix pbtime by switching from readpb_ to ireadns_.
    Former-commit-id: 3418be1305d36efda70eba282072c9e3c066b075

--- met/src/tools/dev_utils/pbtime.cc
+++ met/src/tools/dev_utils/pbtime.cc
@@ -45,44 +45,21 @@ using namespace std;
 //
 
 static const char *program_name = "pbtime";
-
-// Constants used to interface to Fortran subroutines
-
-// Maximum number of BUFR parameters
-static const int mxr8pm         = 10;
-// Maximum number of BUFR levels
-static const int mxr8lv         = 255;
-// Maximum number of BUFR event sequences
-static const int mxr8vn         = 10;
-// Maximum number of BUFR variable types
-static const int mxr8vt         = 6;
-
-// File unit number for opening the PrepBufr file
 static int file_unit            = 11;
-
-////////////////////////////////////////////////////////////////////////
-
-// Shared memory defined in the PREPBC common block in readpb.prm
-static double      hdr[mxr8pm];
-static double      evns[mxr8vt][mxr8vn][mxr8lv][mxr8pm];
-static int         nlev;
 
 ////////////////////////////////////////////////////////////////////////
 
 extern "C" {
    void openpb_(const char *, int *);
-   void readpb_(int *, char *, int *, int *, int *, double[mxr8pm],
-                double[mxr8vt][mxr8vn][mxr8lv][mxr8pm], int *);
+   void ireadns_(int *, char *, int *);
 }
 
 ////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[]) {
-   int i_date, i_ret;
-   int mon, day, yr, hr, min, sec;
+   int i_date;
    ConcatString pb_file, blk_file;
    unixtime ut;
-   int req_lev = mxr8lv;
    char hdr_typ[512], time_str[512];
 
    if(argc != 2) {
@@ -106,14 +83,12 @@ int main(int argc, char *argv[]) {
    openpb_(blk_file, &file_unit);
 
    // Read the first PrepBufr message
-   readpb_(&file_unit, hdr_typ, &i_date, &i_ret, &nlev, hdr, evns, &req_lev);
+   ireadns_(&file_unit, hdr_typ, &i_date);
 
    // Format and dump out the time string
    sprintf(time_str, "%.10i", i_date);
    ut = yyyymmddhh_to_unix(time_str);
-   unix_to_mdyhms(ut, mon, day, yr, hr, min, sec);
-   sprintf(time_str, "%.4i%.2i%.2i_%.2i%.2i%.2i", yr, mon, day, hr, min, sec);
-   cout << time_str << "\n" << flush;
+   cout << unix_to_yyyymmdd_hhmmss(ut) << "\n" << flush;
 
    // Delete the temporary blocked file
    remove_temp_file(blk_file);
