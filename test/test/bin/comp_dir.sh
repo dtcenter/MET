9d30562eb1aab56152b0cba2db95afd5a17b7c1e
John Halley Gotway <johnhg@ucar.edu>
2019-05-03 21:21:00 +0000

    Major resturcturing of the MET svn repo to make it follow the traditional trunk/branches/tags model.  This is in hopes that it'll export nicely to git now.
    Former-commit-id: a9eeae8398581ea3531f1226be5bc983efe8aa15

---
+++ test/test/bin/comp_dir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# check for environment variables, use defaults if necessary
+MET_TEST_BASE=${MET_TEST_BASE:-/d3/projects/MET/MET_development/svn-met-dev.cgd.ucar.edu/trunk/test}
+MET_TEST_RSCRIPT=${MET_TEST_RSCRIPT:-/usr/local/bin/Rscript}
+COMP_SCRIPT=$MET_TEST_BASE/R_test/comp_dir.R
+
+# get environment settings
+. ${MET_TEST_BASE}/bin/set_env.sh
+
+$MET_TEST_RSCRIPT $COMP_SCRIPT $@ 2>&1
+
