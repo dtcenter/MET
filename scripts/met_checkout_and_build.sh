d278f0ad36756dab654c993148383e499f19d579
John Halley-Gotway <johnhg@ucar.edu>
2019-05-13 13:55:09 -0600

    Updating checkout and build script.

--- scripts/met_checkout_and_build.sh
+++ scripts/met_checkout_and_build.sh
@@ -8,8 +8,8 @@
 # of MET using either the latest version of the files or using
 # an existing tag or branch name.  First, go to the directory where
 # you'd like the release built.  Then run:
-#    svn co https://svn-met-dev.cgd.ucar.edu/build/scripts
-#    scripts/met_checkout_and_build.sh [new|tag|branch] [name]
+#    git clone https://github.com/NCAR/MET
+#    MET/scripts/met_checkout_and_build.sh [new|tag|branch] [name]
 #
 # For a new release, this script will:
 # (1) Checkout the latest version of the MET source code and
@@ -35,15 +35,10 @@
 #=======================================================================
 
 # Constants
-SVN_BASE="https://svn-met-dev.cgd.ucar.edu"
+GIT_REPO="https://github.com/NCAR/MET"
 
 # MET_DEVELOPMENT must be set to build a release
 export MET_DEVELOPMENT=true
-
-# Repository Structure:
-#    Development version: SVN_BASE/trunk/met
-#    Tagged versions:     SVN_BASE/tags/met/TAG_NAME
-#    Branch versions:     SVN_BASE/branches/met/BRANCH_NAME
 
 # Get the number of arguments.
 NARGS=$#
@@ -52,7 +47,7 @@ NARGS=$#
 usage() {
    echo
    echo "USAGE: met_checkout_and_build.sh <new, tag, branch> <name>"
-   echo "       'met_checkout_and_build.sh new'             to build a new release with no TAG"
+   echo "       'met_checkout_and_build.sh new'             to build a new release from develop with no TAG"
    echo "       'met_checkout_and_build.sh new_tag    NAME' to build a new release and tag it"
    echo "       'met_checkout_and_build.sh new_branch NAME' to build a new release and create a branch"
    echo "       'met_checkout_and_build.sh tag        NAME' to build an existing tag"
@@ -86,74 +81,61 @@ run_command() {
 }
 
 # Run svn info to update contents of version.txt
-get_svn_info() {
-
-  echo "CALLING: svn info $1"
-  svn info $1 |
-    egrep '^URL:|^Last Changed Rev:|^Last Changed Date:' |
-    sed 's=URL: =Repository:\t=' |
-    sed 's=Last Changed Rev: =Revision:\t=' |
-    sed 's=Last Changed Date: =Change Date:\t=' > $2/data/version.txt
+get_git_info() {
+  echo "CALLING: git config, git rev-parse, and git log"
+  git config --get remote.origin.url | sed -r 's/^/Repository:\t/g' > $1/data/version.txt
+  git rev-parse --short HEAD         | sed -r 's/^/Last Changed Rev:\t/g' >> $1/data/version.txt
+  git log -1 --format=%cd met        | sed -r 's/^/Last Changed Date:\t/g' >> $1/data/version.txt
 }
+
+# Clone repo into a working directory
+mkdir build; cd build
+git clone ${GIT_REPO}
+cd MET
 
 # Parse the command line
 if [[ ${NARGS} -eq 1 && $1 == "new" ]]; then
 
-   echo "Checking out the latest version of MET without tagging."
-   CMD='export'
-   SVN_PATH="${SVN_BASE}/trunk/met"
-   run_command "svn ${CMD} -q ${SVN_PATH}"
+   echo "Checking out the latest MET develop branch without tagging."
+   run_command "git checkout develop"
    BUILD_ARGS=""
-   get_svn_info ${SVN_PATH} met
+   get_git_info met
 
 elif [[ ${NARGS} -eq 2 && $1 == "new_tag" ]]; then
 
-   echo "Checking out the latest version of MET and tagging as '$2'."
-   CMD='co'
-   SVN_PATH="${SVN_BASE}/tags/met/$2"
-   run_command "svn ${CMD} -q ${SVN_BASE}/trunk/met"
-   echo "Tagging met $2" > log.msg
-   run_command "svn cp ${SVN_BASE}/trunk/met ${SVN_PATH} -F log.msg"
-   echo "Tagging test $2" > log.msg
-   run_command "svn cp ${SVN_BASE}/trunk/test ${SVN_BASE}/tags/test/$2 -F log.msg"
-   run_command "mv met $2"
+   echo "Checking out the latest MET develop branch and tagging as '$2'."
+   run_command "git checkout develop"
+   echo "Creating tag $2 from the develop branch" > log.msg
+   run_command "git tag -a $2 -F log.msg"
+   run_command "git push origin $2"
    rm log.msg
    BUILD_ARGS="$2"
-   get_svn_info ${SVN_PATH} ${BUILD_ARGS}
+   get_git_info ${BUILD_ARGS}
 
 elif [[ ${NARGS} -eq 2 && $1 == "new_branch" ]]; then
 
-   echo "Checking out the latest version of MET and creating branch '$2'."
-   CMD='co'
-   SVN_PATH="${SVN_BASE}/branches/met/$2"
-   run_command "svn ${CMD} -q ${SVN_BASE}/trunk/met"
-   echo "Creating met branch $2" > log.msg
-   run_command "svn cp ${SVN_BASE}/trunk/met ${SVN_PATH} -F log.msg"
-   run_command "svn ${CMD} -q ${SVN_BASE}/trunk/test"
-   echo "Creating test branch $2" > log.msg
-   run_command "svn cp ${SVN_BASE}/trunk/test ${SVN_BASE}/branches/test/$2 -F log.msg"
-   run_command "mv met $2"
+   echo "Checking out the latest MET develop branch and creating branch '$2'."
+   run_command "git checkout develop"
+   echo "Creating met branch $2 from the develop branch" > log.msg
+   run_command "git checkout -b $2 -F log.msg"
+   run_command "git push -u origin $2"
    rm log.msg
    BUILD_ARGS="$2"
-   get_svn_info ${SVN_PATH} ${BUILD_ARGS}
+   get_git_info ${BUILD_ARGS}
 
 elif [[ ${NARGS} -eq 2 && $1 == "tag" ]]; then
 
    echo "Checking out the '$2' tagged version of MET."
-   CMD='export'
-   SVN_PATH="${SVN_BASE}/tags/met/$2"
-   run_command "svn ${CMD} -q ${SVN_PATH}"
+   run_command "git checkout met-8.1"
    BUILD_ARGS="$2"
-   get_svn_info ${SVN_PATH} ${BUILD_ARGS}
+   get_git_info ${BUILD_ARGS}
 
 elif [[ ${NARGS} -eq 2 && $1 == "branch" ]]; then
 
    echo "Checking out the '$2' branch version of MET."
-   CMD='export'
-   SVN_PATH="${SVN_BASE}/branches/met/$2"
-   run_command "svn ${CMD} -q ${SVN_PATH}"
+   run_command "git checkout $2"
    BUILD_ARGS="$2"
-   get_svn_info ${SVN_PATH} ${BUILD_ARGS}
+   get_git_info ${BUILD_ARGS}
 
 else
    usage
@@ -161,7 +143,5 @@ else
 fi
 
 # Call the build script to build the release
-run_command "cp -f ${2:-met}/build/met_build.sh scripts/."
-run_command "chmod +x scripts/met_build.sh"
 run_command "scripts/met_build.sh ${BUILD_ARGS}"
-run_command "rm -rf ${2:-met}"
+run_command "mv *.tar.gz ../../. ; cd ../.. ; rm -rf build"
