56a7624b023f07e1a9cb0ea82f122f405d51a72e
John Halley-Gotway <johnhg@ucar.edu>
2019-05-14 16:26:41 -0600

    Updates to met_build.sh for master_v3.0.1

--- scripts/met_build.sh
+++ scripts/met_build.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 #
 # Build a release for the MET
 # (Model Evaluation Tools)
@@ -7,8 +7,7 @@
 # The MET_build script will build a release
 # of the MET and will tar up the source code.
 # This script is called by the MET_checkout_and_build
-# script and should be done so from the top-level
-# MET source code directory.
+# script.
 #
 # The desired version of the code must have alredy
 # been checked out prior to calling this script.
@@ -27,44 +26,31 @@ DATE=`date +%Y%m%d`
 CUR_REV=`git rev-parse --short HEAD`
 
 # Check for 0 or 1 argument
-if [[ ${NARGS} -eq 0 ]]; then
-   VERSION="MET_rev${CUR_REV}"
-elif [[ ${NARGS} -eq 1 ]]; then
-   VERSION=$1
+if [ ${NARGS} -eq 0 ]; then
+   VERSION="met-${CUR_REV}"
+elif [ ${NARGS} -eq 1 ]; then
+   VERSION="met-${1}"
 else
+   echo
    echo "USAGE: MET_build <version_number>"
-   exit
+   echo
+   exit 1
 fi
 
-# Go to the top-level MET directory
-MET_PATH=`echo $0 | sed 's/build\/met_build.sh/./'`
-cd ${MET_PATH}
-
-# Make a new directory and copy the current
-# directory into it
-mkdir   ../${VERSION}
-cp -r * ../${VERSION}
-cd      ..
-
-# Set the top-level MET directory
-TOP=${VERSION}
-
 # Move Makefile_release over to Makefile
-for FILE in `find ${TOP} -name Makefile_release`; do
+for FILE in `find met -name Makefile_release`; do
   NEW=`echo ${FILE} | sed 's/_release//g'`
   mv ${FILE} ${NEW}
 done
 
 # Copy default config files into data/config
-for FILE in `find ${TOP} -name "*Config_default"`; do
-  cp ${FILE} ${TOP}/data/config/.
+for FILE in `find met -name "*Config_default"`; do
+  cp ${FILE} met/data/config/.
 done
 
 # Cleanup
-rm -rf ${TOP}/build
-
-# Remove any .git directories before building
-rm -rf `find ${TOP} -name '.git'`
+rm -rf met/build
 
 # Tar up the newly built MET distribution
-tar -cvzf ${VERSION}.${DATE}.tar.gz ${TOP}
+mv met ${VERSION}
+tar -cvzf ${VERSION}.${DATE}.tar.gz ${VERSION}
