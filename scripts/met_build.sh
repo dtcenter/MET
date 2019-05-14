612b7a349071ebe6bb3cad12fb0e87766b0689e3
John Halley-Gotway <johnhg@ucar.edu>
2019-05-14 15:28:16 -0600

    Updating METv4.1 met_build.sh script

--- scripts/met_build.sh
+++ scripts/met_build.sh
@@ -16,9 +16,6 @@
 #
 #================================================
 
-# MET_DEVELOPMENT must be set to build a release
-MET_DEVELOPMENT=true
-
 # Store the number of arguments
 NARGS=$#
 
@@ -28,80 +25,69 @@ DATE=`date +%Y%m%d`
 # Get the current revision hash
 CUR_REV=`git rev-parse --short HEAD`
 
-# Check for a met sub-directory
-if [[ ! -e "met" ]]; then
-  echo
-  echo "ERROR: no \"met\" subdirectory found!"
-  echo
-  exit 1
-fi
-
 # Check for 0 or 1 arguments
 if [[ ${NARGS} -eq 0 ]]; then
-  VERSION="met-${CUR_REV}"
+   VERSION="MET_rev${CUR_REV}"
+   mv met ${VERSION}
 elif [[ ${NARGS} -eq 1 ]]; then
-  VERSION=$1
+   VERSION=$1
 else
-  echo
-  echo "USAGE: MET_build <version_number>"
-  echo
-  exit 1
+   echo "USAGE: MET_build <version_number>"
+   exit
 fi
 
-# Copy the current met directory
-cp -r met ${VERSION}
-cd ${VERSION}
+# Set the top-level MET directory
+TOP=${VERSION}
 
-# Set the MET build version for bootstrap by stripping off leading "met-"
-export MET_BUILD_VERSION=`echo $VERSION | sed 's/met-//g'`
-echo "Building MET version '${MET_BUILD_VERSION}'"
+# Enter the top-level MET directory
+cd ${TOP}
 
-# Run 'make gen_sources' to generate source code
-echo "Running 'make gen_sources' to create the generated source files."
-make gen_sources > /dev/null
+  # Run 'make gen_sources' to generate source code
+  echo "Running 'make gen_sources' to create the generated source files."
+  make gen_sources > /dev/null
 
-# Move Makefile_release over to Makefile, skipping the top-level one
-echo "Renaming 'Makefile_release' to 'Makefile'."
-for FILE in `find ./ -mindepth 2 -name Makefile_release`; do
-  NEW=`echo ${FILE} | sed 's/_release//g'`
-  mv ${FILE} ${NEW}
-done
+  # Move Makefile_release over to Makefile, skipping the top-level one
+  echo "Renaming 'Makefile_release' to 'Makefile'."
+  for FILE in `find ./ -mindepth 2 -name Makefile_release`; do
+    NEW=`echo ${FILE} | sed 's/_release//g'`
+    mv ${FILE} ${NEW}
+  done
 
-# Run 'make clean' to clean out partial build
-echo "Running 'make clean' to clean out partial build."
-make clean > /dev/null
+  # Run 'make clean' to clean out partial build
+  echo "Running 'make clean' to clean out partial build."
+  make clean > /dev/null
 
-# Rename top-level Makefile
-echo "Renaming top-level 'Makefile_release' to 'Makefile'."
-mv Makefile_release Makefile
+  # Rename top-level Makefile
+  echo "Renaming top-level 'Makefile_release' to 'Makefile'."
+  mv Makefile_release Makefile
 
-# Remove top-level user_defs_dev.mk
-echo "Removing top-level 'user_defs_dev.mk'."
-rm -f user_defs_dev.mk
+  # Remove top-level user_defs_dev.mk
+  echo "Removing top-level 'user_defs_dev.mk'."
+  rm -f user_defs_dev.mk
 
-# Remove the development utilities
-echo "Removing 'src/tools/dev_utils'."
-rm -rf src/tools/dev_utils
+  # Remove the development utilities
+  echo "Removing 'src/tools/dev_utils'."
+  rm -rf src/tools/dev_utils
 
-# Remove the enum_to_string library
-echo "Removing 'src/basic/enum_to_string'."
-rm -rf src/basic/enum_to_string
+  # Remove the enum_to_string library
+  echo "Removing 'src/basic/enum_to_string'."
+  rm -rf src/basic/enum_to_string
 
-# Remove any source code for test programs named 'test_*.cc'
-echo "Removing source code for test programs named 'test_*.cc'."
-rm -rf `find ./ -name "test_*.cc"`
+  # Remove any source code for test programs named 'test_*.cc'
+  echo "Removing source code for test programs named 'test_*.cc'."
+  rm -rf `find ./ -name "test_*.cc"`
 
-# Remove any .project or .cproject files before building
-echo "Removing any instances of '.*project'."
-rm -rf `find ./ -name ".*project"`
+  # Remove any .project or .cproject files before building
+  echo "Removing any instances of '.*project'."
+  rm -rf `find ./ -name ".*project"`
 
-# Remove any .svn directories before building
-echo "Removing any instances of '.git'."
-rm -rf `find ./ -name '.git'`
+  # Remove any .git directories before building
+  echo "Removing any instances of '.git'."
+  rm -rf `find ./ -name '.git'`
 
-# Remove the top-level build directory
-echo "Removing top-level 'build' directory."
-rm -rf build
+  # Remove the top-level build directory
+  echo "Removing top-level 'build' directory."
+  rm -rf build
 
 # Go back up one level
 cd ..
@@ -110,11 +96,3 @@ cd ..
 TAR_FILE="${VERSION}.${DATE}.tar.gz"
 echo "Creating tar file '${TAR_FILE}'"
 tar -czf ${TAR_FILE} ${TOP}
-
-# Construct the desired name for the tar file.  autoconf
-# creates the tar file using it's standard naming convention.
-TAR_FILE="${VERSION}.${DATE}.tar.gz"
-
-echo "Copying tar file to new name: '${TAR_FILE}'"
-mv met-* ../${TAR_FILE}
-
