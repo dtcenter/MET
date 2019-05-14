820b7a66e1fce43e3c25710f66b6b75f4139c877
John Halley-Gotway <johnhg@ucar.edu>
2019-05-14 15:51:06 -0600

    Working

--- scripts/met_build.sh
+++ scripts/met_build.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 #
 # Build a release for the MET
 # (Model Evaluation Tools)
@@ -26,45 +26,39 @@ DATE=`date +%Y%m%d`
 CUR_REV=`git rev-parse --short HEAD`
 
 # Check for 0 or 1 argument
-if [[ ${NARGS} -eq 0 ]]; then
+if [ ${NARGS} -eq 0 ]; then
    VERSION="MET_${DATE}_rev${CUR_REV}"
    mv met ${VERSION}
-elif [[ ${NARGS} -eq 1 ]]; then
+elif [ ${NARGS} -eq 1 ]; then
    VERSION=$1
 else
    echo "USAGE: MET_build <version_number>"
    exit
 fi
 
-# Set the top-level MET directory
-TOP=${VERSION}
-
 # Move over the met_data
 for DIR in `ls met_data`; do
-   mv met_data/${DIR}/* ${TOP}/data/${DIR}/.
+   mv met_data/${DIR}/* met/data/${DIR}/.
 done
 rm -rf met_data
 
 # Move over the met_doc
-mv met_doc/*.pdf ${TOP}/doc/.
+mv met_doc/*.pdf met/doc/.
 rm -rf met_doc
 
 # Move Makefile_release over to Makefile
-for FILE in `find ${TOP} -name Makefile_release`; do
+for FILE in `find met -name Makefile_release`; do
   NEW=`echo ${FILE} | sed 's/_release//g'`
   mv ${FILE} ${NEW}
 done
 
 # Copy default config files into data/config
-for FILE in `find ${TOP} -name "*Config_default"`; do
-  cp ${FILE} ${TOP}/data/config/.
+for FILE in `find met -name "*Config_default"`; do
+  cp ${FILE} met/data/config/.
 done
 
 # Cleanup
-rm -rf ${TOP}/build
-
-# Remove any .git directories before building
-rm -rf `find ./ -name '.git'`
+rm -rf met/build
 
 # Tar up the newly built MET distribution
-tar -cvzf ${VERSION}.${DATE}.tar.gz ${TOP}
+tar -cvzf ${VERSION}.${DATE}.tar.gz met
