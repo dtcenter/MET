264219691dd2c0a5b55b4baa9e5cee3a0cd38a06
davidfillmore <fillmore.winslow.david@gmail.com>
2019-06-07 12:07:14 -0600

    Added vx_series_data subdir for new vx_series_data lib.

---
+++ met/src/libcode/vx_series_data/series_data.cc
@@ -0,0 +1,23 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+// ** Copyright UCAR (c) 1992 - 2019
+// ** University Corporation for Atmospheric Research (UCAR)
+// ** National Center for Atmospheric Research (NCAR)
+// ** Research Applications Lab (RAL)
+// ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include "series_data.h"
+
+////////////////////////////////////////////////////////////////////////
