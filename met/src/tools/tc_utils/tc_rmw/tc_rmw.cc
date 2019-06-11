fc6530de4a28ed81a1fbb45ccbe595ed1bb1877f
davidfillmore <fillmore.winslow.david@gmail.com>
2019-06-11 10:41:21 -0600

    Moved parse_file_list to vx_series_data lib.

--- met/src/tools/tc_utils/tc_rmw/tc_rmw.cc
+++ met/src/tools/tc_utils/tc_rmw/tc_rmw.cc
@@ -58,8 +58,6 @@ static void set_config(const StringArray&);
 static void set_out(const StringArray&);
 static void set_logfile(const StringArray&);
 static void set_verbosity(const StringArray&);
-static StringArray parse_file_list(const StringArray&,
-    const GrdFileType);
 static void get_series_data(int, VarInfo*, DataPlane&);
 static void get_series_entry(int, VarInfo*,
     const StringArray&, const GrdFileType,
@@ -427,36 +425,6 @@ void set_logfile(const StringArray& a) {
 
 void set_verbosity(const StringArray& a) {
     mlog.set_verbosity_level(atoi(a[0].c_str()));
-}
-
-////////////////////////////////////////////////////////////////////////
-
-StringArray parse_file_list(const StringArray& a,
-    const GrdFileType type) {
-
-    Met2dDataFile *mtddf = (Met2dDataFile*) 0;
-    StringArray list;
-
-    // Check for empty list
-    if(a.n_elements() == 0) {
-        mlog << Error << "\nparse_file_list() -> "
-             << "empty list!\n\n";
-        exit(1);
-    }
-
-    // Attempt read of first file
-    mtddf = mtddf_factory.new_met_2d_data_file(a[0].c_str(), type);
-
-    // If successful store list
-    if(mtddf) {
-        list.add(a);
-        delete mtddf;
-        mtddf = (Met2dDataFile*) 0;
-    } else { // Otherwise read a list of files 
-        list = parse_ascii_file_list(a[0].c_str());
-    }
-
-    return list;
 }
 
 ////////////////////////////////////////////////////////////////////////
