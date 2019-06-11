0b1ff567c0abe9a139bf86033b66067ad175d6db
davidfillmore <fillmore.winslow.david@gmail.com>
2019-06-11 10:19:19 -0600

    Moved parse_file_list to series_data lib.

--- met/src/libcode/vx_series_data/series_data.cc
+++ met/src/libcode/vx_series_data/series_data.cc
@@ -20,4 +20,35 @@ using namespace std;
 
 #include "series_data.h"
 
+StringArray parse_file_list(const StringArray& a,
+    const GrdFileType type) {
+
+    Met2dDataFileFactory mtddf_factory;
+    Met2dDataFile *mtddf = (Met2dDataFile*) 0;
+    StringArray list;
+
+    // Check for empty list
+    if(a.n_elements() == 0) {
+        mlog << Error << "\nparse_file_list() -> "
+             << "empty list!\n\n";
+        exit(1);
+    }   
+
+    // Attempt read of first file
+    mtddf = mtddf_factory.new_met_2d_data_file(
+        a[0].c_str(), type);
+
+    // If successful store list
+    if(mtddf) {
+        list.add(a);
+        delete mtddf;
+        mtddf = (Met2dDataFile*) 0;
+    } else { // Otherwise read a list of files 
+        list = parse_ascii_file_list(a[0].c_str());
+    }
+
+    return list;
+
+}
+
 ////////////////////////////////////////////////////////////////////////
