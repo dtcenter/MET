b0b93beecf1c4f8572feb9302bfd61f546a223e3
John Halley-Gotway <johnhg@ucar.edu>
2019-08-30 15:42:23 -0600

    Just fixing some spacing

--- met/src/libcode/vx_tc_util/atcf_line_base.cc
+++ met/src/libcode/vx_tc_util/atcf_line_base.cc
@@ -157,8 +157,8 @@ int ATCFLineBase::read_line(LineDataFile * ldf) {
 ////////////////////////////////////////////////////////////////////////
 
 int ATCFLineBase::is_header() const {
-  if(basin().comparecase(0, 5, "BASIN") == 0) return(1);
-   else                                  return(0);
+    if(basin().comparecase(0, 5, "BASIN") == 0) return(1);
+    else                                        return(0);
 }
 
 ////////////////////////////////////////////////////////////////////////
