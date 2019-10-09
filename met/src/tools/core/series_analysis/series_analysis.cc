5864fbcf39b45a46086eda930563282ba3558e0f
John Halley-Gotway <johnhg@ucar.edu>
2019-10-09 12:12:03 -0600

    In Series-Analysis, have to erase the PairDataPoint objects after each loop through the data.

--- met/src/tools/core/series_analysis/series_analysis.cc
+++ met/src/tools/core/series_analysis/series_analysis.cc
@@ -627,6 +627,9 @@ void process_scores() {
    // Loop over the data reads
    for(i_read=0; i_read<n_reads; i_read++) {
 
+      // Initialize PairDataPoint objects
+      for(i=0; i<conf_info.block_size; i++) pd_ptr[i].erase();
+
       // Starting grid point
       i_point = i_read*conf_info.block_size;
 
@@ -993,12 +996,12 @@ void do_cnt(int n, const PairDataPoint *pd_ptr) {
 void do_sl1l2(int n, const PairDataPoint *pd_ptr) {
    int i, j;
    SL1L2Info s_info;
-   NumArray w_na;
+   NumArray wgt_na;
 
    mlog << Debug(4) << "Computing Scalar Partial Sums.\n";
 
    // Set weights to constant value
-   for(i=0; i<pd_ptr->f_na.n(); i++) w_na.add(default_grid_weight);
+   wgt_na.add_const(default_grid_weight, pd_ptr->f_na.n());
 
    // Loop over the continuous thresholds and compute scalar partial sums
    for(i=0; i<conf_info.fcnt_ta.n(); i++) {
@@ -1009,7 +1012,7 @@ void do_sl1l2(int n, const PairDataPoint *pd_ptr) {
       s_info.logic   = conf_info.cnt_logic;
 
       // Compute partial sums
-      s_info.set(pd_ptr->f_na, pd_ptr->o_na, pd_ptr->cmn_na, w_na);
+      s_info.set(pd_ptr->f_na, pd_ptr->o_na, pd_ptr->cmn_na, wgt_na);
 
       // Add statistic value for each possible SL1L2 column
       for(j=0; j<conf_info.output_stats[stat_sl1l2].n(); j++) {
