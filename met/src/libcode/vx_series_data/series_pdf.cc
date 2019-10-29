d411423f5eb50f8399377de3557a1c2cc46d0097
davidfillmore <fillmore.winslow.david@gmail.com>
2019-10-29 11:55:10 -0700

    Added new series_pdf files to Makefile.am.

---
+++ met/src/libcode/vx_series_data/series_pdf.cc
@@ -0,0 +1,29 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+// ** Copyright UCAR (c) 1992 - 2019
+// ** University Corporation for Atmospheric Research (UCAR)
+// ** National Center for Atmospheric Research (NCAR)
+// ** Research Applications Lab (RAL)
+// ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+////////////////////////////////////////////////////////////////////////
+
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include "series_pdf.h"
+
+////////////////////////////////////////////////////////////////////////
+
+void bin_series_data(const VarInfo* data_info,
+    const DataPlane& dp,
+    const MaskPlane& mp) {
+
+}
+
+////////////////////////////////////////////////////////////////////////
---
+++ met/src/libcode/vx_series_data/series_pdf.h
@@ -0,0 +1,32 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+// ** Copyright UCAR (c) 1992 - 2019
+// ** University Corporation for Atmospheric Research (UCAR)
+// ** National Center for Atmospheric Research (NCAR)
+// ** Research Applications Lab (RAL)
+// ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+////////////////////////////////////////////////////////////////////////
+
+#ifndef  __SERIES_PDF_H__
+#define  __SERIES_PDF_H__
+
+////////////////////////////////////////////////////////////////////////
+
+#include "vx_util.h"
+#include "vx_log.h"
+#include "vx_data2d.h"
+#include "vx_data2d_factory.h"
+#include "vx_grid.h"
+
+////////////////////////////////////////////////////////////////////////
+
+void bin_series_data(const VarInfo*,
+    const DataPlane&,
+    const MaskPlane&);
+
+////////////////////////////////////////////////////////////////////////
+
+#endif  // __SERIES_PDF_H__
+
+////////////////////////////////////////////////////////////////////////
