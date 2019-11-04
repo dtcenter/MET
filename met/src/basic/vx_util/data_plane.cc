00cebb87c8423b08ae462160c9c5c3c071b46f19
rgbullock <bullock@ucar.edu>
2019-11-04 12:19:04 -0700

    added funcions to DataPlane to replace bad data

--- met/src/basic/vx_util/data_plane.cc
+++ met/src/basic/vx_util/data_plane.cc
@@ -328,6 +328,22 @@ void DataPlane::censor(const ThreshArray &censor_thresh,
 
 ///////////////////////////////////////////////////////////////////////////////
 
+
+void DataPlane::replace_bad_data(const double value)
+
+{
+
+int j;
+
+if ( is_bad_data(Data[j]) )  Data[j] = value;
+
+return;
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
 int DataPlane::two_to_one(int x, int y) const {
    int n;
 
@@ -1361,3 +1377,28 @@ return ( *(Plane[n]) );
 
 
 ///////////////////////////////////////////////////////////////////////////////
+
+
+void DataPlaneArray::replace_bad_data(const double value)
+
+{
+
+int j;
+
+for (j=0; j<Nplanes; ++j)  {
+
+   Plane[j]->replace_bad_data(value);
+
+}
+
+
+return;
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+
