5d7cdb64600e90fac45883da431e409128ad5298
John Halley-Gotway <johnhg@ucar.edu>
2019-12-19 08:32:48 -0700

    Update build environment for running nightly build on dakota for the master_v8.1 branch.

--- scripts/test_nightly.sh
+++ scripts/test_nightly.sh
@@ -39,35 +39,22 @@ KEEP_STUFF_DURATION=5    #This is in days
 # Store the scripts location
 SCRIPTS=`dirname $0`
 
-# Variables required to build MET (pre-METv5.0)
-export NETCDF=/d3/projects/MET/MET_releases/external_libs/netcdf/netcdf-4.1.3
-export GSL=/d3/projects/MET/MET_releases/external_libs/gsl/gsl-1.13
-export BUFRLIB=/d3/projects/MET/MET_releases/external_libs/bufrlib/bufrlib_gnu
-export BUFRINC=/d3/projects/MET/MET_releases/external_libs/bufrlib/bufrlib_gnu
-export HDF=/d3/projects/MET/MET_releases/external_libs/hdf/HDF4.2r3
-export HDFEOS=/d3/projects/MET/MET_releases/external_libs/hdf_eos/hdfeos
-
-# Variables required to build MET (METv5.0 and later)
+# Variables required to build MET
 export MET_DEVELOPMENT=true
-export MET_NETCDF=/d3/projects/MET/MET_releases/external_libs/netcdf/netcdf-4.1.3
-export MET_GSL=/d3/projects/MET/MET_releases/external_libs/gsl/gsl-1.13
-export MET_GRIB2CINC=/d3/projects/MET/MET_releases/external_libs/g2clib/g2clib-1.4.0
-export MET_GRIB2CLIB=/d3/projects/MET/MET_releases/external_libs/g2clib/g2clib-1.4.0
-export MET_BUFRLIB=/d3/projects/MET/MET_releases/external_libs/bufrlib/bufrlib_gnu
-export MET_HDF=/d3/projects/MET/MET_releases/external_libs/hdf/HDF4.2r3
-export MET_HDFEOS=/d3/projects/MET/MET_releases/external_libs/hdf_eos/hdfeos
+export MET_NETCDF=/usr/local/netcdf
+export MET_HDF5=/usr/local/hdf5
+export MET_HDFINC=/usr/local/hdf4/include/hdf
+export MET_HDFLIB=/usr/local/hdf4/lib
+export MET_HDFEOS=/usr/local/hdfeos
+export MET_BUFR=/usr/local
+export MET_GSL=/d3/projects/MET/MET_releases/external_libs/gnu_6.3.0
+export MET_GRIB2C=/d3/projects/MET/MET_releases/external_libs/gnu_6.3.0
 export MET_CAIROINC=/usr/include/cairo
 export MET_CAIROLIB=/usr/lib
 export MET_FREETYPEINC=/usr/include/freetype2
 export MET_FREETYPELIB=/usr/lib
-
-# Variables required for using Python numpy to create numpy array of data to be used by MET tools
 export MET_PYTHON_CC="-I/usr/include/python2.7 -I/usr/include/x86_64-linux-gnu/python2.7"
 export MET_PYTHON_LD="-L/usr/lib/python2.7/config-x86_64-linux-gnu -L/usr/lib -lpython2.7 -lpthread -ldl  -lutil -lm"
-
-# Variables required to build MET (METv6.0 and later)
-export MET_NETCDF=/usr/local/netcdf
-export MET_HDF5=/usr/local/hdf5
 
 # Variables required to run MET
 export MET_FONT_DIR=/d3/projects/MET/MET_test_data/unit_test/fonts
