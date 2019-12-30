24c8599e7483e9f016b70053a80e2b8b5e3af362
John Halley-Gotway <johnhg@ucar.edu>
2019-12-30 16:00:46 -0700

    Switch from writing ECNT output lines using the PairDataEnsemble class to using the ECNTInfo class instead, which is needed to support the computation of binned climatology mean statistics.

--- met/src/libcode/vx_stat_out/stat_columns.h
+++ met/src/libcode/vx_stat_out/stat_columns.h
@@ -516,8 +516,8 @@ extern void write_mpr_row   (StatHdrColumns &, const PairDataPoint *, STATOutput
                              bool update_thresh = true);
 extern void write_isc_row   (StatHdrColumns &, const ISCInfo &, STATOutputType,
                              AsciiTable &, int &, AsciiTable &, int &);
-extern void write_ecnt_row  (StatHdrColumns &, const PairDataEnsemble *, STATOutputType,
-                             AsciiTable &, int &, AsciiTable &, int &);
+extern void write_ecnt_row  (StatHdrColumns &, const ECNTInfo &, STATOutputType,
+                             int, int, AsciiTable &, int &, AsciiTable &, int &);
 extern void write_rhist_row (StatHdrColumns &, const PairDataEnsemble *, STATOutputType,
                              AsciiTable &, int &, AsciiTable &, int &);
 extern void write_phist_row (StatHdrColumns &, const PairDataEnsemble *, STATOutputType,
@@ -535,36 +535,66 @@ extern void write_relp_row  (StatHdrColumns &, const PairDataEnsemble *, STATOut
 extern void write_header_cols(const StatHdrColumns &,  AsciiTable &, int);
 
 // Write out the columns of data specific to each line type
-extern void write_fho_cols   (const CTSInfo &,          AsciiTable &, int, int);
-extern void write_ctc_cols   (const CTSInfo &,          AsciiTable &, int, int);
-extern void write_cts_cols   (const CTSInfo &,     int, AsciiTable &, int, int);
-extern void write_mctc_cols  (const MCTSInfo &,         AsciiTable &, int, int);
-extern void write_mcts_cols  (const MCTSInfo &,    int, AsciiTable &, int, int);
-extern void write_cnt_cols   (const CNTInfo &,     int, AsciiTable &, int, int);
-extern void write_sl1l2_cols (const SL1L2Info &,        AsciiTable &, int, int);
-extern void write_sal1l2_cols(const SL1L2Info &,        AsciiTable &, int, int);
-extern void write_vl1l2_cols (const VL1L2Info &,        AsciiTable &, int, int);
-extern void write_val1l2_cols(const VL1L2Info &,        AsciiTable &, int, int);
-extern void write_vcnt_cols  (const VL1L2Info &,        AsciiTable &, int, int);
-extern void write_pct_cols   (const PCTInfo &,          AsciiTable &, int, int);
-extern void write_pstd_cols  (const PCTInfo &,     int, AsciiTable &, int, int);
-extern void write_pjc_cols   (const PCTInfo &,          AsciiTable &, int, int);
-extern void write_prc_cols   (const PCTInfo &,          AsciiTable &, int, int);
-extern void write_eclv_cols  (const TTContingencyTable &, const NumArray &, AsciiTable &, int, int);
-extern void write_nbrctc_cols(const NBRCTSInfo &,       AsciiTable &, int, int);
-extern void write_nbrcts_cols(const NBRCTSInfo &,  int, AsciiTable &, int, int);
-extern void write_nbrcnt_cols(const NBRCNTInfo &,  int, AsciiTable &, int, int);
-extern void write_nbrcnt_cols(const NBRCNTInfo &,  int, AsciiTable &, int, int);
-extern void write_grad_cols  (const GRADInfo &,         AsciiTable &, int, int);
-extern void write_dmap_cols  (const DMAPInfo &,         AsciiTable &, int, int);
-extern void write_mpr_cols   (const PairDataPoint *,    int, AsciiTable &, int, int);
-extern void write_isc_cols   (const ISCInfo &,     int, AsciiTable &, int, int);
-extern void write_ecnt_cols  (const PairDataEnsemble *,      AsciiTable &, int, int);
-extern void write_rhist_cols (const PairDataEnsemble *,      AsciiTable &, int, int);
-extern void write_phist_cols (const PairDataEnsemble *,      AsciiTable &, int, int);
-extern void write_orank_cols (const PairDataEnsemble *, int, AsciiTable &, int, int);
-extern void write_ssvar_cols (const PairDataEnsemble *, int, double, AsciiTable &, int, int);
-extern void write_relp_cols  (const PairDataEnsemble *,      AsciiTable &, int, int);
+extern void write_fho_cols   (const CTSInfo &,
+                              AsciiTable &, int, int);
+extern void write_ctc_cols   (const CTSInfo &,
+                              AsciiTable &, int, int);
+extern void write_cts_cols   (const CTSInfo &, int,
+                              AsciiTable &, int, int);
+extern void write_mctc_cols  (const MCTSInfo &,
+                              AsciiTable &, int, int);
+extern void write_mcts_cols  (const MCTSInfo &, int,
+                              AsciiTable &, int, int);
+extern void write_cnt_cols   (const CNTInfo &, int,
+                              AsciiTable &, int, int);
+extern void write_sl1l2_cols (const SL1L2Info &,
+                              AsciiTable &, int, int);
+extern void write_sal1l2_cols(const SL1L2Info &,
+                              AsciiTable &, int, int);
+extern void write_vl1l2_cols (const VL1L2Info &,
+                              AsciiTable &, int, int);
+extern void write_val1l2_cols(const VL1L2Info &,
+                              AsciiTable &, int, int);
+extern void write_vcnt_cols  (const VL1L2Info &,
+                              AsciiTable &, int, int);
+extern void write_pct_cols   (const PCTInfo &,
+                              AsciiTable &, int, int);
+extern void write_pstd_cols  (const PCTInfo &, int,
+                              AsciiTable &, int, int);
+extern void write_pjc_cols   (const PCTInfo &,
+                              AsciiTable &, int, int);
+extern void write_prc_cols   (const PCTInfo &,
+                              AsciiTable &, int, int);
+extern void write_eclv_cols  (const TTContingencyTable &, const NumArray &,
+                              AsciiTable &, int, int);
+extern void write_nbrctc_cols(const NBRCTSInfo &,
+                              AsciiTable &, int, int);
+extern void write_nbrcts_cols(const NBRCTSInfo &, int,
+                              AsciiTable &, int, int);
+extern void write_nbrcnt_cols(const NBRCNTInfo &, int,
+                              AsciiTable &, int, int);
+extern void write_nbrcnt_cols(const NBRCNTInfo &, int,
+                              AsciiTable &, int, int);
+extern void write_grad_cols  (const GRADInfo &,
+                              AsciiTable &, int, int);
+extern void write_dmap_cols  (const DMAPInfo &,
+                              AsciiTable &, int, int);
+extern void write_mpr_cols   (const PairDataPoint *, int,
+                              AsciiTable &, int, int);
+extern void write_isc_cols   (const ISCInfo &, int,
+                              AsciiTable &, int, int);
+extern void write_ecnt_cols  (const ECNTInfo &,
+                              AsciiTable &, int, int);
+extern void write_rhist_cols (const PairDataEnsemble *,
+                              AsciiTable &, int, int);
+extern void write_phist_cols (const PairDataEnsemble *,
+                              AsciiTable &, int, int);
+extern void write_orank_cols (const PairDataEnsemble *, int,
+                              AsciiTable &, int, int);
+extern void write_ssvar_cols (const PairDataEnsemble *, int, double,
+                              AsciiTable &, int, int);
+extern void write_relp_cols  (const PairDataEnsemble *,
+                              AsciiTable &, int, int);
 
 // Setup column justification for STAT AsciiTable objects
 extern void justify_stat_cols(AsciiTable &);
