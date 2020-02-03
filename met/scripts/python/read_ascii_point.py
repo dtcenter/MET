98c163267a259547a7719fb602a76cb92ad54643
John Halley-Gotway <johnhg@ucar.edu>
2020-02-03 13:53:44 -0700

    Fix scripts/python/Makefile.am.

--- met/scripts/python/read_ascii_point.py
+++ met/scripts/python/read_ascii_point.py
@@ -17,7 +17,17 @@ if len(sys.argv) == 2:
     try:
         # Print some output to verify that this script ran
         print("Input File:  " + repr(input_file))
-        point_data = pd.read_csv(input_file, header=None, delim_whitespace=True).values.tolist()
+        # TODO: make this work
+        #point_data = pd.read_csv(input_file, header=None, delim_whitespace=True).values.tolist()
+        
+        point_data = [[ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0,  7, 837.0,  1618, 'NA', 1618    ], 
+                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 11, 837.0,  1618, 'NA', 273.05  ], 
+                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 17, 837.0,  1618, 'NA', 271.85  ], 
+                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 52, 837.0,  1618, 'NA', 92      ], 
+                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 53, 837.0,  1618, 'NA', 0.00417 ], 
+                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0,  7, 826.0,  1724, 'NA', 1724    ], 
+                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 11, 826.0,  1724, 'NA', 274.55  ]]
+        
         print("Data Length: " + repr(len(point_data)))
         print("Data Type:   " + repr(type(point_data)))
     except NameError:
