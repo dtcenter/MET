d2883c877288f65e0612e0be3b53d08d42995641
John Halley-Gotway <johnhg@ucar.edu>
2020-02-04 15:55:32 -0700

    Define which of the 11-column ascii input files should be strings.

--- met/scripts/python/read_ascii_point.py
+++ met/scripts/python/read_ascii_point.py
@@ -19,17 +19,9 @@ if len(sys.argv) == 2:
     try:
         # Print some output to verify that this script ran
         print("Input File:\t" + repr(input_file))
-        # TODO: make this work
-        #point_data = pd.read_csv(input_file, header=None, delim_whitespace=True).values.tolist()
-        
-        point_data = [[ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0,  7, 837.0,  1618, 'NA', 1618    ], 
-                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 11, 837.0,  1618, 'NA', 273.05  ], 
-                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 17, 837.0,  1618, 'NA', 271.85  ], 
-                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 52, 837.0,  1618, 'NA', 92      ], 
-                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 53, 837.0,  1618, 'NA', 0.00417 ], 
-                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0,  7, 826.0,  1724, 'NA', 1724    ], 
-                      [ 'ADPUPA', '72365', '20070331_120000',   35.03, -106.62, 1618.0, 11, 826.0,  1724, 'NA', 274.55  ]]
-        
+        point_data = pd.read_csv(input_file, header=None, delim_whitespace=True,
+                          names=['typ', 'sid', 'vld', 'lat', 'lon', 'elv', 'var', 'lvl', 'hgt', 'qc', 'obs'],
+                          dtype={'typ':'str','sid':'str','var':'str','qc':'str'}).values.tolist()
         print("Data Length:\t" + repr(len(point_data)))
         print("Data Type:\t" + repr(type(point_data)))
     except NameError:
