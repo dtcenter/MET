f941bdf8483a5ba2de686e507aa1d7fa4aa0601a
John Halley Gotway <johnhg@ucar.edu>
2020-11-18 11:07:50 -0700

    Per #1572, list the met-9.1.1 release date as 20201119 for the docs.

--- met/docs/conf.py
+++ met/docs/conf.py
@@ -24,7 +24,7 @@ version = '9.1.1'
 verinfo = version
 release = f'{version}'
 release_year = '2020'
-release_date = f'{release_year}0810'
+release_date = f'{release_year}1119'
 copyright = f'{release_year}, {author}'
 
 # -- General configuration ---------------------------------------------------
