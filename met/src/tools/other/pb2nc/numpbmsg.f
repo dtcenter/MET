12b0de0424be51560301e2f855e38c5278e61652
hsoh-u <hsoh@ucar.edu>
2021-01-19 09:49:36 -0700

    Bugfix 1618 develop pb2nc (#1623)
    Co-authored-by: Howard Soh <hsoh@kiowa.rap.ucar.edu>

--- met/src/tools/other/pb2nc/numpbmsg.f
+++ met/src/tools/other/pb2nc/numpbmsg.f
@@ -24,7 +24,29 @@ C*
 C*
         END
 
-        
+
+        SUBROUTINE NUMPBMSG_NEW ( PBFILE, FID, NMSG )
+C*
+        INCLUDE    'readpb.prm'
+C*
+        CHARACTER  PBFILE*(FILEMXSTRL)
+        INTEGER    FID, FID2
+        INTEGER    NMSG
+        REAL*8     R8ARR(1, 1)
+C*
+C-----------------------------------------------------------------------
+C*
+C*      Call UFBTAB to figure out how many messages the PrepBufr file
+C*      attached to FID contains
+C*
+        OPEN  ( UNIT = FID, FILE = PBFILE, FORM = 'UNFORMATTED' )
+        FID2 = -FID
+        CALL UFBTAB  ( FID2, R8ARR, 1, 1, NMSG, ' ' )
+        CLOSE ( UNIT = FID )
+C*
+        END
+
+
         SUBROUTINE GET_TMIN ( FID, TMIN )
 C*
         INTEGER    FID, TMIN
