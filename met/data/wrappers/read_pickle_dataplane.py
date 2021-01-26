6d46603cad053d426e22cf3927b7358892ec06aa
David Fillmore <fillmore.winslow.david@gmail.com>
2021-01-26 09:46:16 -0700

    Start on read of netcdf as pickle alternative.

--- met/data/wrappers/read_pickle_dataplane.py
+++ met/data/wrappers/read_pickle_dataplane.py
@@ -9,7 +9,16 @@
 import sys
 import numpy as np
 import pickle
+import netCDF4 as nc
 
 print('Python Script:\t', sys.argv[0])
 print('Load Pickle:\t', sys.argv[1])
 met_info = pickle.load(open(sys.argv[1], "rb"))
+
+netcdf_filename = sys.argv[1] + '.nc4'
+print('Read NetCDF:\t',  netcdf_filename)
+
+# read NetCDF file
+ds = nc.Dataset(netcdf_filename, 'r')
+met_data = ds['met_data'][:]
+met_info['met_data'] = met_data
