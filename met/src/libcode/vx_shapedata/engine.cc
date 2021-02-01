0b37b5f818b9210bcf0fcbb8d279b2e02ba04838
John Halley Gotway <johnhg@ucar.edu>
2021-02-01 14:41:58 -0700

    Per #1643, redefine the contents of the existing AREA_RATIO output column from MODE. Define it as FCST/OBS object area instead of min/max. Update the User's Guide to note the change and also clarify that the MTD VOLUME_RATIO output really is FCST/OBS.

--- met/src/libcode/vx_shapedata/engine.cc
+++ met/src/libcode/vx_shapedata/engine.cc
@@ -3157,8 +3157,8 @@ void write_pair(ModeFuzzyEngine &eng, const Grid & grid, const int n_f, const in
    // Difference in aspect ratio
    at.set_entry(row, c++, eng.pair_single[n].aspect_diff);
 
-   // Area ratio
-   at.set_entry(row, c++, eng.pair_single[n].area_ratio);
+   // Fcst/Obs area ratio
+   at.set_entry(row, c++, eng.pair_single[n].fo_area_ratio);
 
    // Intersection area
    at.set_entry(row, c++,
@@ -3460,8 +3460,8 @@ void write_cluster_pair(ModeFuzzyEngine &eng, const Grid & grid, const int n,
    // Difference in aspect ratio
    at.set_entry(row, c++, eng.pair_cluster[n].aspect_diff);
 
-   // Area ratio
-   at.set_entry(row, c++, eng.pair_cluster[n].area_ratio);
+   // Fcst/Obs area ratio
+   at.set_entry(row, c++, eng.pair_cluster[n].fo_area_ratio);
 
    // Intersection area
    at.set_entry(row, c++,
