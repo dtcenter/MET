1e6eb9ee260658c2e2d0098ce93d91f193698f7f
David Fillmore <fillmore.winslow.david@gmail.com>
2021-02-03 19:20:17 -0700

    Convert grid.nx and grid.ny to int.

--- met/data/wrappers/read_pickle_dataplane.py
+++ met/data/wrappers/read_pickle_dataplane.py
@@ -13,7 +13,8 @@ import netCDF4 as nc
 
 print('Python Script:\t', sys.argv[0])
 print('Load Pickle:\t', sys.argv[1])
-met_info = pickle.load(open(sys.argv[1], "rb"))
+# met_info = pickle.load(open(sys.argv[1], "rb"))
+met_info = {}
 
 netcdf_filename = sys.argv[1] + '.nc4'
 print('Read NetCDF:\t',  netcdf_filename)
@@ -21,11 +22,16 @@ print('Read NetCDF:\t',  netcdf_filename)
 # read NetCDF file
 ds = nc.Dataset(netcdf_filename, 'r')
 met_data = ds['met_data'][:]
+met_attrs = {}
 grid = {}
 for attr, attr_val in ds.__dict__.items():
-    print(attr, attr_val)
     if 'grid' in attr:
         grid_attr = attr.split('.')[1]
         grid[grid_attr] = attr_val
-print(grid)
+    else:
+        met_attrs[attr] = attr_val
+grid['nx'], grid['ny'] = int(grid['nx']), int(grid['ny'])
+met_attrs['grid'] = grid
 met_info['met_data'] = met_data
+met_info['attrs'] = met_attrs
+print(met_info)
