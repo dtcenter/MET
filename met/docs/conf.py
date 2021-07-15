131564f8fe5afb31a6860339a8a98d8488198d08
Keith Searight <keith.searight@noaa.gov>
2021-07-15 17:11:55 -0600

    Remove sphinx.ext.imgmath in conf.py

--- met/docs/conf.py
+++ met/docs/conf.py
@@ -32,7 +32,7 @@ copyright = f'{release_year}, {author}'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx', 'sphinx.ext.imgmath']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx']
 
 # settings for ReadTheDocs PDF creation
 latex_engine = 'pdflatex'
