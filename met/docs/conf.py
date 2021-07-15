31acbe8b09c0870aa23ac6be88840b789fe03bb6
Keith Searight <keith.searight@noaa.gov>
2021-07-15 16:28:18 -0600

    Add sphinx.ext.pngmath to conf.py

--- met/docs/conf.py
+++ met/docs/conf.py
@@ -32,7 +32,7 @@ copyright = f'{release_year}, {author}'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx, sphinx.ext.pngmath']
 
 # settings for ReadTheDocs PDF creation
 latex_engine = 'pdflatex'
