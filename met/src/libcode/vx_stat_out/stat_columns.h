3c9b2e41da9a001f864efdcce5ad2a5454f4d743
John Halley Gotway <johnhg@ucar.edu>
2021-08-02 16:54:08 -0600

    Per #1788, write the SSIDX columns. However we don't need to function to write the whole row since it's only written by Stat-Analysis.

--- met/src/libcode/vx_stat_out/stat_columns.h
+++ met/src/libcode/vx_stat_out/stat_columns.h
@@ -120,8 +120,6 @@ extern void write_ssvar_row (StatHdrColumns &, const PairDataEnsemble *, double,
                              AsciiTable &, int &, AsciiTable &, int &);
 extern void write_relp_row  (StatHdrColumns &, const PairDataEnsemble *, STATOutputType,
                              AsciiTable &, int &, AsciiTable &, int &);
-extern void write_ssidx_row (StatHdrColumns &, double, STATOutputType,
-                             AsciiTable &, int &, AsciiTable &, int &);
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -191,7 +189,7 @@ extern void write_ssvar_cols (const PairDataEnsemble *, int, double,
                               AsciiTable &, int, int);
 extern void write_relp_cols  (const PairDataEnsemble *,
                               AsciiTable &, int, int);
-extern void write_ssidx_cols (double,
+extern void write_ssidx_cols (const SSIndexInfo,
                               AsciiTable &, int, int);
 
 // Setup column justification for STAT AsciiTable objects
