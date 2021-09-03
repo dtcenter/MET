7473c302c5fac9746ad023702aa71cd8307c5943
John Halley Gotway <johnhg@ucar.edu>
2021-09-03 14:12:10 -0600

    Per #1823, no actual code changes. Just making code header formatting consistent. This issue will require modifying these classes to wrap global grids.

--- met/src/basic/vx_util/GridTemplate.h
+++ met/src/basic/vx_util/GridTemplate.h
@@ -1,59 +1,37 @@
-// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-// ** Copyright UCAR (c) 1990 - 2021
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+// ** Copyright UCAR (c) 1992 - 2021
 // ** University Corporation for Atmospheric Research (UCAR)
 // ** National Center for Atmospheric Research (NCAR)
-// ** Boulder, Colorado, USA
-// ** BSD licence applies - redistribution and use in source and binary
-// ** forms, with or without modification, are permitted provided that
-// ** the following conditions are met:
-// ** 1) If the software is modified to produce derivative works,
-// ** such modified software should be clearly marked, so as not
-// ** to confuse it with the version available from UCAR.
-// ** 2) Redistributions of source code must retain the above copyright
-// ** notice, this list of conditions and the following disclaimer.
-// ** 3) Redistributions in binary form must reproduce the above copyright
-// ** notice, this list of conditions and the following disclaimer in the
-// ** documentation and/or other materials provided with the distribution.
-// ** 4) Neither the name of UCAR nor the names of its contributors,
-// ** if any, may be used to endorse or promote products derived from
-// ** this software without specific prior written permission.
-// ** DISCLAIMER: THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS
-// ** OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-// ** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+// ** Research Applications Lab (RAL)
+// ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
 
-/* RCS info
- *   $Author: dixon $
- *   $Locker:  $
- *   $Date: 2016/03/03 19:21:31 $
- *   $Id: GridTemplate.hh,v 1.9 2016/03/03 19:21:31 dixon Exp $
- *   $Revision: 1.9 $
- *   $State: Exp $
- */
+///////////////////////////////////////////////////////////////////////////////
+//
+//   Filename:   GridTemplate.h
+//
+//   Description:
+//      Class implementing a template to be applied to
+//      gridded data.
+//
+//   Mod#   Date      Name            Description
+//   ----   ----      ----            -----------
+//   000    01-01-99  Rehak           Initial version.
+//
+///////////////////////////////////////////////////////////////////////////////
 
-/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
+#ifndef __GRID_TEMPLATE_H__
+#define __GRID_TEMPLATE_H__
 
-/************************************************************************
- * GridTemplate.hh: class implementing a template to be applied to
- *                  gridded data.
- *
- * RAP, NCAR, Boulder CO
- *
- * January 1999
- *
- * Nancy Rehak
- *
- ************************************************************************/
-
-#ifndef GridTemplate_HH
-#define GridTemplate_HH
+///////////////////////////////////////////////////////////////////////////////
 
 #include <vector>
 #include <cstdio>
 #include <memory>
 #include "GridOffset.h"
 #include "GridPoint.h"
+
+///////////////////////////////////////////////////////////////////////////////
 
 using namespace std;
 
@@ -175,6 +153,8 @@ class GridTemplate
 
 };
 
+///////////////////////////////////////////////////////////////////////////////
+
 // The factory knows about child classes and can create them for you
 
 class GridTemplateFactory {
@@ -195,7 +175,6 @@ class GridTemplateFactory {
       GridTemplate_None,
       GridTemplate_Square,
       GridTemplate_Circle,
-      //GridTemplate_Rectangle,
       GridTemplate_NUM_TEMPLATES
    };
 
@@ -218,14 +197,10 @@ class GridTemplateFactory {
 // Caller assumes ownership of the returned pointer.
    GridTemplate* buildGT(GridTemplates gt, int width);
 
-   //FUTURE DEV NOTE:
-   // If we ever decide to support rectangles or elipses, we are going to
-   // need to modify these methods to take more than just a single width & shape
-   // to define the grid template.  I suggest building a structure/union that holds
-   // all the defining characteristics of a grid template (shape, height, width, radius, etc.)
-   // and passing that around everywhere that we currently pass around a shape & width.
-
-
 };
 
-#endif
+///////////////////////////////////////////////////////////////////////////////
+
+#endif   //  __GRID_TEMPLATE_H__
+
+///////////////////////////////////////////////////////////////////////////////
