f7f76ca922f038a794520f1a687fc2198e5e6cea
Seth Linden <linden@kiowa.rap.ucar.edu>
2021-09-08 15:08:15 -0600

    In add_point_obs() added if block to apply obs_qty_exc_filt. SL

--- met/src/libcode/vx_statistics/pair_data_point.cc
+++ met/src/libcode/vx_statistics/pair_data_point.cc
@@ -913,6 +913,7 @@ void VxPairDataPoint::add_point_obs(float *hdr_arr, const char *hdr_typ_str,
 
    // Check whether the GRIB code for the observation matches
    // the specified code
+   //cout << "var_name = " << var_name << endl;
    if((var_name != 0) && (0 < strlen(var_name))) {
       if(var_name != obs_info->name()) {
          rej_var++;
@@ -924,14 +925,32 @@ void VxPairDataPoint::add_point_obs(float *hdr_arr, const char *hdr_typ_str,
       return;
    }
 
-   // Check if the observation quality flag is included in the list
+   cout << "CHECK before applying quality include and exclude flags" << endl;
+   
+   // Check if the observation quality include flag is in the list
    if(obs_qty_inc_filt.n() && strcmp(obs_qty, "")) {
       bool qty_match = false;
       for(i=0; i<obs_qty_inc_filt.n() && !qty_match; i++) {
-         if(obs_qty == obs_qty_inc_filt[i]) qty_match = true;
+        cout << "obs_qty_inc_filt[i] = " << obs_qty_inc_filt[i] << endl;
+        if(obs_qty == obs_qty_inc_filt[i]) qty_match = true;
       }
       if(!qty_match) {
          rej_qty++;
+         cout << "CHECK2 before qty_inc return" << endl;
+         return;
+      }
+   }
+   
+   // Check if the observation quality exclude flag is in the list
+   if(obs_qty_exc_filt.n() && strcmp(obs_qty, "")) {
+      bool qty_match = false;
+      for(i=0; i<obs_qty_exc_filt.n() && !qty_match; i++) {
+        cout << "obs_qty_exc_filt[i] = " << obs_qty_exc_filt[i] << endl;
+        if(obs_qty != obs_qty_exc_filt[i]) qty_match = true;
+      }
+      if(!qty_match) {
+         rej_qty++;
+         cout << "CHECK2 before qty_exc return" << endl;
          return;
       }
    }
