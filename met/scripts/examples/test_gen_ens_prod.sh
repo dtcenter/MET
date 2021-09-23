ec188ea64dc1f5eaff019912618ed1e326129ade
John Halley Gotway <johnhg@ucar.edu>
2021-09-23 13:50:10 -0600

    Per #1904, fix command line in example to use -out instead of -outdir.

--- met/scripts/examples/test_gen_ens_prod.sh
+++ met/scripts/examples/test_gen_ens_prod.sh
@@ -5,4 +5,4 @@ echo "*** Running Gen-Ens-Prod using GRIB forecasts ***"
 gen_ens_prod \
    -ens ../data/sample_fcst/2009123112/*gep*/d01_2009123112_02400.grib \
    -config config/GenEnsProdConfig \
-   -outdir ${TEST_OUT_DIR}/gen_ens_prod/gen_ens_prod_20100101_120000V_ens.nc -v 2
+   -out ${TEST_OUT_DIR}/gen_ens_prod/gen_ens_prod_20100101_120000V_ens.nc -v 2
