e0257c6277bb28a924a2b235c1d20fb6088c388c
Maff Glover <matthew.glover@metoffice.gov.uk>
2021-11-05 14:22:43 +0000

    Feature 1926. Revert temporary C++17 temporary file code tweak.

--- met/src/basic/vx_config/temp_file.cc
+++ met/src/basic/vx_config/temp_file.cc
@@ -65,10 +65,12 @@ ConcatString make_temp_file_name(const char *prefix, const char *suffix) {
 ////////////////////////////////////////////////////////////////////////
 
 void remove_temp_file(const ConcatString file_name) {
+   int errno;
+
    //
    // Attempt to remove the file and print out any error message
    //
-   if(!remove(file_name.c_str())) {
+   if((errno = remove(file_name.c_str())) != 0) {
       mlog << Error << "\nremove_temp_file() -> "
            << "can't delete temporary file: \""
            << file_name << "\" ("
