fa61672e145c747195d281fe7ed533c41f0bf2b2
johnhg <johnhg@ucar.edu>
2021-12-13 19:10:56 -0700

    Update develop-ref after #1990, #1992, and #1994 (#1995)
    Co-authored-by: Julie Prestopnik <jpresto@seneca.rap.ucar.edu>
    Co-authored-by: johnhg <johnhg@ucar.edu>
    Co-authored-by: Seth Linden <linden@kiowa.rap.ucar.edu>
    Co-authored-by: John Halley Gotway <johnhg@ucar.edu>
    Co-authored-by: j-opatz <59586397+j-opatz@users.noreply.github.com>
    Co-authored-by: Howard Soh <hsoh@kiowa.rap.ucar.edu>
    Co-authored-by: jprestop <jpresto@ucar.edu>
    Co-authored-by: Seth Linden <linden@ucar.edu>
    Co-authored-by: hsoh-u <hsoh@ucar.edu>

--- met/src/basic/vx_util/get_filenames.cc
+++ met/src/basic/vx_util/get_filenames.cc
@@ -112,10 +112,13 @@ struct stat sbuf;
 if ( S_ISDIR(sbuf.st_mode) )  {
 
    //
-   //  process directory
+   //  get filenames from the directory and sort them
+   //  to make the order consistent across platforms
    //
 
    b = get_filenames_from_dir(search_dir.c_str(), prefix, suffix);
+
+   b.sort();
 
    a.add(b);
 
@@ -198,9 +201,16 @@ while ( (entry = readdir(directory)) != NULL )  {
 
    }
 
+   //
+   //  get filenames from the directory and sort them
+   //  to make the order consistent across platforms
+   //
+
    if ( S_ISDIR(sbuf.st_mode) )  {
 
       b = get_filenames_from_dir(entry_path, prefix, suffix);
+
+      b.sort();
 
       a.add(b);
 
