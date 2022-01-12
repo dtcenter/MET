b26788310d7ce3ff98d1c1063742fe898b13ac86
John Halley Gotway <johnhg@ucar.edu>
2022-01-12 12:41:58 -0700

    Fixup the Rscript diffing logic to support running the nightly build on seneca. The problem here is that the ncdiff -x -v command line options that worked in version 4.7.0 NO LONGER work in 4.9.2. Added a check to skip over the time_bounds variable when processing the ncdiff output.

--- test/R_test/test_util.R
+++ test/R_test/test_util.R
@@ -753,6 +753,11 @@ compareNc = function(nc1, nc2, verb, strict=0, delta=-1, comp_var=0){
 	# for each variable present in the file, check for differences
 	for(strVar in names(ncFileD$var)){
 
+		# Skip the time_bounds variable
+		# Note: Running "ncdiff -x -v time_bounds" successfully excludes that variable
+	        #       in version 4.7.0. However, it fails in version 4.9.2. 	
+		if (strVar == "time_bounds"){ next; }
+
 		# check the variable attributes for differences
 		listAtt1 = ncatt_get(ncFile1, varid=strVar);
 		listAtt1Nam = names(listAtt1);
