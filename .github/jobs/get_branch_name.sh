41d4040c71854bd1e88a8fc3a96fad8f741b4680
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-20 13:55:16 -0700

    Per #1546, added function to get branch name depending on if event is pull request or push. Set unit test workflow to trigger on pull requests into develop branch

---
+++ .github/jobs/get_branch_name.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# If pull request, use GitHub head ref and add -PR to end
+# Otherwise use GitHub ref
+
+if [ "${GITHUB_EVENT_NAME}" == "pull_request" ] ; then
+  branch_name=${GITHUB_HEAD_REF}-PR
+else
+  branch_name=${GITHUB_REF}
+fi
+
+branch_name=${branch_name#"refs/heads/"}
+
+echo ::set-output name=branch_name::$branch_name
