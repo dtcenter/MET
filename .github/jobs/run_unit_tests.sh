e85c79ce23b5b529c5b22eebbbfc1e11c95f94bd
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-20 06:54:11 -0700

    Per #1546, added error checking and reporting to unit test script

--- .github/jobs/run_unit_tests.sh
+++ .github/jobs/run_unit_tests.sh
@@ -24,7 +24,18 @@ export MET_TEST_MET_PYTHON_EXE=/usr/bin/python3
 echo "Running all MET unit tests..."
 echo "Writing logs to /met/unit_test.log"
 ${MET_TEST_BASE}/bin/unit_test.sh > /met/unit_test.log
+if [ $? != 0 ]; then
+    echo "ERROR: Unit tests failed"
+    exit 1
+fi
 
-echo "Running comparison"
+
+echo "Running comparison on test output"
 echo "Writing logs to /met/comp_dir.log"
 ${MET_TEST_BASE}/bin/comp_dir.sh ${MET_TEST_TRUTH} ${MET_TEST_OUTPUT} > /met/comp_dir.log
+if [ $? != 0 ]; then
+    echo "ERROR: Test output comparison failed"
+    exit 1
+fi
+
+echo "Success"
