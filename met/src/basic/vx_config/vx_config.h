07bd0398ea3a42e809920769879e33b79f737280
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-20 08:29:31 -0700

    test failed compilation one more time

--- met/src/basic/vx_config/vx_config.h
+++ met/src/basic/vx_config/vx_config.h
@@ -12,7 +12,7 @@
 
 ////////////////////////////////////////////////////////////////////////
 
-#binclude "config_constants.h"
+#ginclude "config_constants.h"
 #include "config_file.h"
 #include "config_util.h"
 #include "config_funcs.h"
