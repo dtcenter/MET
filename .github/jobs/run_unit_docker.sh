286c00d34d1e7dcca34a8c49150724d3dcedc126
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-20 11:37:17 -0700

    fixed command run inside docker

--- .github/jobs/run_unit_docker.sh
+++ .github/jobs/run_unit_docker.sh
@@ -9,5 +9,5 @@ volumes_from="--volumes-from met_input --volumes-from met_truth"
 mount_args="-v ${RUNNER_WORKSPACE}/diff:/data/output/met_data_diff -v ${RUNNER_WORKSPACE}/logs:/met/logs"
 
 # run unit test script inside Docker, mount MET input and truth data
-cmd="${MET_REPO_DIR}/.github/jobs/run_unit_tests.sh"
-time_command docker run ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c "${cmd}"
+cmd="\${MET_REPO_DIR}/.github/jobs/run_unit_tests.sh"
+time_command docker run ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
