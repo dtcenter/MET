3c553897d6616d390e7874a363306bee5e1442d2
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-20 07:13:05 -0700

    Per #1546, changed build docker script to use Dockerfile.copy to use local source code, set environment variables that set docker build args in workflow yml

--- .github/jobs/build_docker_image.sh
+++ .github/jobs/build_docker_image.sh
@@ -2,20 +2,13 @@
 
 source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 
-  local duration=$(( SECONDS - start_seconds ))
-  echo "TIMING: Command took `printf '%02d' $(($duration / 60))`:`printf '%02d' $(($duration % 60))` (MM:SS): '$*'"
-  if [ ${error} -ne 0 ]; then
-    echo "ERROR: '$*' exited with status = ${error}"
-  fi
-  return $error
-}
+#prefix=refs/heads/
+#branch_name=${GITHUB_REF#"$prefix"}
+DOCKERHUB_TAG=dtcenter/met:${SOURCE_BRANCH}
 
-prefix=refs/heads/
-branch_name=${GITHUB_REF#"$prefix"}
-DOCKERHUB_TAG=dtcenter/met:${branch_name}
-
-DOCKERFILE_DIR=${GITHUB_WORKSPACE}/scripts/docker
+DOCKERFILE_PATH=${GITHUB_WORKSPACE}/scripts/docker/Dockerfile.copy
 
 time_command docker build -t ${DOCKERHUB_TAG} \
-       --build-arg SOURCE_BRANCH=$branch_name \
-       $DOCKERFILE_DIR
+    --build-arg SOURCE_BRANCH \
+    --build-arg MET_BASE_IMAGE \
+    -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}
