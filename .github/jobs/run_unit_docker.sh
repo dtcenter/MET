7a592a97b5e7fb63dbe6182f57f26d9268deaa7a
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-21 09:59:40 -0700

    Per #1546, fixed incorrect directory to mount diff output

--- .github/jobs/run_unit_docker.sh
+++ .github/jobs/run_unit_docker.sh
@@ -6,7 +6,7 @@ DOCKERHUB_TAG=dtcenter/met:${SOURCE_BRANCH}
 
 volumes_from="--volumes-from met_input --volumes-from met_truth"
 
-mount_args="-v ${RUNNER_WORKSPACE}/diff:/data/output/met_data_diff -v ${RUNNER_WORKSPACE}/logs:/met/logs"
+mount_args="-v ${RUNNER_WORKSPACE}/diff:/data/output/met_test_diff -v ${RUNNER_WORKSPACE}/logs:/met/logs"
 
 # run unit test script inside Docker, mount MET input and truth data
 cmd="\${MET_REPO_DIR}/.github/jobs/run_unit_tests.sh"
