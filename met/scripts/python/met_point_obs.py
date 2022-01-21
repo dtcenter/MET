b8e0de3ce3a208d8fbded5e912f7c60c7ca6726c
Howard Soh <hsoh@kiowa.rap.ucar.edu>
2022-01-21 11:31:48 -0700

    #1844 Make met_point_obs as abstract class

--- met/scripts/python/met_point_obs.py
+++ met/scripts/python/met_point_obs.py
@@ -2,26 +2,39 @@
 Created on Nov 10, 2021
 
 @author: hsoh
+
+- This is the base class and the customized script should extend the met_point_obs.
+- The customized script (for example "custom_reader") must implement "def read_data(self, args)"
+  which fills the array (list) variables at __init__().
+- The args can be 1) single string argument, 2) the list of arguments, and 3) the dictionary of arguments.
+- The variable "met_point_data" must be set for MET tools
+- The customized script is expected to include following codes:
+
+    # prepare arguments for the customized script
+    args = {'input', sys.argv[1]}   # or args = []
+    point_obs_data = custom_reader()
+    point_obs_data.read_data()
+    met_point_data = point_obs_data.get_point_data()
 '''
 
-
+from abc import ABC, abstractmethod
 import numpy as np
 
 COUNT_SHOW = 30
 
 MET_PYTHON_OBS_ARGS = "MET_POINT_PYTHON_ARGS"
 
-class met_point_obs(object):
+class met_point_obs(ABC):
     '''
     classdocs
     '''
+
     python_prefix = 'PYTHON_POINT_RAW'
 
     def __init__(self, use_var_id=True):
         '''
         Constructor
         '''
-        self.raw_data = None
         self.use_var_id = use_var_id    # True if variable index, False if GRIB code
 
         # Header
@@ -43,7 +56,7 @@ class met_point_obs(object):
         self.hdr_irpt_typ  = [] # optional
         self.hdr_inst_typ  = [] # optional
 
-        #Observation data        
+        #Observation data
         self.nobs = 0
         self.nobs_qty = 0
         self.nobs_var = 0
@@ -56,9 +69,11 @@ class met_point_obs(object):
         self.obs_qty_table = []  # (nobs_qty, mxstr)
         self.obs_var_table = []  # (nobs_var, mxstr2) optional if GRIB code at self.obs_vid
 
-    #def convert(self):
-    #    pass
-    
+    @abstractmethod
+    def read_data(self, args):
+        # args should be list or dictionary
+        pass
+
     def get_point_data(self):
         if self.nhdr <= 0:
             self.nhdr = len(self.hdr_lat)
@@ -81,11 +96,11 @@ class met_point_obs(object):
     @staticmethod
     def is_python_script(arg_value):
         return arg_value.startswith(met_point_obs.python_prefix)
-    
+
     @staticmethod
     def get_python_script(arg_value):
         return arg_value[len(met_point_obs.python_prefix)+1:]
-    
+
     @staticmethod
     def print_data(key, data_array, show_count=COUNT_SHOW):
         if isinstance(data_array, list):
@@ -102,68 +117,73 @@ class met_point_obs(object):
             print("      {k:10s}: {v}".format(k=key, v= data_array))
 
     @staticmethod
-    def print_point_data(met_point_data):
+    def print_point_data(met_point_data, print_subset=True):
         print(' === MET point data by python embedding ===')
-        met_point_obs.print_data('nhdr', met_point_data['nhdr'])
-        met_point_obs.print_data('nobs' , met_point_data['nobs'])
-        met_point_obs.print_data('use_var_id',met_point_data['use_var_id'])
-        met_point_obs.print_data('hdr_typ',met_point_data['hdr_typ'])
-        met_point_obs.print_data('obs_hid',met_point_data['obs_hid'])
-        met_point_obs.print_data('obs_vid',met_point_data['obs_vid'])
-        met_point_obs.print_data('obs_val',met_point_data['obs_val'])
-        met_point_obs.print_data('obs_var_table',met_point_data['obs_var_table'])
-        met_point_obs.print_data('obs_qty_table',met_point_data['obs_qty_table'])
-        met_point_obs.print_data('hdr_typ_table',met_point_data['hdr_typ_table'])
-        met_point_obs.print_data('hdr_sid_table',met_point_data['hdr_sid_table'])
-        met_point_obs.print_data('hdr_vld_table',met_point_data['hdr_vld_table'])
+        if print_subset:
+            met_point_obs.print_data('nhdr', met_point_data['nhdr'])
+            met_point_obs.print_data('nobs' , met_point_data['nobs'])
+            met_point_obs.print_data('use_var_id',met_point_data['use_var_id'])
+            met_point_obs.print_data('hdr_typ',met_point_data['hdr_typ'])
+            met_point_obs.print_data('obs_hid',met_point_data['obs_hid'])
+            met_point_obs.print_data('obs_vid',met_point_data['obs_vid'])
+            met_point_obs.print_data('obs_val',met_point_data['obs_val'])
+            met_point_obs.print_data('obs_var_table',met_point_data['obs_var_table'])
+            met_point_obs.print_data('obs_qty_table',met_point_data['obs_qty_table'])
+            met_point_obs.print_data('hdr_typ_table',met_point_data['hdr_typ_table'])
+            met_point_obs.print_data('hdr_sid_table',met_point_data['hdr_sid_table'])
+            met_point_obs.print_data('hdr_vld_table',met_point_data['hdr_vld_table'])
+        else:
+            print('All',met_point_data)
+            print("      nhdr: ",met_point_data['nhdr'])
+            print("      nobs: ",met_point_data['nobs'])
+            print('use_var_id: ',met_point_data['use_var_id'])
+            print('hdr_typ: ',met_point_data['hdr_typ'])
+            print('obs_vid: ',met_point_data['obs_vid'])
+            print('obs_val: ',met_point_data['obs_val'])
+            print('obs_var_table: ',met_point_data['obs_var_table'])
+            print('obs_qty_table: ',met_point_data['obs_qty_table'])
+            print('hdr_typ_table: ',met_point_data['hdr_typ_table'])
+            print('hdr_sid_table: ',met_point_data['hdr_sid_table'])
+            print('hdr_vld_table: ',met_point_data['hdr_vld_table'])
         print(' === MET point data by python embedding ===')
 
 
-def get_sample_point_obs():
-    point_obs_data = met_point_obs()
-    point_obs_data.hdr_typ = np.array([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])
-    point_obs_data.hdr_sid = np.array([ 0, 0, 0, 0, 0, 1, 2, 3, 3, 1, 2, 2, 3, 0, 0, 0, 0, 0, 1, 2, 3, 3, 1, 2, 2, 3 ])
-    point_obs_data.hdr_vld = np.array([ 0, 1, 2, 3, 4, 4, 3, 4, 3, 4, 5, 4, 3, 0, 1, 2, 3, 4, 4, 3, 4, 3, 4, 5, 4, 3 ])
-    point_obs_data.hdr_lat = np.array([ 43., 43., 43., 43., 43., 43., 43., 43., 43., 46., 46., 46., 46., 43., 43., 43., 43., 43., 43., 43., 43., 43., 46., 46., 46., 46. ])
-    point_obs_data.hdr_lon = np.array([ -89., -89., -89., -89., -89., -89., -89., -89., -89., -92., -92., -92., -92., -89., -89., -89., -89., -89., -89., -89., -89., -89., -92., -92., -92., -92. ])
-    point_obs_data.hdr_elv = np.array([ 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220. ])
+# This is a sample drived class
+class sample_met_point_obs(met_point_obs):
 
-    point_obs_data.obs_qid = np.array([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])
-    point_obs_data.obs_hid = np.array([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 25 ])
-    point_obs_data.obs_vid = np.array([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ])
-    point_obs_data.obs_lvl = np.array([ 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000. ])
-    point_obs_data.obs_hgt = np.array([ 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2. ])
-    point_obs_data.obs_val = np.array([ 292., 292.5, 293., 293.5, 294., 294.5, 295., 295.5, 296., 292., 293.4, 293., 296., 294., 92., 92.5, 93., 93.5, 94., 94.5, 95., 95.5, 96., 92., 93.4, 93., 96., 94. ])
+    #@abstractmethod
+    def read_data(self, arg_map={}):
+        self.hdr_typ = np.array([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])
+        self.hdr_sid = np.array([ 0, 0, 0, 0, 0, 1, 2, 3, 3, 1, 2, 2, 3, 0, 0, 0, 0, 0, 1, 2, 3, 3, 1, 2, 2, 3 ])
+        self.hdr_vld = np.array([ 0, 1, 2, 3, 4, 4, 3, 4, 3, 4, 5, 4, 3, 0, 1, 2, 3, 4, 4, 3, 4, 3, 4, 5, 4, 3 ])
+        self.hdr_lat = np.array([ 43., 43., 43., 43., 43., 43., 43., 43., 43., 46., 46., 46., 46., 43., 43., 43., 43., 43., 43., 43., 43., 43., 46., 46., 46., 46. ])
+        self.hdr_lon = np.array([ -89., -89., -89., -89., -89., -89., -89., -89., -89., -92., -92., -92., -92., -89., -89., -89., -89., -89., -89., -89., -89., -89., -92., -92., -92., -92. ])
+        self.hdr_elv = np.array([ 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220., 220. ])
 
-    point_obs_data.hdr_typ_table = [ "ADPSFC" ]
-    point_obs_data.hdr_sid_table = [ "001", "002", "003", "004" ]
-    point_obs_data.hdr_vld_table = [
-            "20120409_115000", "20120409_115500", "20120409_120100", "20120409_120500", "20120409_121000",
-            "20120409_120000" ]
-    point_obs_data.obs_var_table = [ "TMP", "RH" ]
-    point_obs_data.obs_qty_table = [ "NA" ]
-    point_obs_data.nhdr = len(point_obs_data.hdr_lat)
-    point_obs_data.nobs = len(point_obs_data.obs_val)
-    #point_obs_data.met_point_data = point_obs_data
-    return point_obs_data
+        self.obs_qid = np.array([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])
+        self.obs_hid = np.array([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 25 ])
+        self.obs_vid = np.array([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ])
+        self.obs_lvl = np.array([ 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000., 1000. ])
+        self.obs_hgt = np.array([ 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2. ])
+        self.obs_val = np.array([ 292., 292.5, 293., 293.5, 294., 294.5, 295., 295.5, 296., 292., 293.4, 293., 296., 294., 92., 92.5, 93., 93.5, 94., 94.5, 95., 95.5, 96., 92., 93.4, 93., 96., 94. ])
+
+        self.hdr_typ_table = [ "ADPSFC" ]
+        self.hdr_sid_table = [ "001", "002", "003", "004" ]
+        self.hdr_vld_table = [
+                "20120409_115000", "20120409_115500", "20120409_120100", "20120409_120500", "20120409_121000",
+                "20120409_120000" ]
+        self.obs_var_table = [ "TMP", "RH" ]
+        self.obs_qty_table = [ "NA" ]
 
 
 def main():
-    met_point_data = get_sample_point_obs().get_point_data()
-    
-    print('All',met_point_data)
-    print("      nhdr: ",met_point_data['nhdr'])
-    print("      nobs: ",met_point_data['nobs'])
-    print('use_var_id: ',met_point_data['use_var_id'])
-    print('hdr_typ: ',met_point_data['hdr_typ'])
-    print('obs_vid: ',met_point_data['obs_vid'])
-    print('obs_val: ',met_point_data['obs_val'])
-    print('obs_var_table: ',met_point_data['obs_var_table'])
-    print('obs_qty_table: ',met_point_data['obs_qty_table'])
-    print('hdr_typ_table: ',met_point_data['hdr_typ_table'])
-    print('hdr_sid_table: ',met_point_data['hdr_sid_table'])
-    print('hdr_vld_table: ',met_point_data['hdr_vld_table'])
-    print('Done python scripot')
+    args = {}   # or args = []
+    point_obs_data = sample_met_point_obs()
+    point_obs_data.read_data(args)
+    met_point_data = point_obs_data.get_point_data()
+
+    point_obs_data.print_point_data(met_point_data, print_subset=False)
 
 if __name__ == '__main__':
     main()
+    print('Done python scripot')
--- met/scripts/python/read_met_point_obs.py
+++ met/scripts/python/read_met_point_obs.py
@@ -2,6 +2,8 @@
 Created on Nov 10, 2021
 
 @author: hsoh
+
+This script reads the MET point observation NetCDF file like MET tools do.
 '''
 
 import os
@@ -10,35 +12,26 @@ import time
 import numpy as np
 import netCDF4 as nc
 
-from met_point_obs import met_point_obs, get_sample_point_obs, MET_PYTHON_OBS_ARGS
+from met_point_obs import met_point_obs, sample_met_point_obs
 
 DO_PRINT_DATA = False
 ARG_PRINT_DATA = 'show_data'
 
 class nc_point_obs(met_point_obs):
 
-    @staticmethod
-    def get_num_array(dataset, var_name):
-        nc_var = dataset.variables.get(var_name, None)
-        return nc_var[:].filled(nc_var._FillValue if '_FillValue' in nc_var.ncattrs() else -9999) if nc_var else []
-    
-    @staticmethod
-    def get_ncbyte_array_to_str(nc_var):
-        nc_str_data = nc_var[:]
-        if nc_var.datatype.name == 'bytes8':
-            nc_str_data = [ str(s.compressed(),"utf-8") for s in nc_var[:]]
-        return nc_str_data
-
-    @staticmethod
-    def get_string_array(dataset, var_name):
-        nc_var = dataset.variables.get(var_name, None)
-        return nc_point_obs.get_ncbyte_array_to_str(nc_var) if nc_var else []
-    
-    #def convert(self):
-    #    pass
-    
-    def read_nectdf(self, nc_filename):
-        if os.path.exists(nc_filename):
+    def read_data(self, args):
+        # args should be list or dictionaryu
+        if isinstance(args, dict):
+            nc_filename = args.get('nc_name',None)
+        elif isinstance(args, list):
+            nc_filename = args[0]
+        else:
+            nc_filename = args
+        if nc_filename is None:
+            print("==ERROR== The input NetCDF filename is missing")
+        elif not os.path.exists(nc_filename):
+            print("==ERROR== input NetCDF file ({f}) does not exist".format(f=nc_filename))
+        else:
             dataset = nc.Dataset(nc_filename, 'r')
             # Header
             self.hdr_typ = dataset['hdr_typ'][:]
@@ -47,10 +40,10 @@ class nc_point_obs(met_point_obs):
             self.hdr_lat = dataset['hdr_lat'][:]
             self.hdr_lon = dataset['hdr_lon'][:]
             self.hdr_elv = dataset['hdr_elv'][:]
-            self.hdr_typ_table = self.get_string_array(dataset, 'hdr_typ_table')
-            self.hdr_sid_table = self.get_string_array(dataset, 'hdr_sid_table')
-            self.hdr_vld_table = self.get_string_array(dataset, 'hdr_vld_table')
-            
+            self.hdr_typ_table = nc_tools.get_string_array(dataset, 'hdr_typ_table')
+            self.hdr_sid_table = nc_tools.get_string_array(dataset, 'hdr_sid_table')
+            self.hdr_vld_table = nc_tools.get_string_array(dataset, 'hdr_vld_table')
+
             nc_var = dataset.variables.get('hdr_prpt_typ', None)
             if nc_var:
                 self.hdr_prpt_typ  = nc_var[:]
@@ -62,9 +55,7 @@ class nc_point_obs(met_point_obs):
                 self.hdr_inst_typ  =nc_var[:]
 
             #Observation data        
-            self.nobs = 0
             self.hdr_sid = dataset['hdr_sid'][:]
-            #self.obs_qty = self.get_num_array(dataset, 'obs_qty')
             self.obs_qty = np.array(dataset['obs_qty'][:])
             self.obs_hid = np.array(dataset['obs_hid'][:])
             self.obs_lvl = np.array(dataset['obs_lvl'][:])
@@ -75,16 +66,31 @@ class nc_point_obs(met_point_obs):
                 self.use_var_id = False
                 nc_var = dataset.variables.get('obs_gc', None)
             else:
-                self.obs_var_table = self.get_string_array(dataset, 'obs_var')
+                self.obs_var_table = nc_tools.get_string_array(dataset, 'obs_var')
             if nc_var:
                 self.obs_vid = np.array(nc_var[:])
             
-            self.obs_qty_rwo = dataset['obs_qty'][:]
+            self.obs_qty_table = nc_tools.get_string_array(dataset, 'obs_qty_table')
 
-            self.obs_qty_table = self.get_string_array(dataset, 'obs_qty_table')
-        else:
-            print("==ERROR== netcdf file ({f}) does not exist".format(f=nc_filename))
-    
+
+class nc_tools():
+
+    @staticmethod
+    def get_num_array(dataset, var_name):
+        nc_var = dataset.variables.get(var_name, None)
+        return nc_var[:].filled(nc_var._FillValue if '_FillValue' in nc_var.ncattrs() else -9999) if nc_var else []
+
+    @staticmethod
+    def get_ncbyte_array_to_str(nc_var):
+        nc_str_data = nc_var[:]
+        if nc_var.datatype.name == 'bytes8':
+            nc_str_data = [ str(s.compressed(),"utf-8") for s in nc_var[:]]
+        return nc_str_data
+
+    @staticmethod
+    def get_string_array(dataset, var_name):
+        nc_var = dataset.variables.get(var_name, None)
+        return nc_tools.get_ncbyte_array_to_str(nc_var) if nc_var else []
 
 
 perf_start_time = time.time()
@@ -92,34 +98,17 @@ perf_start_counter = time.perf_counter_ns()
 
 point_obs_data = None
 if len(sys.argv) == 1:
-    point_obs_data = get_sample_point_obs()
+    point_obs_data = sample_met_point_obs()
+    point_obs_data.read_data([])
 else:
     netcdf_filename = sys.argv[1]
-    if os.path.exists(netcdf_filename):
-        point_obs_data = nc_point_obs()
-        point_obs_data.read_nectdf(netcdf_filename)
-    elif met_point_obs.is_python_script(netcdf_filename):
-        python_script = met_point_obs.get_python_script(netcdf_filename)
-        python_args = []
-        if len(sys.argv) > 2:
-            if ARG_PRINT_DATA == sys.argv[-1]:
-                python_args = sys.argv[2:-1]
-            else:
-                python_args = sys.argv[2:]
-        python_args_string = " ".join(python_args) if 0 < len(python_args) else ''
-        os.environ[MET_PYTHON_OBS_ARGS] = python_args_string
-        #os.system(python_command)
-        exec(open(python_script).read())
-        print('------')
+    args = [ netcdf_filename ]
+    #args = { 'nc_name': netcdf_filename }
+    point_obs_data = nc_point_obs()
+    point_obs_data.read_data(args)
 
-        if point_obs_data is None:
-            print("PYTHON: Fail to get point_obs_data by calling {s} from {m}".format(
-                    s=python_script, m=sys.argv[0]))
     if ARG_PRINT_DATA == sys.argv[-1]:
         DO_PRINT_DATA = True
-    
-    point_obs_data = nc_point_obs()
-    point_obs_data.read_nectdf(netcdf_filename)
 
 if point_obs_data is not None:
     met_point_data = point_obs_data.get_point_data() 
