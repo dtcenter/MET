c1ddd159e91b8558113fea2566d04dacdac03fbe
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-24 11:43:35 -0700

    try running docker with --name, ci-run-unit

--- .github/jobs/run_diff_docker.sh
+++ .github/jobs/run_diff_docker.sh
@@ -15,8 +15,8 @@ export TESTS_TO_RUN=$TESTS
 
 # run unit test script inside Docker, mount MET input and truth data
 cmd="\${MET_REPO_DIR}/.github/jobs/run_diff_tests.sh"
-#time_command docker run -e TESTS_TO_RUN ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
-time_command docker run -e TESTS_TO_RUN ${volumes_from} ${mount_args} run_unit bash -c \"${cmd}\"
+time_command docker run --name=run_unit -e TESTS_TO_RUN ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
+#time_command docker run -e TESTS_TO_RUN ${volumes_from} ${mount_args} run_unit bash -c \"${cmd}\"
 if [ $? != 0 ]; then
   exit 1
 fi
