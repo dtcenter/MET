45748611832a78fd16292747f264756c413d66e7
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-24 12:21:47 -0700

    temporary skip compile to test running docker, ci-skip-compile, ci-run-unit

--- .github/jobs/run_diff_docker.sh
+++ .github/jobs/run_diff_docker.sh
@@ -4,19 +4,21 @@ source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 
 DOCKERHUB_TAG=${DOCKERHUB_REPO}:${SOURCE_BRANCH}
 
+LOCAL_OUTPUT_DIR=${RUNNER_WORKSPACE}/output
+DOCKER_OUTPUT_DIR=/data/output/met_test_output
+
 LOCAL_DIFF_DIR=${RUNNER_WORKSPACE}/diff
 DOCKER_DIFF_DIR=/data/output/met_test_diff
 
 volumes_from="--volumes-from met_truth"
 
-mount_args="-v $LOCAL_DIFF_DIR:$DOCKER_DIFF_DIR -v ${RUNNER_WORKSPACE}/logs:/met/logs"
+mount_args="-v $LOCAL_OUTPUT_DIR:$DOCKER_OUTPUT_DIR -v $LOCAL_DIFF_DIR:$DOCKER_DIFF_DIR -v ${RUNNER_WORKSPACE}/logs:/met/logs"
 
 export TESTS_TO_RUN=$TESTS
 
 # run unit test script inside Docker, mount MET input and truth data
 cmd="\${MET_REPO_DIR}/.github/jobs/run_diff_tests.sh"
-#time_command docker run --name=run_unit -e TESTS_TO_RUN ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
-time_command docker exec -e TESTS_TO_RUN run_unit bash -c \"${cmd}\"
+time_command docker run -e TESTS_TO_RUN ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
 if [ $? != 0 ]; then
   exit 1
 fi
