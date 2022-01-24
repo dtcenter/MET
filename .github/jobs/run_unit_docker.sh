a14b920455cc9b983fe3a809dc9af9d7b000c890
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-24 11:57:13 -0700

    try running docker with -d (detach) to run in background with volumes mounted and run docker exec commands to execute scripts, ci-run-unit

--- .github/jobs/run_unit_docker.sh
+++ .github/jobs/run_unit_docker.sh
@@ -13,9 +13,10 @@ mount_args="-v $LOCAL_DIFF_DIR:$DOCKER_DIFF_DIR -v ${RUNNER_WORKSPACE}/logs:/met
 
 export TESTS_TO_RUN=$TESTS
 
-time_command docker create --name=run_unit ${DOCKERHUB_TAG}
+#time_command docker create --name=run_unit ${DOCKERHUB_TAG}
 
-time_command docker start run_unit
+#time_command docker start run_unit
+time_command docker run -d --name run_unit ${volumes_from} ${mount_args} ${DOCKERHUB_TAG}
 if [ $? != 0 ]; then
   exit 1
 fi
@@ -23,7 +24,7 @@ fi
 # run unit test script inside Docker, mount MET input and truth data
 cmd="\${MET_REPO_DIR}/.github/jobs/run_unit_tests.sh"
 #time_command docker run --name=run_unit -e TESTS_TO_RUN ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
-time_command docker exec -e TESTS_TO_RUN ${volumes_from} ${mount_args} run_unit bash -c \"${cmd}\"
+time_command docker exec -e TESTS_TO_RUN run_unit bash -c \"${cmd}\"
 if [ $? != 0 ]; then
   exit 1
 fi
