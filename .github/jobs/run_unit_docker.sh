94f728359f89bf14e56a0435c998b8b46a54b2a8
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-24 10:56:10 -0700

    start docker container then run separate commands in container, ci-run-unit

--- .github/jobs/run_unit_docker.sh
+++ .github/jobs/run_unit_docker.sh
@@ -13,6 +13,11 @@ mount_args="-v $LOCAL_DIFF_DIR:$DOCKER_DIFF_DIR -v ${RUNNER_WORKSPACE}/logs:/met
 
 export TESTS_TO_RUN=$TESTS
 
+time_command docker start ${DOCKERHUB_TAG}
+if [ $? != 0 ]; then
+  exit 1
+fi
+
 # run unit test script inside Docker, mount MET input and truth data
 cmd="\${MET_REPO_DIR}/.github/jobs/run_unit_tests.sh"
 time_command docker run -e TESTS_TO_RUN ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
