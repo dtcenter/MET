ffeffad301cd5f21795c6ad90e269d8afb62ac9c
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-25 15:44:40 -0700

    added logic to set job control variables to run diff and update truth data

--- .github/jobs/set_job_controls.sh
+++ .github/jobs/set_job_controls.sh
@@ -3,12 +3,23 @@
 run_compile=true
 run_push=false
 run_unit_tests=false
+run_diff=false
+run_update_truth=false
 met_base_image=minimum
 dockerhub_repo=dtcenter/met-dev
 
 if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
 
-  run_unit_tests=true
+  # only run diff logic if pull request INTO develop or main_v*
+  # branches, not branches ending with -ref
+  if [ "${GITHUB_BASE_REF: -4}" != "-ref" ] && \
+    ([ "${GITHUB_BASE_REF:0:7}" == "develop" ]); then #|| \
+     #[ "${GITHUB_BASE_REF:0:6}" == "main_v" ]); then
+
+    run_unit_tests=true
+    run_diff=true
+
+  fi
 
 elif [ "${GITHUB_EVENT_NAME}" == "push" ]; then
 
@@ -19,16 +30,28 @@ elif [ "${GITHUB_EVENT_NAME}" == "push" ]; then
 
   if grep -q "ci-run-unit" <<< "$commit_msg"; then
     run_unit_tests=true
+    run_diff=true
   fi
+
+  branch_name=`cut -d "/" -f3 <<< "${GITHUB_REF}"`
+
+  # if branch ends with -ref, update truth data from unit tests
+  if [ "${branch_name: -4}" == -ref ]; then
+
+    run_compile=true
+    run_push=true
+    run_unit_tests=true
+    run_diff=false
+    run_update_truth=true
 
   # if develop branch, push Docker image to dtcenter/met
   # set all options to prevent commit override
-  branch_name=`cut -d "/" -f3 <<< "${GITHUB_REF}"`
-  if [ "$branch_name" == "develop" ]; then
+  elif [ "$branch_name" == "develop" ]; then
      # || [ "${branch_name:0:6}" == "main_v" ]; then
     run_compile=true
     run_push=true
     run_unit_tests=false
+    run_diff=false
     met_base_image=minimum
     dockerhub_repo=dtcenter/met
   fi
@@ -46,6 +69,8 @@ fi
 echo ::set-output name=run_compile::$run_compile
 echo ::set-output name=run_push::$run_push
 echo ::set-output name=run_unit_tests::$run_unit_tests
+echo ::set-output name=run_diff::$run_diff
+echo ::set-output name=run_update_truth::$run_update_truth
 echo ::set-output name=met_base_image::$met_base_image
 echo ::set-output name=dockerhub_repo::$dockerhub_repo
 
