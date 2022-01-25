088bcb16aae7b029b94f182040bbecb0539cd249
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-25 15:49:37 -0700

    Per #1546, added logic to update truth data volume on DockerHub for -ref branches

---
+++ .github/jobs/create_docker_truth.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
+
+image_name=dtcenter/met-data-output:${TRUTH_DATA_VERSION}
+
+time_command docker build -t ${image_name} \
+       --build-arg TRUTH_DIR=met_test_truth \
+       -f ${GITHUB_WORKSPACE}/.github/jobs/Dockerfile.truth \
+       ${RUNNER_WORKSPACE}
