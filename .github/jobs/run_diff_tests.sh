fb6cd8dcbf41d1977f8bbaeb07598cee829a11e4
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-25 14:09:19 -0700

    print contents of log file on failure for easier debugging

--- .github/jobs/run_diff_tests.sh
+++ .github/jobs/run_diff_tests.sh
@@ -17,6 +17,7 @@ CMD_LOGFILE=/met/logs/comp_dir.log
 time_command ${MET_TEST_BASE}/bin/comp_dir.sh ${MET_TEST_TRUTH} ${MET_TEST_OUTPUT}
 if [ $? != 0 ]; then
     echo "ERROR: Test output comparison failed"
+    cat /met/logs/comp_dir.log
     exit 1
 fi
 
@@ -25,6 +26,7 @@ CMD_LOGFILE=/met/logs/copy_diff_files.log
 time_command ${MET_REPO_DIR}/.github/jobs/copy_diff_files.py
 if [ $? != 0 ]; then
     echo "ERROR: Copy diff files script failed"
+    cat /met/logs/copy_diff_files.log
     exit 1
 fi
 
--- .github/jobs/run_unit_tests.sh
+++ .github/jobs/run_unit_tests.sh
@@ -18,6 +18,7 @@ for testname in $TESTS_TO_RUN; do
   time_command ${MET_TEST_BASE}/perl/unit.pl ${MET_TEST_BASE}/xml/unit_${testname}.xml
   if [ $? != 0 ]; then
       echo "ERROR: Unit test ${testname} failed"
+      cat /met/logs/unit_${testname}.log
       exit 1
   fi
 done
