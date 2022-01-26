e4bffe9a530328eb1dca729bfbc712a1cb3524be
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-26 09:30:43 -0700

    revert back to using full MET image for diff test since unit_test base is needed to run each unit test anyway

--- .github/jobs/run_diff_docker.sh
+++ .github/jobs/run_diff_docker.sh
@@ -2,8 +2,7 @@
 
 source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 
-#DOCKERHUB_TAG=${DOCKERHUB_REPO}:${SOURCE_BRANCH}
-DOCKERHUB_TAG=dtcenter/met-base:unit_test
+DOCKERHUB_TAG=${DOCKERHUB_REPO}:${SOURCE_BRANCH}
 
 # Get truth output data
 ${GITHUB_WORKSPACE}/.github/jobs/get_test_truth_data.sh ${TRUTH_DATA_VERSION}
@@ -18,21 +17,19 @@ DOCKER_DIFF_DIR=/data/output/met_test_diff
 LOCAL_LOG_DIR=${RUNNER_WORKSPACE}/logs
 DOCKER_LOG_DIR=/met/logs
 
-MET_REPO_DIR=/met/MET
-
 # Create local directories to store output
 mkdir -p ${LOCAL_LOG_DIR}
 mkdir -p ${LOCAL_DIFF_DIR}
 
 # mount output and log dirs, mount GitHub files into MET_REPO_DIR
-mount_args="-v ${GITHUB_WORKSPACE}:/${MET_REPO_DIR} -v ${LOCAL_OUTPUT_DIR}:${DOCKER_OUTPUT_DIR} -v ${LOCAL_DIFF_DIR}:${DOCKER_DIFF_DIR} -v ${LOCAL_LOG_DIR}:${DOCKER_LOG_DIR}"
+mount_args="-v ${LOCAL_OUTPUT_DIR}:${DOCKER_OUTPUT_DIR} -v ${LOCAL_DIFF_DIR}:${DOCKER_DIFF_DIR} -v ${LOCAL_LOG_DIR}:${DOCKER_LOG_DIR}"
 
 # Set up data volumes
 volumes_from="--volumes-from met_truth"
 
 # run unit test script inside Docker, mount MET output and truth data
 # set MET_REPO_DIR env var in Docker to mounted directory
-cmd="export MET_REPO_DIR=${MET_REPO_DIR}; \${MET_REPO_DIR}/.github/jobs/run_diff_tests.sh"
+cmd="\${MET_REPO_DIR}/.github/jobs/run_diff_tests.sh"
 time_command docker run ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
 if [ $? != 0 ]; then
   exit 1
