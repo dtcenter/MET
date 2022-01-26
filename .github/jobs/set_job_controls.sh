01cab474e4e70c643bbd02b71e8e32c96b3c6eb2
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-26 11:04:27 -0700

    Testing: 3rd try - temporarily set to update truth and write to test truth data docker volume

--- .github/jobs/set_job_controls.sh
+++ .github/jobs/set_job_controls.sh
@@ -3,8 +3,8 @@
 run_compile=true
 run_push=false
 run_unit_tests=false
-run_diff=false
-run_update_truth=false
+run_diff=true
+run_update_truth=true
 met_base_image=minimum
 truth_data_version=develop
 
