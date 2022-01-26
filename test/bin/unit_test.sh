1b842c29c4d824d95e74253e1a211565ae4f4893
John Halley Gotway <johnhg@ucar.edu>
2022-01-26 15:40:45 -0700

    Per #1543, ci-run-unit split unit_climatology.xml up into 3 pieces where tests are grouped by the type of climo data being used. If/when we subset the input datasets, this would be a logic grouping based on data dependencies.

--- test/bin/unit_test.sh
+++ test/bin/unit_test.sh
@@ -81,7 +81,9 @@ UNIT_XML="unit_ascii2nc.xml \
           unit_aeronet.xml \
           unit_shift_data_plane.xml \
           unit_mtd.xml \
-          unit_climatology.xml \
+          unit_climatology_1.0deg.xml \
+          unit_climatology_1.5deg.xml \
+          unit_climatology_2.0deg.xml \
           unit_grib_tables.xml \
           unit_grid_weight.xml \
           unit_netcdf.xml \
