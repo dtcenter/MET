4f92d4560d8135643f002512966e4df18f16b980
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-01-26 09:45:14 -0700

    added docker push for creating new truth data volume

--- .github/jobs/create_docker_truth.sh
+++ .github/jobs/create_docker_truth.sh
@@ -8,3 +8,9 @@ time_command docker build -t ${image_name} \
        --build-arg TRUTH_DIR=met_test_truth \
        -f ${GITHUB_WORKSPACE}/.github/jobs/Dockerfile.truth \
        ${RUNNER_WORKSPACE}
+if [ $? != 0 ]; then
+  echo "ERROR: Docker build failed"
+  exit 1
+fi
+
+time_command docker push ${image_name}
