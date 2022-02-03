1d68dfec3570dbbbc5d170f1bd017a8892abe708
John Halley Gotway <johnhg@ucar.edu>
2022-02-03 09:35:01 -0700

    Per #1583, add initial support for interp.type.method = HIRA.

--- met/src/basic/vx_util/interp_mthd.h
+++ met/src/basic/vx_util/interp_mthd.h
@@ -42,7 +42,8 @@ enum InterpMthd {
    InterpMthd_Lower_Left,
    InterpMthd_Gaussian,
    InterpMthd_MaxGauss,
-   InterpMthd_Geog_Match
+   InterpMthd_Geog_Match,
+   InterpMthd_HiRA
 };
 
 //
@@ -69,6 +70,7 @@ static const char interpmthd_lower_left_str[]  = "LOWER_LEFT";
 static const char interpmthd_gaussian_str[]    = "GAUSSIAN";
 static const char interpmthd_maxgauss_str[]    = "MAXGAUSS";
 static const char interpmthd_geog_match_str[]  = "GEOG_MATCH";
+static const char interpmthd_hira_str[]        = "HIRA";
 
 ///////////////////////////////////////////////////////////////////////////////
 
