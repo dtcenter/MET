9c4319127d45d812b7bda6fb02499ffee15560b1
John Halley Gotway <johnhg@ucar.edu>
2022-05-12 11:36:08 -0600

    Per #1920, removing bad default path for MET_TEST_BASE.

--- internal/test_unit/bin/comp_dir.sh
+++ internal/test_unit/bin/comp_dir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # check for environment variables, use defaults if necessary
-MET_TEST_BASE=${MET_TEST_BASE:-/d3/projects/MET/MET_development/svn-met-dev.cgd.ucar.edu/trunk/test}
+MET_TEST_BASE=${MET_TEST_BASE}
 MET_TEST_RSCRIPT=${MET_TEST_RSCRIPT:-/usr/local/bin/Rscript}
 COMP_SCRIPT=$MET_TEST_BASE/R_test/comp_dir.R
 
