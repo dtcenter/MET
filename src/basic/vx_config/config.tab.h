f9a153ac35a3a112118f1dc60322f2f118cb4784
davidalbo <dave@ucar.edu>
2022-05-20 14:54:24 -0600

    Feature 2072 tc rmw winds (#2165)
    * bug fix for NAN and for using missing data as if its not missing
    
    * added parameters to control converting winds to tangential/radial components
    
    * bug fixes
    
    * Refactored to put wind conversion into it's own class TCRMW_WindConverter
    
    * changed default for converting winds to TRUE, and put parameters for wind conversion into the unit test config files
    
    * Per #2072, updating tc_rmw Makefile.in to compile new files.
    
    * Per #2072, update to the output from the lex/yacc version on seneca.
    
    * Changed Error to Warning in mlog messages, for consistency with MET conventions
    
    * Removing commented out code.
    
    * Per #2072, combine warnings spread across multiple lines into a single call to the logger. Replace references to ugrid/vgrid with u/v wind. Fix a typo and clean up some whitespace.
    
    Co-authored-by: Dave Albo <dave@seneca.rap.ucar.edu>
    Co-authored-by: John Halley Gotway <johnhg@ucar.edu>

--- src/basic/vx_config/config.tab.h
+++ src/basic/vx_config/config.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.3.2.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2019 Free Software Foundation,
+   Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,30 +31,42 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_CONFIG_CONFIG_TAB_H_INCLUDED
+# define YY_CONFIG_CONFIG_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int configdebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     IDENTIFIER = 258,
-     QUOTED_STRING = 259,
-     INTEGER = 260,
-     FLOAT = 261,
-     BOOLEAN = 262,
-     COMPARISON = 263,
-     NA_COMPARISON = 264,
-     LOGICAL_OP_NOT = 265,
-     LOGICAL_OP_AND = 266,
-     LOGICAL_OP_OR = 267,
-     FORTRAN_THRESHOLD = 268,
-     BUILTIN = 269,
-     LOCAL_VAR = 270,
-     SIMPLE_PERC_THRESH = 271,
-     USER_FUNCTION = 272,
-     PRINT = 273,
-     UNARY_MINUS = 274
-   };
+  enum yytokentype
+  {
+    IDENTIFIER = 258,
+    QUOTED_STRING = 259,
+    INTEGER = 260,
+    FLOAT = 261,
+    BOOLEAN = 262,
+    COMPARISON = 263,
+    NA_COMPARISON = 264,
+    LOGICAL_OP_NOT = 265,
+    LOGICAL_OP_AND = 266,
+    LOGICAL_OP_OR = 267,
+    FORTRAN_THRESHOLD = 268,
+    BUILTIN = 269,
+    LOCAL_VAR = 270,
+    SIMPLE_PERC_THRESH = 271,
+    USER_FUNCTION = 272,
+    PRINT = 273,
+    UNARY_MINUS = 274
+  };
 #endif
 /* Tokens.  */
 #define IDENTIFIER 258
@@ -77,13 +87,13 @@
 #define PRINT 273
 #define UNARY_MINUS 274
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 195 "config.tab.yy"
+
+union YYSTYPE
 {
+#line 195 "config.tab.yy" /* yacc.c:1921  */
+
 
    char text[max_id_length + 1];
 
@@ -101,14 +111,18 @@ typedef union YYSTYPE
 
    PC_info pc_info;
 
-}
-/* Line 1529 of yacc.c.  */
-#line 107 "config.tab.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 116 "config.tab.h" /* yacc.c:1921  */
+};
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE configlval;
 
+int configparse (void);
+
+#endif /* !YY_CONFIG_CONFIG_TAB_H_INCLUDED  */
