c5ea4c845d73dc0760eb5b31d978f0cd3b137880
johnhg <johnhg@ucar.edu>
2022-05-20 17:16:38 -0600

    Update develop-ref after #2165 (#2167)
    Co-authored-by: Julie Prestopnik <jpresto@seneca.rap.ucar.edu>
    Co-authored-by: johnhg <johnhg@ucar.edu>
    Co-authored-by: Seth Linden <linden@kiowa.rap.ucar.edu>
    Co-authored-by: John Halley Gotway <johnhg@ucar.edu>
    Co-authored-by: j-opatz <59586397+j-opatz@users.noreply.github.com>
    Co-authored-by: Howard Soh <hsoh@kiowa.rap.ucar.edu>
    Co-authored-by: John Halley Gotway <johnhg@kiowa.rap.ucar.edu>
    Co-authored-by: jprestop <jpresto@ucar.edu>
    Co-authored-by: Howard Soh <hsoh@seneca.rap.ucar.edu>
    Co-authored-by: Randy Bullock <bullock@seneca.rap.ucar.edu>
    Co-authored-by: davidfillmore <fillmore.winslow.david@gmail.com>
    Co-authored-by: rgbullock <bullock@ucar.edu>
    Co-authored-by: Seth Linden <linden@seneca.rap.ucar.edu>
    Co-authored-by: George McCabe <23407799+georgemccabe@users.noreply.github.com>
    Co-authored-by: Seth Linden <linden@ucar.edu>
    Co-authored-by: hsoh-u <hsoh@ucar.edu>
    Co-authored-by: John Halley Gotway <johnhg@seneca.rap.ucar.edu>
    Co-authored-by: MET Tools Test Account <met_test@seneca.rap.ucar.edu>
    Co-authored-by: mo-mglover <78152252+mo-mglover@users.noreply.github.com>
    Co-authored-by: davidalbo <dave@ucar.edu>
    Co-authored-by: lisagoodrich <33230218+lisagoodrich@users.noreply.github.com>
    Co-authored-by: Dan Adriaansen <dadriaan@ucar.edu>
    Co-authored-by: Dave Albo <dave@seneca.rap.ucar.edu>
    Co-authored-by: Molly Smith <molly.b.smith@noaa.gov>

--- src/libcode/vx_color/color_parser_yacc.h
+++ src/libcode/vx_color/color_parser_yacc.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.3.2.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2019 Free Software Foundation,
+   Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,22 +31,34 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_COLOR_COLOR_PARSER_YACC_H_INCLUDED
+# define YY_COLOR_COLOR_PARSER_YACC_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int colordebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     ID = 258,
-     COLOR_NAME = 259,
-     INTEGER = 260,
-     QUOTED_STRING = 261,
-     FLOAT = 262,
-     BLEND = 263,
-     HSV = 264,
-     GRAYVALUE = 265,
-     CMYK = 266
-   };
+  enum yytokentype
+  {
+    ID = 258,
+    COLOR_NAME = 259,
+    INTEGER = 260,
+    QUOTED_STRING = 261,
+    FLOAT = 262,
+    BLEND = 263,
+    HSV = 264,
+    GRAYVALUE = 265,
+    CMYK = 266
+  };
 #endif
 /* Tokens.  */
 #define ID 258
@@ -61,13 +71,13 @@
 #define GRAYVALUE 265
 #define CMYK 266
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 126 "color_parser_yacc.yy"
+
+union YYSTYPE
 {
+#line 126 "color_parser_yacc.yy" /* yacc.c:1921  */
+
 
    char text[129];
 
@@ -79,14 +89,18 @@ typedef union YYSTYPE
 
    ColorNumber nval;
 
-}
-/* Line 1529 of yacc.c.  */
-#line 85 "color_parser_yacc.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 94 "color_parser_yacc.h" /* yacc.c:1921  */
+};
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE colorlval;
 
+int colorparse (void);
+
+#endif /* !YY_COLOR_COLOR_PARSER_YACC_H_INCLUDED  */
