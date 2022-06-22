c18dc8c9b699418d0fb894649735ad5104447b0f
John Halley Gotway <johnhg@ucar.edu>
2022-06-22 16:54:43 -0600

    Hotfix to develop since I forgot to update the version number in conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ print(sys.path)
 project = 'MET'
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Opatz, J., T. Jensen, J. Prestopnik, H. Soh, L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman'
-version = '10.1.0'
+version = '11.0.0-beta1'
 verinfo = version
 release = f'{version}'
 release_year = '2022'
