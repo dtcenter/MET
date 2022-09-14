310c471a734f310a6780fafd243fa4084b49577f
johnhg <johnhg@ucar.edu>
2022-09-14 10:58:50 -0600

    Update develop-ref after #2247 (#2261)
    Co-authored-by: Julie Prestopnik <jpresto@seneca.rap.ucar.edu>
    Co-authored-by: Howard Soh <hsoh@seneca.rap.ucar.edu>
    Co-authored-by: jprestop <jpresto@ucar.edu>
    Co-authored-by: j-opatz <59586397+j-opatz@users.noreply.github.com>
    Co-authored-by: Randy Bullock <bullock@seneca.rap.ucar.edu>
    Co-authored-by: davidfillmore <fillmore.winslow.david@gmail.com>
    Co-authored-by: rgbullock <bullock@ucar.edu>
    Co-authored-by: John Halley Gotway <johnhg@kiowa.rap.ucar.edu>
    Co-authored-by: johnhg <johnhg@ucar.edu>
    Co-authored-by: John Halley Gotway <johnhg@ucar.edu>
    Co-authored-by: Seth Linden <linden@seneca.rap.ucar.edu>
    Co-authored-by: George McCabe <23407799+georgemccabe@users.noreply.github.com>
    Co-authored-by: Seth Linden <linden@kiowa.rap.ucar.edu>
    Co-authored-by: Howard Soh <hsoh@kiowa.rap.ucar.edu>
    Co-authored-by: Seth Linden <linden@ucar.edu>
    Co-authored-by: hsoh-u <hsoh@ucar.edu>
    Co-authored-by: John Halley Gotway <johnhg@seneca.rap.ucar.edu>
    Co-authored-by: MET Tools Test Account <met_test@seneca.rap.ucar.edu>
    Co-authored-by: mo-mglover <78152252+mo-mglover@users.noreply.github.com>
    Co-authored-by: davidalbo <dave@ucar.edu>
    Co-authored-by: lisagoodrich <33230218+lisagoodrich@users.noreply.github.com>
    Co-authored-by: Dan Adriaansen <dadriaan@ucar.edu>
    Co-authored-by: Dave Albo <dave@seneca.rap.ucar.edu>
    Co-authored-by: Lisa Goodrich <lisag@seneca.rap.ucar.edu>
    Co-authored-by: Molly Smith <molly.b.smith@noaa.gov>
    Co-authored-by: Jonathan Vigh <jvigh@ucar.edu>
    Co-authored-by: bikegeek <3753118+bikegeek@users.noreply.github.com>

--- src/basic/vx_util/num_array.h
+++ src/basic/vx_util/num_array.h
@@ -109,7 +109,9 @@ class NumArray {
       double mean() const;
       double mean_sqrt() const;
       double mean_fisher() const;
-
+      double mean_abs_diff() const;
+      double wmean_abs_diff() const;
+      
       double variance(int skip_index = bad_data_int) const;
       double stdev(int skip_index = bad_data_int) const;
 
