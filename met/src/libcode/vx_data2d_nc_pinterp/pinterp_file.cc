ba1014eb3c9b14ee609a1666c614ef06dbd2f48d
johnhg <johnhg@ucar.edu>
2022-10-19 16:34:51 -0600

    Update main_v10.1-ref after #2311 (#2312)
    Co-authored-by: George McCabe <23407799+georgemccabe@users.noreply.github.com>
    Co-authored-by: johnhg <johnhg@ucar.edu>
    Co-authored-by: Julie Prestopnik <jpresto@seneca.rap.ucar.edu>
    Co-authored-by: Howard Soh <hsoh@seneca.rap.ucar.edu>
    Co-authored-by: hsoh-u <hsoh@ucar.edu>
    Co-authored-by: jprestop <jpresto@ucar.edu>

--- met/src/libcode/vx_data2d_nc_pinterp/pinterp_file.cc
+++ met/src/libcode/vx_data2d_nc_pinterp/pinterp_file.cc
@@ -567,18 +567,10 @@ int i;
 short s;
 float f;
 double d = bad_data_double;
-float add_offset   = 0.f;
-float scale_factor = 1.f;
 double missing_value = get_var_missing_value(var);
 double fill_value    = get_var_fill_value(var);
-NcVarAtt *att_add_offset   = get_nc_att(var, (string)"add_offset");
-NcVarAtt *att_scale_factor = get_nc_att(var, (string)"scale_factor");
-if (!IS_INVALID_NC_P(att_add_offset) && !IS_INVALID_NC_P(att_scale_factor)) {
-   add_offset = get_att_value_float(att_add_offset);
-   scale_factor = get_att_value_float(att_scale_factor);
-}
-if (att_add_offset) delete att_add_offset;
-if (att_scale_factor) delete att_scale_factor;
+float add_offset     = get_var_add_offset_value(var);
+float scale_factor   = get_var_scale_factor_value(var);
 
 switch ( GET_NC_TYPE_ID_P(var) )  {
 
@@ -790,16 +782,8 @@ float  f[Ny];
 
 long offsets[dim_count];
 long lengths[dim_count];
-float add_offset   = 0.f;
-float scale_factor = 1.f;
-NcVarAtt *att_add_offset   = get_nc_att(v, (string)"add_offset");
-NcVarAtt *att_scale_factor = get_nc_att(v, (string)"scale_factor");
-if (!IS_INVALID_NC_P(att_add_offset) && !IS_INVALID_NC_P(att_scale_factor)) {
-   add_offset = get_att_value_float(att_add_offset);
-   scale_factor = get_att_value_float(att_scale_factor);
-}
-if (att_add_offset) delete att_add_offset;
-if (att_scale_factor) delete att_scale_factor;
+float add_offset   = get_var_add_offset_value(var->var);
+float scale_factor = get_var_scale_factor_value(var->var);
 
 for (int k=0; k<dim_count; k++) {
   offsets[k] = (a[k] == vx_data2d_star) ? 0 : a[k];
