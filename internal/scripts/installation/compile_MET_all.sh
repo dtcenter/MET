a4fef9d7c492a683da9b287f57e1ce695def6eda
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-01-10 11:11:20 -0700

    per discussion dtcenter/metplus#1997, handle NetCDF4-C for new tar.gz file and old zip file

--- internal/scripts/installation/compile_MET_all.sh
+++ internal/scripts/installation/compile_MET_all.sh
@@ -648,8 +648,8 @@ if [ $COMPILE_NETCDF -eq 1 ]; then
   mkdir -p ${LIB_DIR}/netcdf
   cd ${LIB_DIR}/netcdf
   rm -rf netcdf*
-  tar -xzf ${TAR_DIR}/netcdf-4*.tar.gz
-  cd netcdf-c-4*
+  tar -xzf ${TAR_DIR}/netcdf-4*.tar.gz > /dev/null 2>&1 || unzip ${TAR_DIR}/netcdf-4*.zip > /dev/null 2>&1
+  cd netcdf-c-* > /dev/null 2>&1 || cd netcdf-4*
   export FC=''
   export F90=''
   echo "cd `pwd`"
