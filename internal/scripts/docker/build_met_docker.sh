937ed60f3beb99bef530c28d72ea9b66edea0180
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-01-13 15:56:44 -0700

    attempt to build MET through GHA using compile script and debian image

--- internal/scripts/docker/build_met_docker.sh
+++ internal/scripts/docker/build_met_docker.sh
@@ -2,34 +2,14 @@
 
 echo "Running script to build MET in Docker"
 
-LOG_FILE=/met/logs/MET-${MET_GIT_NAME}_configure.log
-
 source internal/scripts/environment/development.docker
 
-echo "Configuring MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
-./bootstrap
-./configure --enable-grib2 --enable-mode_graphics --enable-modis --enable-lidar2nc --enable-python > ${LOG_FILE}
-if [ $? != 0 ]; then
-    exit 1
-fi
+export SKIP_LIBS=yes
+export USE_MET_TAR_FILE=FALSE
 
-if [ ! -z "${MAKE_ARGS}" ]; then
-  echo Adding make arguments: ${MAKE_ARGS}
-fi
-
-LOG_FILE=/met/MET-${MET_GIT_NAME}/make_install.log
+LOG_FILE=/met/MET-${MET_GIT_NAME}/compile_MET_all.log
 echo "Compiling MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
-make ${MAKE_ARGS} install > ${LOG_FILE}
-if [ $? != 0 ]; then
-    exit 1
-fi
-
-LOG_FILE=/met/logs/MET-${MET_GIT_NAME}_make_test.log
-echo "Testing MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
-make ${MAKE_ARGS} test > ${LOG_FILE} 2>&1
-if [ $? != 0 ]; then
-    exit 1
-fi
+../installation/compile_MET_all.sh > ${LOG_FILE}
 
 if [[ $MET_GIT_NAME == "v"* ]]; then
     cd /met; rm -rf MET-*;
