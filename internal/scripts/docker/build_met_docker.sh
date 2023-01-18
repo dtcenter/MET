77bbcfdf66b683e6fe17423c951c63f5d22ae7df
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-01-18 10:24:56 -0700

    revert back to installing MET without using the compilation script so logs can be more easily captured

--- internal/scripts/docker/build_met_docker.sh
+++ internal/scripts/docker/build_met_docker.sh
@@ -4,15 +4,28 @@ echo "Running script to build MET in Docker"
 
 source internal/scripts/environment/development.docker
 
-export SKIP_LIBS=yes
-export USE_MET_TAR_FILE=FALSE
-
-LOG_FILE=/met/MET-${MET_GIT_NAME}/compile_MET_all.log
-echo "Compiling MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
-./internal/scripts/installation/compile_MET_all.sh > ${LOG_FILE} 2>&1
+LOG_FILE=/met/logs/MET-${MET_GIT_NAME}_configure.log
+echo "Configuring MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
+./bootstrap
+./configure --enable-grib2 --enable-mode_graphics --enable-modis --enable-lidar2nc --enable-python > ${LOG_FILE}
 if [ $? != 0 ]; then
   cat ${LOG_FILE}
-  echo "ERROR: Compilation failed"
+  exit 1
+fi
+
+LOG_FILE=/met/logs/MET-${MET_GIT_NAME}/make_install.log
+echo "Compiling MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
+make install > ${LOG_FILE}
+if [ $? != 0 ]; then
+  cat ${LOG_FILE}
+  exit 1
+fi
+
+LOG_FILE=/met/logs/MET-${MET_GIT_NAME}_make_test.log
+echo "Testing MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
+make test > ${LOG_FILE} 2>&1
+if [ $? != 0 ]; then
+  cat ${LOG_FILE}
   exit 1
 fi
 
