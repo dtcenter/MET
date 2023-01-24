9e056e391ebbec181487976b6881c4c03668ac32
John Halley Gotway <johnhg@ucar.edu>
2023-01-24 11:11:06 -0700

    Per #2412, unrelated to this issue but adding the -j option to the make commands in build_met_docker.sh. This should improve the speed of compilation job in GitHub actions. Using 'make -j install' improves the compilation time from 8m40s to 2m58s. So definitely worth trying!

--- internal/scripts/docker/build_met_docker.sh
+++ internal/scripts/docker/build_met_docker.sh
@@ -15,14 +15,14 @@ fi
 
 LOG_FILE=/met/MET-${MET_GIT_NAME}/make_install.log
 echo "Compiling MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
-make install > ${LOG_FILE}
+make -j install > ${LOG_FILE}
 if [ $? != 0 ]; then
     exit 1
 fi
 
 LOG_FILE=/met/logs/MET-${MET_GIT_NAME}_make_test.log
 echo "Testing MET ${MET_GIT_NAME} and writing log file ${LOG_FILE}"
-make test > ${LOG_FILE} 2>&1
+make -j test > ${LOG_FILE} 2>&1
 if [ $? != 0 ]; then
     exit 1
 fi
