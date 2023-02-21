ee79d7795db537349f00dc3313ac193f128fc8d8
Lisa Goodrich <lisag@ucar.edu>
2023-02-21 11:58:19 -0700

    adding sphinx_design to config.py and requirements.txt so that dropdown menus will work.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ copyright = f'{release_year}, {author}'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # Adding 'sphinx_panels' to use drop-down menus in appendixA. 
-extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx','sphinx_panels',]
+extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx','sphinx_panels','sphinx_design',]
 
 # settings for ReadTheDocs PDF creation
 latex_engine = 'pdflatex'
