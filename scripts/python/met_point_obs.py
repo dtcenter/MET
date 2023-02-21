dd6aaa92e0639a4b4cce73508a0a5a6596b1e0b7
Howard Soh <hsoh@seneca.rap.ucar.edu>
2023-02-21 09:01:44 -0700

    #2428 CSV: check if grib code

--- scripts/python/met_point_obs.py
+++ scripts/python/met_point_obs.py
@@ -373,6 +373,7 @@ class csv_point_obs(ABC, base_met_point_obs):
         self.obs_hgt = [ self.FILL_VALUE for _ in range(0, obs_cnt) ]  # (nobs) float
         self.obs_val = [ self.FILL_VALUE for _ in range(0, obs_cnt) ]  # (nobs) float
 
+        self.use_var_id = not self.is_grib_code()
         self.convert_point_data()
 
     def check_csv_record(self, csv_point_data, index):
