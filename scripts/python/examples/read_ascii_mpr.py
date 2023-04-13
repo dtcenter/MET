9fae26f3954ab075526f9d77e17cb622b6b5180b
Howard Soh <hsoh@seneca.rap.ucar.edu>
2023-04-13 08:57:07 -0600

    #2285 Changed argument usecols to col_start and col_last for read_mpr

--- scripts/python/examples/read_ascii_mpr.py
+++ scripts/python/examples/read_ascii_mpr.py
@@ -13,19 +13,20 @@ print("Python Script:\t" + repr(sys.argv[0]))
    ##
 
 if len(sys.argv) != 2:
-    print("ERROR: read_ascii_point.py -> Must specify exactly one input file.")
-    sys.exit(1)
+   print("ERROR: read_ascii_mpr.py -> Must specify exactly one input file.")
+   sys.exit(1)
 
 # Read the input file as the first argument
 input_file = os.path.expandvars(sys.argv[1])
 try:
-    print("Input File:\t" + repr(input_file))
+   print("Input File:\t" + repr(input_file))
 
-    # Read MPR lines, skipping the header row and first column.
-    mpr_data = mpr_data.read_mpr(input_file)
-    print("Data Length:\t" + repr(len(mpr_data)))
-    print("Data Type:\t" + repr(type(mpr_data)))
+   # Read MPR lines by using pandas package, skipping the header row and
+   # first column. Input should be a 36 column text data.
+   mpr_data = mpr_data.read_mpr(input_file, col_start=1, col_last=36, skiprows=1)
+   print("Data Length:\t" + repr(len(mpr_data)))
+   print("Data Type:\t" + repr(type(mpr_data)))
 except NameError:
-    print("Can't find the input file")
+   print("Can't find the input file")
 
 ########################################################################
