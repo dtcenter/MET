d4c8844a8a709c372fa954ba131adc14071af1ac
Howard Soh <hsoh@seneca.rap.ucar.edu>
2023-04-14 11:11:00 -0600

    #2474 Removed the convertion option

--- scripts/python/examples/read_ascii_point.py
+++ scripts/python/examples/read_ascii_point.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from met.point import met_point_tools as tools
+from met.point import met_point_tools
 
 ########################################################################
 
@@ -17,14 +17,7 @@ if arg_cnt < 2:
    print("ERROR: read_ascii_point.py -> Missing an input file.")
    sys.exit(1)
 
-do_convert = False
 last_index = 2
-if arg_cnt > last_index:
-   opt_convert = sys.argv[2]
-   if opt_convert.lower() == "do_convert" or opt_convert.lower() == "convert":
-      do_convert = True
-      last_index += 1
-
 if last_index < arg_cnt:
    print(" INFO: read_ascii_point.py -> Too many argument, ignored {o}.".format(
          o=' '.join(sys.argv[last_index:])))
@@ -48,13 +41,9 @@ try:
    #   (11) numeric: Observation_Value
 
    # Read 11 column text input data by using pandas package
-   point_data = tools.read_text_point_obs(input_file)
+   point_data = met_point_tools.read_text_point_obs(input_file)
    print("     point_data: Data Length:\t" + repr(len(point_data)))
    print("     point_data: Data Type:\t" + repr(type(point_data)))
-   if do_convert:
-      # Convert 11 column list to MET's internal python instance
-      met_point_data = tools.convert_point_data(point_data)
-      print(" met_point_data: Data Type:\t" + repr(type(met_point_data)))
 except FileNotFoundError:
    print(f"The input file {input_file} does not exist")
    sys.exit(1)
