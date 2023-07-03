1219aaa74d93ad1b9c4ef6027a7dcefa675e427d
jprestop <jpresto@ucar.edu>
2023-07-03 09:33:51 -0600

    Removing unnecessary spaces

--- src/libcode/vx_color/color_list.cc
+++ src/libcode/vx_color/color_list.cc
@@ -4,7 +4,7 @@
 // ** National Center for Atmospheric Research (NCAR)
 // ** Research Applications Lab (RAL)
 // ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
-// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*   
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
 
 
 
