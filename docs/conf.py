a98c70cf28f4a879abfd7fb737c954e283cf5ac0
John Halley Gotway <johnhg@ucar.edu>
2023-09-15 14:56:43 -0600

    Feature #2681 v12.0.0-beta1 (#2685)

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,12 @@ print(sys.path)
 
 project = 'MET'
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
-author_list = 'Jensen, T., J. Prestopnik, H. Soh, L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz'
-version = '11.1.0'
+author_list = 'Prestopnik, J., H. Soh, L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz, T. Jensen'
+version = '12.0.0-beta1'
 verinfo = version
 release = f'{version}'
 release_year = '2023'
-release_date = f'{release_year}-07-31'
+release_date = f'{release_year}-09-15'
 copyright = f'{release_year}, {author}'
 
 # -- General configuration ---------------------------------------------------
