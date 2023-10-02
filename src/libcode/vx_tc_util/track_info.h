3b7d6530e79b2d9627d796e838eb1db40f7e2f30
Seth Linden <linden@ucar.edu>
2023-10-02 14:45:44 -0600

    Update src/libcode/vx_tc_util/track_info.h
    Co-authored-by: John Halley Gotway <johnhg@ucar.edu>

--- src/libcode/vx_tc_util/track_info.h
+++ src/libcode/vx_tc_util/track_info.h
@@ -169,7 +169,6 @@ class TrackInfo {
       void add_watch_warn(const ConcatString &, WatchWarnType, unixtime);
       bool add_diag_data(DiagFile &, const StringArray &);
       void add_diag_value(int, double);
-      //void add_uniq_diag_name(const string diag_name);
       void add_uniq_diag_name(const std::string);
       
       bool has(const ATCFTrackLine &) const;
