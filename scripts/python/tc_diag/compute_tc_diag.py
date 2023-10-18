e3db0c6c60a06a11c0e610d7315af41af8620b21
John Halley Gotway <johnhg@ucar.edu>
2023-10-18 16:17:12 -0600

    Per #2550, add SKIP_DATA_NAMES and make the script more pythonic

--- scripts/python/tc_diag/compute_tc_diag.py
+++ scripts/python/tc_diag/compute_tc_diag.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import numpy as np
+import argparse
+import pathlib
 from tc_diag_driver import post_resample_driver
 
 ###########################################
@@ -21,125 +23,147 @@ SOUNDING_DATA_NAMES = (
     "T", "R", "Z", "U", "V"
 )
 
+# Diagnostic names to be skipped
+SKIP_DATA_NAMES = (
+    "850RADIAL", "850DVRG", "200VORT"
+)
+
 # Bad data integer value
 BAD_DATA_INT = 9999
 
 ###########################################
 
-print("Python Script:\t" + repr(sys.argv[0]))
+def main():
 
-   ##
-   ## Expect exactly 3 arguments
-   ##
+    # Handle arguments
+    args = _get_args()
 
-if len(sys.argv) != 4:
-    print("ERROR: compute_tc_diag.py -> Must specify a configuration file, distance to land file, and NetCDF cylindrical coordinates file.")
-    sys.exit(1)
+    config_filename = os.path.expandvars(args.config_file)
+    land_filename   = os.path.expandvars(args.land_file)
+    data_filename   = os.path.expandvars(args.data_file)
 
-# Configuration file
-config_filename = os.path.expandvars(sys.argv[1])
-try:
-   print("Config File:\t" + repr(config_filename))
-except NameError:
-   print("Can't find the configuration file")
-   sys.exit(1)
+    # Print verbose arguments
+    if args.verbose:
+        print("Python Script:\t" + os.path.expandvars(sys.argv[0]))
+        print("Config File:\t"   + config_filename)
+        print("Land File:\t"     + land_filename)
+        print("Data File:\t"     + data_filename)
 
-# Distance to land file
-land_filename = os.path.expandvars(sys.argv[2])
-try:
-   print("Land File:\t" + repr(land_filename))
-except NameError:
-   print("Can't find the distance to land file")
-   sys.exit(1)
+    # Read config file and return a DriverConfig object
+    config = post_resample_driver.config_from_file(
+                 config_filename,
+                 post_resample_driver.DriverConfig)
 
-# Input NetCDF file
-data_filename = os.path.expandvars(sys.argv[3])
 
-try:
-   print("Data File:\t" + repr(data_filename))
-except NameError:
-   print("Can't find the input data file")
-   sys.exit(1)
+    # JHG, need to catch and handle exceptions here
+    # Robert will update the driver code to make sure that sufficient
+    # radii have been handed to the code and produce an exception if not
 
-# Read config file and return a DriverConfig object
-config = post_resample_driver.config_from_file(
-    config_filename, post_resample_driver.DriverConfig)
+    # Call drive to calculate the diagnostics
+    try:
+        results = post_resample_driver.diag_calcs(
+                      config, data_filename, suppress_exceptions=True,
+                      land_lut_override=land_filename)
+    except:
+        print("Error computing diagnostics with command (", ' '.join(sys.argv), ")", sep="")
+        sys.exit(1)
 
-# JHG, need to catch and handle exceptions here
-# Robert will update the driver code to make sure that sufficient
-# radii have been handed to the code and produce an exception if not
+    # Store results in STORM, SOUNDING, and CUSTOM dictionaries
+    storm_data    = {}
+    sounding_data = {}
+    custom_data   = {}
+    units         = {}
 
-# JHG, note that VMAX reported here should be renamed to MAXWIND for the diag output
-#    # Change VMAX to MAXWIND
-#    if name == "VMAX":
-#        name = "MAXWIND"
+    # Process pressure independent diagnostics
+    for d in results.pressure_independent.keys():
 
-results = post_resample_driver.diag_calcs(
-    config, data_filename, suppress_exceptions=True,
-    land_lut_override=land_filename)
+        # Diagonstic names are reported as upper-case
+        name = d.upper()
 
-# Store results in STORM, SOUNDING, and CUSTOM data dictionaries
-# based on their output file location.
-storm_data    = {}
-sounding_data = {}
-custom_data   = {}
-units         = {}
-
-# Process pressure independent diagnostics
-for d in results.pressure_independent.keys():
-
-    # Diagonstic names are reported as upper-case
-    name = d.upper()
-
-    # Store units
-    if 'units' in results.pressure_independent[d].attrs:
-        units[name] = results.pressure_independent[d].attrs["units"].upper()
-
-    # Check for bad data
-    val = results.pressure_independent[d].values[0]
-    if np.isnan(val):
-        val = BAD_DATA_INT
-
-    # Store value
-    if name in STORM_DATA_NAMES:
-        storm_data[name] = val
-    elif name in SOUNDING_DATA_NAMES:
-        sounding_data[name] = val
-    else:
-        custom_data[name] = val
-
-# Add units for sounding diagnostics
-for d in results.soundings.keys():
-
-    # Store pressure levels
-    levels = results.soundings[d].coords['level_hPa']
-
-    # Store units for diagnostic names, all upper-case
-    if 'units' in results.soundings[d].attrs:
-        units[d.upper()] = results.soundings[d].attrs["units"].upper()
-
-# Loop over the sounding pressure levels
-for idx, prs in enumerate(levels):
-
-    # Loop over the sounding diagnostics
-    for d in results.soundings.keys():
-
-        # Left-pad pressure value to 4 digits
-        name_prs = d.upper() + '_' + f'{prs:04}'
+        # Store units
+        if 'units' in results.pressure_independent[d].attrs:
+            units[name] = results.pressure_independent[d].attrs["units"].upper()
 
         # Check for bad data
-        val = results.soundings[d].values[0,idx]
+        val = results.pressure_independent[d].values[0]
         if np.isnan(val):
             val = BAD_DATA_INT
 
         # Store value
-        if d.upper() in SOUNDING_DATA_NAMES:
-            sounding_data[name_prs] = val
+        if name in SKIP_DATA_NAMES:
+            continue
+        elif name in STORM_DATA_NAMES:
+            storm_data[name] = val
+        elif name in SOUNDING_DATA_NAMES:
+            sounding_data[name] = val
         else:
-            custom_data[name_prs] = val
+            custom_data[name] = val
 
-# Print dictionaries
-print(f"\nSTORM DATA ({len(storm_data)}):\n", storm_data)
-print(f"\nSOUNDING DATA ({len(sounding_data)}):\n", sounding_data)
-print(f"\nCUSTOM DATA ({len(custom_data)}):\n", custom_data)
-print(f"\nUNITS ({len(units)}):\n", units, "\n")
+    # Add units for sounding diagnostics
+    for d in results.soundings.keys():
+
+        # Store pressure levels
+        levels = results.soundings[d].coords['level_hPa']
+
+        # Store units for diagnostic names, all upper-case
+        if 'units' in results.soundings[d].attrs:
+            units[d.upper()] = results.soundings[d].attrs["units"].upper()
+
+    # Loop over the sounding pressure levels
+    for idx, prs in enumerate(levels):
+
+        # Loop over the sounding diagnostics
+        for d in results.soundings.keys():
+
+            # Left-pad pressure value to 4 digits
+            name_prs = d.upper() + '_' + f'{prs.data:04}'
+
+            # Check for bad data
+            val = results.soundings[d].values[0,idx]
+            if np.isnan(val):
+                val = BAD_DATA_INT
+
+            # Store value
+            if d.upper() in SOUNDING_DATA_NAMES:
+                sounding_data[name_prs] = val
+            else:
+                custom_data[name_prs] = val
+
+    # Print verbose dictionary contents
+    if args.verbose:
+        print(f"\nSTORM DATA ({len(storm_data)}) =", storm_data)
+        print(f"\nSOUNDING DATA ({len(sounding_data)}) =", sounding_data)
+        print(f"\nCUSTOM DATA ({len(custom_data)}) =", custom_data)
+        print(f"\nUNITS ({len(units)}) =", units, "\n")
+
+def _get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Compute tropical cyclone diagnostics for a single storm location."
+    )
+    parser.add_argument(
+        "config_file",
+        type=pathlib.Path,
+        help="YAML config file specifying how to process diag vars.",
+    )
+    parser.add_argument(
+        "land_file",
+        type=pathlib.Path,
+        help="ASCII distance to land file.",
+    )
+    parser.add_argument(
+        "data_file",
+        type=pathlib.Path,
+        help="NetCDF file containing model data resampled to cylindrical grid.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Print verbose log messages.",
+    )
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
