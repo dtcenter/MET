eee49469acacf6f6ed5f9dcbb6ed83b137226752
John Halley Gotway <johnhg@ucar.edu>
2023-12-12 17:27:36 +0000

    Per #2761, update output from Bison to GNU Bison 3.8.2, which is the version used by Debian Bookworm on our devleopment machine, seneca.

--- src/basic/vx_config/config.tab.h
+++ src/basic/vx_config/config.tab.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.3.2.  */
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2019 Free Software Foundation,
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
    Inc.
 
    This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,8 +31,9 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Undocumented macros, especially those whose name start with YY_,
-   are private implementation details.  Do not rely on them.  */
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
 
 #ifndef YY_CONFIG_CONFIG_TAB_H_INCLUDED
 # define YY_CONFIG_CONFIG_TAB_H_INCLUDED
@@ -44,31 +45,40 @@
 extern int configdebug;
 #endif
 
-/* Token type.  */
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    IDENTIFIER = 258,
-    QUOTED_STRING = 259,
-    INTEGER = 260,
-    FLOAT = 261,
-    BOOLEAN = 262,
-    COMPARISON = 263,
-    NA_COMPARISON = 264,
-    LOGICAL_OP_NOT = 265,
-    LOGICAL_OP_AND = 266,
-    LOGICAL_OP_OR = 267,
-    FORTRAN_THRESHOLD = 268,
-    BUILTIN = 269,
-    LOCAL_VAR = 270,
-    SIMPLE_PERC_THRESH = 271,
-    USER_FUNCTION = 272,
-    PRINT = 273,
-    UNARY_MINUS = 274
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    IDENTIFIER = 258,              /* IDENTIFIER  */
+    QUOTED_STRING = 259,           /* QUOTED_STRING  */
+    INTEGER = 260,                 /* INTEGER  */
+    FLOAT = 261,                   /* FLOAT  */
+    BOOLEAN = 262,                 /* BOOLEAN  */
+    COMPARISON = 263,              /* COMPARISON  */
+    NA_COMPARISON = 264,           /* NA_COMPARISON  */
+    LOGICAL_OP_NOT = 265,          /* LOGICAL_OP_NOT  */
+    LOGICAL_OP_AND = 266,          /* LOGICAL_OP_AND  */
+    LOGICAL_OP_OR = 267,           /* LOGICAL_OP_OR  */
+    FORTRAN_THRESHOLD = 268,       /* FORTRAN_THRESHOLD  */
+    BUILTIN = 269,                 /* BUILTIN  */
+    LOCAL_VAR = 270,               /* LOCAL_VAR  */
+    SIMPLE_PERC_THRESH = 271,      /* SIMPLE_PERC_THRESH  */
+    USER_FUNCTION = 272,           /* USER_FUNCTION  */
+    PRINT = 273,                   /* PRINT  */
+    UNARY_MINUS = 274              /* UNARY_MINUS  */
   };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
-/* Tokens.  */
+/* Token kinds.  */
+#define YYEMPTY -2
+#define YYEOF 0
+#define YYerror 256
+#define YYUNDEF 257
 #define IDENTIFIER 258
 #define QUOTED_STRING 259
 #define INTEGER 260
@@ -89,10 +99,9 @@ extern int configdebug;
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-
 union YYSTYPE
 {
-#line 191 "config.tab.yy" /* yacc.c:1921  */
+#line 191 "config.tab.yy"
 
 
    char text[max_id_length + 1];
@@ -112,9 +121,9 @@ union YYSTYPE
    PC_info pc_info;
 
 
-#line 116 "config.tab.h" /* yacc.c:1921  */
-};
+#line 125 "config.tab.h"
 
+};
 typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
@@ -123,6 +132,8 @@ typedef union YYSTYPE YYSTYPE;
 
 extern YYSTYPE configlval;
 
+
 int configparse (void);
+
 
 #endif /* !YY_CONFIG_CONFIG_TAB_H_INCLUDED  */
