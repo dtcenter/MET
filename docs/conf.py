ae2475ef89f8aaab94a710f49774d3373cb3d00f
Julie Prestopnik <jpresto@ucar.edu>
2023-12-13 11:17:51 -0700

    Update conf.py
    Adding 'sphinx_rtd_theme',

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ copyright = f'{release_year}, {author}'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # Adding 'sphinx_panels' to use drop-down menus in appendixA. 
-extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx','sphinx_panels',]
+extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx','sphinx_panels','sphinx_rtd_theme',]
 
 # settings for ReadTheDocs PDF creation
 latex_engine = 'pdflatex'
