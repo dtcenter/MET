7392cb16e8da4b802ccc21d35857ae546b07cf3c
Natalie babij <natalie.babij@natbabij-mac1.home>
2024-02-05 21:58:53 -0500

    progress on main function (putting together test commands)

--- internal/test_unit/python/unit.py
+++ internal/test_unit/python/unit.py
@@ -4,6 +4,7 @@
 #add imports...
 #import lxml     #note: developing with v 4.9.3
 import os
+from pathlib import Path
 import re
 import xml.etree.ElementTree as ET
 
@@ -58,10 +59,8 @@ def unit(test_xml, file_log=None, cmd_only=False, noexit=False, memchk=False, ca
         if true, activate valgrind with callcheck
     """
 
-    # read command arguments
-
-    # parse the test xml file parameter
-
+    # read command arguments  --> could do this in if "name"="main"
+   
     # # if command  only mode is enabled, disable logging
     # $cmd_only and $file_log = 0;
 
@@ -71,128 +70,152 @@ def unit(test_xml, file_log=None, cmd_only=False, noexit=False, memchk=False, ca
 
     
     # parse xml file
-    test_root = ET.parse(test_xml)
+    try:
+        test_root = ET.parse(test_xml)
+    except e:
+        print(e)
+        # and exit?
 
-    # # report any error that stopped parsing
-    # if( $@ ){
-    #   $@ =~ s/at \/.*?$//s;
-    #   die "ERROR: parsing error $@\n";
-    # }
 
     # # parse the children of the met_test element
-    # $tree->[0] eq "met_test" or die "ERROR: unexpected top-level element " . $tree->[0] . "\n";
-    # my @tests = build_tests( @{ $tree->[1] } );
-    tests = build_tests(test_root)
+    if test_root.getroot().tag != 'met_test':
+        print(f"ERROR: unexpected top-level element. Expected 'met_test', got '{test_root.tag}'")
+        # and exit?
+    else:
+        tests = build_tests(test_root)
 
-    return tests
-
-# # determine the max length of the test names
-# my $name_wid = 12;
-# $name_wid < length($_->{"name"}) and $name_wid = length($_->{"name"}) for @tests;
-
-# # default return value
-# my $ret_val = 0;
-# my $VALGRIND_OPT_MEM ="--leak-check=full --show-leak-kinds=all --error-limit=no -v";
-# my $VALGRIND_OPT_CALL ="--tool=callgrind --dump-instr=yes --simulate-cache=yes --collect-jumps=yes";
-
-
-# # run each test
-# for my $test (@tests){
-
-#   # print the test name
-#   $cmd_only or printf "TEST: %-*s - ", $name_wid, $test->{"name"};
-
-#   # prepare the output space
-#   my @outputs = ( @{$test->{"out_pnc"}}, @{$test->{"out_gnc"}}, @{$test->{"out_stat"}}, @{$test->{"out_ps"}}, @{$test->{"out_exist"}}, @{$test->{"out_not_exist"}} );
-#   for my $output ( @outputs ){
-#     -s $output and qx/rm -rf $output/;
-#     vx_file_mkdir($output);
-#   }
-
-#   # set the test environment variables
-#   $ENV{$_} = $test->{"env"}{$_} for keys %{ $test->{"env"} };
-
-#   # build the text command
-#   my ($cmd, $ret_ok, $out_ok) = ($test->{"exec"} . $test->{"param"});
-#   $cmd =~ s/[ \n\t]+$//m;
-#   if ($memchk) {
-#     $cmd = "valgrind ".$VALGRIND_OPT_MEM." ".$cmd;
-#   }
-#   elsif ($callchk) {
-#     $cmd = "valgrind ".$VALGRIND_OPT_CALL." ".$cmd;
-#   }
-
-#   # if writing a command file, print the environment and command, then loop
-#   if( $cmd_only ){
-#     print "export $_=\'" . $test->{"env"}{$_} . "\'\n" for sort keys %{ $test->{"env"} };
-#     print "$cmd\n";
-#     print "unset $_\n" for sort keys %{ $test->{"env"} };
-#     print "\n";
-#     next;
-#   }
-  
-#   # run and time the test command
-#   my $t_start = [gettimeofday];
-#   my @cmd_outs = qx{$cmd 2>&1};
-#   my $t_elaps = tv_interval( $t_start );
-#   unshift @cmd_outs, "$cmd\n";
-
-#   # check the return status and output files
-#   $ret_ok = (! $?);
-#   if( $ret_ok ){
-#     $out_ok = 1;
-#     do{ push @cmd_outs, qx/$mpnc -v $_/; $? and $out_ok = 0 } for ( @{ $test->{"out_pnc"} } );
-#     do{ push @cmd_outs, qx/$mgnc -v $_/; $? and $out_ok = 0 } for ( @{ $test->{"out_gnc"} } );
-#     for my $output ( @{ $test->{"out_stat"} } ){
-#       -s $output or 
-#         push @cmd_outs, "ERROR: stat file missing \"$output\"\n" and $out_ok = 0 and next;
-#       int(qx{cat $output 2>/dev/null | egrep -v '^VERSION' | wc -l}) or
-#         push @cmd_outs, "ERROR: stat data missing from file \"$output\"\n" and $out_ok = 0;
-#     }
-#     for my $output ( @{ $test->{"out_ps"} } ){
-#       -s $output or 
-#         push @cmd_outs, "ERROR: postscript file missing \"$output\"\n" and $out_ok = 0 and next;
-#       !(qx{gs -sDEVICE=nullpage -dQUIET -dNOPAUSE -dBATCH $output 2>/dev/null}) or
-#         push @cmd_outs, "ERROR: ghostscript error for postscript file \"$output\"\n" and $out_ok = 0;
-#     }
-#     for my $output ( @{ $test->{"out_exist"} } ){
-#       -s $output or 
-#         push @cmd_outs, "ERROR: file missing when it should exist \"$output\"\n" and $out_ok = 0 and next;
-#     }
-#     for my $output ( @{ $test->{"out_not_exist"} } ){
-#       !(-s $output) or 
-#         push @cmd_outs, "ERROR: file exists when it should be missing \"$output\"\n" and $out_ok = 0 and next;
-#     }
+    # # determine the max length of the test names
+    name_wid = max([len(test['name']) for test in tests])
     
-#   }
+    
 
-#   # unset the test environment variables
-#   delete $ENV{$_} for keys %{ $test->{"env"} };
-   
-#   # print the test result
-#   printf "%s - %7.3f sec\n", $ret_ok && $out_ok ? "pass" : "FAIL", sprintf("%7.3f", $t_elaps);
+    # # default return value
+    # my $ret_val = 0;
+    VALGRIND_OPT_MEM ="--leak-check=full --show-leak-kinds=all --error-limit=no -v"
+    VALGRIND_OPT_CALL ="--tool=callgrind --dump-instr=yes --simulate-cache=yes --collect-jumps=yes"
 
-#   # build a list of environment variable exports and unsets for reporting
-#   my @set_envs;
-#   push @set_envs, "export $_=\'" . $test->{"env"}{$_} . "\'\n" for sort keys %{ $test->{"env"} };
-#   my @unset_envs;
-#   push @unset_envs, "unset $_\n" for sort keys %{ $test->{"env"} };
+    # # run each test
+    for test in tests:
+    #   # print the test name
+        print(f"TEST: {test['name']}")
+    #   $cmd_only or printf "TEST: %-*s - ", $name_wid, $test->{"name"};
 
-#   # if the log file is activated, print the test information
-#   if( $file_log ){
-#     print $fh_log "\n\n";
-#     print $fh_log "$_" for (@set_envs, @cmd_outs, @unset_envs);
-#     print $fh_log "\n\n";
-#   }
+    #   # prepare the output space
+        output_keys = [key for key in test.keys() if key.startswith('out_')]
+        outputs = [test[key] for key in output_keys]
+        for output in outputs:
+            try:
+                Path(output).unlink()
+            except FileNotFoundError:
+                pass
+            except e:
+                print(e)
+            # calls a perl function from VxUtil.pm (in test_unit/lib) to make the directory 
+            output_dir = Path(output).parent
+            output_dir.mkdir(parents=True)     #should error be raised if dir already exists? 
 
-#   # on failure, print the problematic test and exit, if requested
-#   if( !($ret_ok && $out_ok) ){
-#     print "$_" for (@set_envs, @cmd_outs, @unset_envs);
-#     $noexit or exit 1;
-#     print "\n\n";
-#   }
+    #   my @outputs = ( @{$test->{"out_pnc"}}, @{$test->{"out_gnc"}}, @{$test->{"out_stat"}}, @{$test->{"out_ps"}}, @{$test->{"out_exist"}}, @{$test->{"out_not_exist"}} );
+    #   for my $output ( @outputs ){
+    #     -s $output and qx/rm -rf $output/;
+    #     vx_file_mkdir($output);
+    #   }
+        # sub vx_file_mkdir {
+        #   while( my $strFile = shift ){
+        #     (my $strDir = $strFile) =~ s/(.*\/).*/$1/;
+        #     qx/mkdir -p $strDir/;
+        #   }
+        # }
 
-# }
+    #   # set the test environment variables
+        if 'env' in test.keys():
+            for key, val in test['env'].items():
+                if val:
+                    os.environ[key] = val
+
+    #   # build the text command
+        cmd = test['exec'] + test['param']
+        cmd = re.sub('[ \n\t]+$', '', cmd)  # not sure this is doing what it should;
+                                            #  may need to remove the +$ from the regex?
+    #   my ($cmd, $ret_ok, $out_ok) = ($test->{"exec"} . $test->{"param"});
+        if memchk:
+            cmd = f"valgrind {VALGRIND_OPT_MEM} {cmd}"
+        elif callchk:
+            cmd = f"valgrind {VALGRIND_OPT_CALL} {cmd}"
+        
+    
+
+    #   # if writing a command file, print the environment and command, then loop
+    #   if( $cmd_only ){
+    #     print "export $_=\'" . $test->{"env"}{$_} . "\'\n" for sort keys %{ $test->{"env"} };
+    #     print "$cmd\n";
+    #     print "unset $_\n" for sort keys %{ $test->{"env"} };
+    #     print "\n";
+    #     next;
+    #   }
+    
+    #   # run and time the test command
+    #   my $t_start = [gettimeofday];
+    #   my @cmd_outs = qx{$cmd 2>&1};
+    #   my $t_elaps = tv_interval( $t_start );
+    #   unshift @cmd_outs, "$cmd\n";
+
+    #   # check the return status and output files
+    #   $ret_ok = (! $?);
+    #   if( $ret_ok ){
+    #     $out_ok = 1;
+    #     do{ push @cmd_outs, qx/$mpnc -v $_/; $? and $out_ok = 0 } for ( @{ $test->{"out_pnc"} } );
+    #     do{ push @cmd_outs, qx/$mgnc -v $_/; $? and $out_ok = 0 } for ( @{ $test->{"out_gnc"} } );
+    #     for my $output ( @{ $test->{"out_stat"} } ){
+    #       -s $output or 
+    #         push @cmd_outs, "ERROR: stat file missing \"$output\"\n" and $out_ok = 0 and next;
+    #       int(qx{cat $output 2>/dev/null | egrep -v '^VERSION' | wc -l}) or
+    #         push @cmd_outs, "ERROR: stat data missing from file \"$output\"\n" and $out_ok = 0;
+    #     }
+    #     for my $output ( @{ $test->{"out_ps"} } ){
+    #       -s $output or 
+    #         push @cmd_outs, "ERROR: postscript file missing \"$output\"\n" and $out_ok = 0 and next;
+    #       !(qx{gs -sDEVICE=nullpage -dQUIET -dNOPAUSE -dBATCH $output 2>/dev/null}) or
+    #         push @cmd_outs, "ERROR: ghostscript error for postscript file \"$output\"\n" and $out_ok = 0;
+    #     }
+    #     for my $output ( @{ $test->{"out_exist"} } ){
+    #       -s $output or 
+    #         push @cmd_outs, "ERROR: file missing when it should exist \"$output\"\n" and $out_ok = 0 and next;
+    #     }
+    #     for my $output ( @{ $test->{"out_not_exist"} } ){
+    #       !(-s $output) or 
+    #         push @cmd_outs, "ERROR: file exists when it should be missing \"$output\"\n" and $out_ok = 0 and next;
+    #     }
+        
+    #   }
+
+    #   # unset the test environment variables
+    #   delete $ENV{$_} for keys %{ $test->{"env"} };
+    
+    #   # print the test result
+    #   printf "%s - %7.3f sec\n", $ret_ok && $out_ok ? "pass" : "FAIL", sprintf("%7.3f", $t_elaps);
+
+    #   # build a list of environment variable exports and unsets for reporting
+    #   my @set_envs;
+    #   push @set_envs, "export $_=\'" . $test->{"env"}{$_} . "\'\n" for sort keys %{ $test->{"env"} };
+    #   my @unset_envs;
+    #   push @unset_envs, "unset $_\n" for sort keys %{ $test->{"env"} };
+
+    #   # if the log file is activated, print the test information
+    #   if( $file_log ){
+    #     print $fh_log "\n\n";
+    #     print $fh_log "$_" for (@set_envs, @cmd_outs, @unset_envs);
+    #     print $fh_log "\n\n";
+    #   }
+
+    #   # on failure, print the problematic test and exit, if requested
+    #   if( !($ret_ok && $out_ok) ){
+    #     print "$_" for (@set_envs, @cmd_outs, @unset_envs);
+    #     $noexit or exit 1;
+    #     print "\n\n";
+    #   }
+
+    # }
+    return tests
 
 
 def build_tests(test_root):
@@ -225,15 +248,17 @@ def build_tests(test_root):
             if (el.tag=='exec' or el.tag=='param'):
                 test[el.tag] = repl_env(el.text)
             elif el.tag=='output':
+                output_names = {
+                    'point_nc' : 'out_pnc',
+                    'grid_nc' : 'out_gnc',
+                    'stat' : 'out_stat',
+                    'ps' : 'out_ps',
+                    'exist' : 'out_exist',
+                    'not_exist' : 'out_not_exist',
+                }
                 for output_el in el:
-                    test[output_el.tag] = repl_env(output_el.text)
-                    # these keys need to be renamed, e.g. point_nc --> out_pnc
-                    #           "point_nc"  : "out_pnc"
-                    #           "grid_nc"   : "out_gnc"
-                    #           "stat"      : "out_stat"
-                    #           "ps"        : "out_ps"
-                    #           "exist"     : "out exist"
-                    #           "not_exist" : "out_not_exist"
+                    test[output_names[output_el.tag]] = repl_env(output_el.text)
+
             elif el.tag=='env':
                 env_dict = {}
                 for env_el in el:
