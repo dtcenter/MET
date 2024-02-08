0c060fac11268be75821f09d34a37e5c5b93c0b4
John Halley Gotway <johnhg@ucar.edu>
2024-02-08 08:51:16 -0700

    Strip out the docker commands since docker is NOT installed inside the MET image.

--- .github/jobs/free_disk_space.sh
+++ .github/jobs/free_disk_space.sh
@@ -49,24 +49,3 @@ printf "\nChecking disk usage after cleanup"
 
 df -h
 
-echo Pruning docker files
-
-cmd="docker images"
-printf "\nBEFORE CLEANUP: $cmd"
-$cmd
-
-cmd="docker image prune -af"
-printf "\nRunning $cmd"
-$cmd
-
-cmd=docker system prune -af
-printf "\nRunning $cmd"
-$cmd
-
-cmd="docker images"
-printf "\nAFTER CLEANUP: $cmd"
-$cmd
-
-printf "\nChecking disk usage after Docker cleanup"
-
-df -h
