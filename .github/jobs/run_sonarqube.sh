e514166dc6bc1b1f2c16b3acae17923f0dc173a6
John Halley Gotway <johnhg@ucar.edu>
2024-03-15 18:49:01 -0600

    Per #2379, see details of progress described in this issue comment: https://github.com/dtcenter/MET/issues/2379#issuecomment-2000242425

---
+++ .github/jobs/run_sonarqube.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
+
+DOCKERHUB_TAG=$(get_dockerhub_tag)
+
+DOCKERFILE_PATH=${GITHUB_WORKSPACE}/internal/scripts/docker/Dockerfile.copy
+
+CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build.log
+
+time_command docker build -t ${DOCKERHUB_TAG} \
+    --build-arg SOURCE_BRANCH \
+    --build-arg MET_BASE_REPO \
+    --build-arg MET_BASE_TAG \
+    --build-arg MET_CONFIG_OPTS \
+    -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}
+if [ $? != 0 ]; then
+  cat ${GITHUB_WORKSPACE}/docker_build.log
+  exit 1
+fi
+
+# Copy the log directory from the image
+id=$(docker create ${DOCKERHUB_TAG})
+time_command docker cp $id:/met/logs met_logs
+mv met_logs/*.log ${GITHUB_WORKSPACE}/.
+docker rm -v $id
---
+++ internal/scripts/docker/build_met_sonarqube.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Run SonarQube Source Code Analyzer within a docker container
+#=======================================================================
+#
+# This run_sonarqube_docker.sh script must be run from the top-level
+# directory of the MET repository to be analyzed. It runs the SonarQube
+# Source Code Analyzer on it.
+#
+# Usage: internal/scripts/sonarqube/run_sonarqube_docker.sh
+#
+#=======================================================================
+
+# Check that this is being run from the top-level MET directory
+if [ ! -e internal/scripts/docker/build_met_sonarqube.sh ]; then
+  echo "ERROR: ${0} must be run from the top-level MET directory"
+  exit 1
+fi
+
+# Source the docker build environment
+source ~/.bashrc
+source internal/scripts/environment/development.docker 
+source .github/jobs/bash_functions.sh
+
+echo PATH=${PATH}
+
+# Locate the wrapper
+WRAPPER_NAME=build-wrapper-linux-x86-64
+SONAR_WRAPPER=$(which $WRAPPER_NAME 2> /dev/null)
+
+if [ ! -e $SONAR_WRAPPER ]; then
+  echo "ERROR: $WRAPPER_NAME not found in the path"
+  exit 1
+else
+  echo "SONAR_WRAPPER=$SONAR_WRAPPER"
+fi
+
+# Locate the scanner
+SCANNER_NAME=sonar-scanner
+SONAR_SCANNER=$(which $SCANNER_NAME 2> /dev/null)
+
+if [ ! -e $SONAR_SCANNER ]; then
+  echo "ERROR: $SCANNER_NAME not found in the path"
+  exit 1
+else
+  echo "SONAR_SCANNER=$SONAR_SCANNER"
+fi
+
+# Set output directory name
+if [ -z ${SONARQUBE_OUT_DIR} ]; then
+  export SONARQUBE_OUT_DIR=bw-outputs
+fi
+
+# Store the full path to the scripts directory
+SONAR_PROPERTIES_DIR=internal/scripts/sonarqube
+SONAR_PROPERTIES=sonar-project.properties
+
+# Copy sonar-project.properties for C/C++ code
+[ -e $SONAR_PROPERTIES ] && rm $SONAR_PROPERTIES
+sed -e "s|SONAR_TOKEN_VALUE|$SONAR_TOKEN_VALUE|" -e "s|SONAR_SERVER_URL|$SONAR_SERVER_URL|" $SONAR_PROPERTIES_DIR/sonar-project.properties > $SONAR_PROPERTIES
+
+# Run the configure script
+time_command ./configure --prefix=`pwd` --enable-all
+
+# Run make clean
+time_command make clean
+
+# Run SonarQube make
+time_command $SONAR_WRAPPER --out-dir $SONARQUBE_OUT_DIR make
+
+# Run SonarQube scan for C/C++ code
+time_command $SONAR_SCANNER
+
+[ -e $SONAR_PROPERTIES ] && rm $SONAR_PROPERTIES
