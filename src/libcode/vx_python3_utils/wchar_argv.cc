342358dae6985bb3a8af1d1e135510d776415c13
John Halley Gotway <johnhg@ucar.edu>
2024-10-17 18:38:23 +0000

    Per #2673, switching to 2 more vectors

--- src/libcode/vx_python3_utils/wchar_argv.cc
+++ src/libcode/vx_python3_utils/wchar_argv.cc
@@ -146,35 +146,28 @@ for (int j=0; j<(a.n()); ++j)  {
 
 int N = len + a.n();
 
-char * s = new char [N];
-
-char ** av = new char * [a.n()];
-
-memset(s, 0, N);
+vector<char> s(N, 0);
+vector<char *> av(a.n(), nullptr);
 
 for (int j=0; j<(a.n()); ++j)  {
-
-   av[j] = s + k;
 
    c = a[j].c_str();
 
    len = c.length();
 
-   m_strncpy(s + k, c.text(), len, method_name);
+   m_strncpy(s.data() + k, c.text(), len, method_name);
+
+   av[j] = s.data() + k;
 
    k += (len + 1);
 
 }
 
-set(a.n(), av);
+set(a.n(), av.data());
 
    //
    //  done
    //
-
-if ( s )  { delete [] s;  s = nullptr; }
-
-if ( av )  { delete [] av;  av = nullptr; }
 
 return;
 
@@ -206,7 +199,7 @@ for (int j=0; j<_argc; ++j)  {
    // we're using the len array here because
    // we don't want to call m_strlen more than
    // once on each argv value
-   if (_argv && _argv[j]) len[j] = m_strlen(_argv[j]);
+   if (_argv) len[j] = m_strlen(_argv[j]);
 
    argv_len += len[j];
 
