2fe55ca2829976ca41defe059395450f52ba4c6d
John Halley Gotway <johnhg@ucar.edu>
2024-10-17 15:27:28 +0000

    Per #2673, remove m_strlen() entirely.

--- src/libcode/vx_python3_utils/wchar_argv.cc
+++ src/libcode/vx_python3_utils/wchar_argv.cc
@@ -206,7 +206,10 @@ for (int j=0; j<_argc; ++j)  {
    // we're using the len array here because
    // we don't want to call m_strlen more than
    // once on each argv value
-   if (_argv) len[j] = m_strlen(_argv[j]);
+   if (_argv) {
+      string arg_str = _argv[j];
+      len[j] = arg_str.length();
+   }
 
    argv_len += len[j];
 
