349600f172da6b8ecc4a3ebf64edb6ee24cd086d
John Halley Gotway <johnhg@ucar.edu>
2025-01-24 16:41:01 -0700

    Feature #3037 v12.1.0-beta1 (#3062)
    * Unrelated to #3037, update the Python embedding Appendix to list gen_ens_prod.
    
    * Per #3037, add 12.1.0-beta1 release notes.
    
    * Per #3037, roll the copyright date from 2024 to 2025
    
    * Per #3037, proposing this updated structure and content for the upgrade instructions
    
    * Per #3037, add upgrade instruction details.
    
    * Per #3037, tweak wording
    
    * Per #3037, updating format of intended bulleted list
    
    ---------
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>

--- src/libcode/vx_gnomon/gnomon.h
+++ src/libcode/vx_gnomon/gnomon.h
@@ -1,5 +1,5 @@
 // *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-// ** Copyright UCAR (c) 1992 - 2024
+// ** Copyright UCAR (c) 1992 - 2025
 // ** University Corporation for Atmospheric Research(UCAR)
 // ** National Center for Atmospheric Research(NCAR)
 // ** Research Applications Lab (RAL)
