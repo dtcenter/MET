24d3d35d3841a571de930ee09abcd7905a0b91ef
John Halley Gotway <johnhg@ucar.edu>
2025-02-25 22:42:20 +0000

    Unrelated to #3064, add met_version to the standard Start and Finish DEBUG(1) log messages.

--- src/basic/vx_util/main.cc
+++ src/basic/vx_util/main.cc
@@ -85,18 +85,18 @@ int main(int argc, char *argv[]) {
 ////////////////////////////////////////////////////////////////////////
 
 void do_pre_process(int argc, char *argv[]) {
-   ConcatString msg, msg2;
 
    store_arguments(argc, argv);
 
    set_user_id();
    met_tool_name = get_tool_name();
 
-   msg << "Start " << met_tool_name << " " << met_version << " by "
-       << met_user_name << "(" << met_user_id << ") at "
-       << get_current_time();
-   msg2 << "  cmd: " << met_cmdline;
-   mlog << Debug(1) << msg << msg2 << "\n";
+   ConcatString msg;
+   msg << "Start " << met_tool_name << " " << met_version
+       << " by " << met_user_name << "(" << met_user_id
+       << ") at " << get_current_time()
+       << " with command: " << met_cmdline;
+   mlog << Debug(1) << msg << "\n";
 
    set_handlers();
 }
@@ -105,9 +105,9 @@ void do_pre_process(int argc, char *argv[]) {
 
 void do_post_process() {
    ConcatString msg;
-   msg << "Finish " << met_tool_name << " " << met_version << " by "
-       << met_user_name << "(" << met_user_id << ") at "
-       << get_current_time();
+   msg << "Finish " << met_tool_name << " " << met_version
+       << " by " << met_user_name << "(" << met_user_id
+       << ") at " << get_current_time();
    mlog << Debug(1) << msg << "\n";
 }
 
