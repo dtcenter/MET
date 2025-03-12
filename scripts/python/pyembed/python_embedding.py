b90400e6160bdfa05001ee3e825b87f3b1270273
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-03-12 12:19:23 -0600

    Revert "per PR feedback from @hsoh-u, removing duplicate class and instead import it from the other location"
    This reverts commit b489e799fd8cf8ea9606eb98c7ed4c86bdf97956.

--- scripts/python/pyembed/python_embedding.py
+++ scripts/python/pyembed/python_embedding.py
@@ -22,9 +22,18 @@ import sys
 import json
 import math
 from importlib import util as import_util
+import numpy as np
 
-from met.logger import NumpyTypeEncoder
-
+# define JSON encoder to convert numpy types to python
+# note: this also exists in python.met.logger
+# can this be imported instead of redefined?
+class NumpyTypeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.generic):
+            return obj.item()
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
 class pyembed_tools():
 
