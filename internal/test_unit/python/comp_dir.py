4fbb50e3ca1e4eae1213cbc946d1ff70d6698214
Natalie Babij <nbabij@seneca.rap.ucar.edu>
2025-03-19 17:32:14 +0000

    fixed saving of diff files (sort of)

--- internal/test_unit/python/comp_dir.py
+++ internal/test_unit/python/comp_dir.py
@@ -21,6 +21,33 @@ from diff_util import compare_dir
 # Assume the command run is ./comp_dir.py MET-${1}/test_output MET-${2}/test_output
 
 def comp_dir(truth_dir, output_dir, diff_dir='', debug=True, save_diff=True):
+    """
+    Compare test output between two MET versions.
+    
+    Essentially a wrapper for compare_dir function from METplus diff_util.
+    Runs compare_dir on test and ref directories. If differences are found,
+    copies relevant files into a new diff directory.
+
+    Parameters
+    -----------
+    truth_dir: path-like
+        Directory containing test output from reference version
+    output_dir: path-like
+        Directory containing test output from test version
+    diff_dir: path-like, default=''
+        Directory where to save files containing differences between two versions
+        If not provided, will be set to output_dir/diff
+    debug: bool, default=True
+        More verbose option (based on settings of compare_dir function)
+    save_diff: bool, default=True
+        Option to save diff files (based on settings of compare_dir function)
+    
+    Returns
+    -------
+    None
+
+    """
+
     print('******************************')
     print("Comparing output to truth data")
     diff_files = compare_dir(truth_dir, output_dir,
@@ -30,25 +57,30 @@ def comp_dir(truth_dir, output_dir, diff_dir='', debug=True, save_diff=True):
     # copy difference files into directory
     # so it can be easily downloaded and compared
     if diff_files:
+        if not diff_dir:
+            diff_dir = os.path.join(output_dir, 'diff')
+
         for truth_file, out_file, _, diff_file in diff_files:
             if truth_file:
-                data_dir = truth_dir
-                label = '_truth'
+                copy_to_diff_dir(truth_file, '_truth', truth_dir, diff_dir)
+                #data_dir = truth_dir
+                #label = '_truth'
             if out_file:
-                data_dir = output_dir
-                label = '_output'
+                copy_to_diff_dir(out_file, '_output', output_dir, diff_dir)
+                # data_dir = output_dir
+                # label = '_output'
             if diff_file:
-                data_dir = output_dir
-                label = ''
+                copy_to_diff_dir(diff_file, '', output_dir, diff_dir)
+                # data_dir = output_dir
+                # label = ''
 
-    if not diff_dir:
-        os.path.join(output_dir, 'diff')
-
+    
+def copy_to_diff_dir(file_path, file_label, data_dir, diff_dir):
     # replace data dir with diff directory
     # add data type identifier to filename before extension (blank for diff output)
     diff_out = file_path.replace(data_dir, diff_dir)
     output_path, extension = os.path.splitext(diff_out)
-    output_path = f'{output_path}{label}{extension}'
+    output_path = f'{output_path}{file_label}{extension}'
 
     # create output directory if it doesn't exist
     output_dir = os.path.dirname(output_path)
@@ -58,10 +90,7 @@ def comp_dir(truth_dir, output_dir, diff_dir='', debug=True, save_diff=True):
     try:
         shutil.copyfile(file_path, output_path)
     except OSError as err:
-        print(f'Could not copy file. {err}')
-        return False
-
-    return True                                
+        print(f'Could not copy file {file_path} to {output_path}. {err}')                            
 
 
     #=================================================
@@ -291,17 +320,18 @@ if __name__ == "__main__":
     # Assume the command run is ./comp_dir.py MET-${1}/test_output MET-${2}/test_output
     parser = argparse.ArgumentParser(description="Compare output files between two directories.")
     parser.add_argument('dir_1',
-                        help='"truth" or "ref" version test_output')
-    parser.add_argument('dir_2')
+                        help='"truth" or "ref" version test_output directory')
+    parser.add_argument('dir_2',
+                        help='"test" version test_output directory')
     #note: the args below are not currently supported
     parser.add_argument('-v', default=1, choices=[0,1,2,3],
-                        help='indicates verbosity level (0-3), default 1')
+                        help='indicates verbosity level (0-3), default 1 (not currently supported)')
     parser.add_argument('-hist', default=0, choices=[0,1],
-                        help='1 to produce histogram error plots for each file')
+                        help='1 to produce histogram error plots for each file (not currently supported)')
     parser.add_argument('-nc_var', action='store_true',
-                        help='if present, compare NetCDF variables')
+                        help='if present, compare NetCDF variables (not currently supported)')
     parser.add_argument('-strict', action='store_true',
-                        help='applies strict equality when comparing numerical values')
+                        help='applies strict equality when comparing numerical values (not currently supported)')
     args = parser.parse_args()
 
     comp_dir(truth_dir=args.dir_1, output_dir=args.dir_2)
