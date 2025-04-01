02611f204f8447e7abca44cd756e47da550caf9b
John Halley Gotway <johnhg@ucar.edu>
2025-04-01 12:55:56 -0600

    Per #3100, version number and release notes updates for v12.1.0-beta2.

--- docs/conf.py
+++ docs/conf.py
@@ -20,11 +20,11 @@ print(sys.path)
 project = 'MET'
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Prestopnik, J., H. Soh, L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz, T. Jensen'
-version = '12.1.0-beta1'
+version = '12.1.0-beta2'
 verinfo = version
 release = f'{version}'
 release_year = '2025'
-release_date = f'{release_year}-01-24'
+release_date = f'{release_year}-04-01'
 copyright = f'{release_year}, {author}'
 
 # -- General configuration ---------------------------------------------------
