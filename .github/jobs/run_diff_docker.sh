331a5ce7301ddcce2d9e199a015ba572375bfdd3
Natalie Babij <nbabij@seneca.rap.ucar.edu>
2025-05-18 04:39:16 +0000

    updated github diff scripts to run metplus-envs:diff container

--- .github/jobs/run_diff_docker.sh
+++ .github/jobs/run_diff_docker.sh
@@ -2,7 +2,9 @@
 
 source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 
-DOCKERHUB_TAG=$(get_dockerhub_tag)
+#DOCKERHUB_TAG=$(get_dockerhub_tag)
+VERSION_EXT=.v6.1     #Note: this comes from METplus/.github/jobs/docker_utils.py
+DOCKERHUB_TAG=dtcenter/metplus-envs:diff${VERSION_EXT}
 
 # Get truth output data
 ${GITHUB_WORKSPACE}/.github/jobs/get_test_truth_data.sh ${TRUTH_DATA_VERSION}
@@ -17,20 +19,28 @@ DOCKER_DIFF_DIR=/data/output/met_test_diff
 LOCAL_LOG_DIR=${RUNNER_WORKSPACE}/logs
 DOCKER_LOG_DIR=/met/logs
 
+LOCAL_REPO_DIR=${GITHUB_WORKSPACE}
+DOCKER_REPO_DIR=/met/MET
+
 # Create local directories to store output
 mkdir -p ${LOCAL_LOG_DIR}
 mkdir -p ${LOCAL_DIFF_DIR}
 
+# Define METplus branch to use for diff testing script
+METPLUS_BRANCH=develop
+
 # mount output and log dirs, mount GitHub files into MET_REPO_DIR
-mount_args="-v ${LOCAL_OUTPUT_DIR}:${DOCKER_OUTPUT_DIR} -v ${LOCAL_DIFF_DIR}:${DOCKER_DIFF_DIR} -v ${LOCAL_LOG_DIR}:${DOCKER_LOG_DIR}"
+mount_args="-v ${LOCAL_OUTPUT_DIR}:${DOCKER_OUTPUT_DIR} -v ${LOCAL_DIFF_DIR}:${DOCKER_DIFF_DIR} -v ${LOCAL_LOG_DIR}:${DOCKER_LOG_DIR} -v ${LOCAL_REPO_DIR}:${DOCKER_REPO_DIR}"
 
 # Set up data volumes
 volumes_from="--volumes-from met_truth"
 
+# set MET_REPO_DIR env var in Docker to mounted directory, and pass METPLUS_BRANCH
+env_arg="-e MET_REPO_DIR=${DOCKER_REPO_DIR} -e METPLUS_BRANCH"
+
 # run unit test script inside Docker, mount MET output and truth data
-# set MET_REPO_DIR env var in Docker to mounted directory
-cmd="\${MET_REPO_DIR}/.github/jobs/run_diff_tests.sh"
-time_command docker run ${volumes_from} ${mount_args} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
+cmd="\${DOCKER_REPO_DIR}/.github/jobs/run_diff_tests.sh"
+time_command docker run ${volumes_from} ${mount_args} ${env_arg} ${DOCKERHUB_TAG} bash -c \"${cmd}\"
 if [ $? != 0 ]; then
   exit 1
 fi
--- .github/jobs/run_diff_tests.sh
+++ .github/jobs/run_diff_tests.sh
@@ -13,7 +13,7 @@ source ${MET_REPO_DIR}/.github/jobs/test_env_vars.sh
 ###
 
 echo "Cloning METplus"
-time_command git clone --single-branch https://github.com/dtcenter/METplus ${METPLUS_DIR}
+time_command git clone --single-branch --branch ${METPLUS_BRANCH} https://github.com/dtcenter/METplus ${METPLUS_DIR}
 
 echo "Running comparison on test output"
 CMD_LOGFILE=/met/logs/comp_dir.log
