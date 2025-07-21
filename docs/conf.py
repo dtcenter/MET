aed1d9abe820e6c73337e8cf01e3aadb6e38385a
John Halley Gotway <johnhg@ucar.edu>
2025-07-21 17:09:10 +0000

    Per #3204, update to version number 12.0.3.

--- docs/conf.py
+++ docs/conf.py
@@ -20,11 +20,11 @@ print(sys.path)
 project = 'MET'
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Jensen, T., J. Prestopnik, H. Soh, L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz'
-version = '12.0.2'
+version = '12.0.3'
 verinfo = version
 release = f'{version}'
 release_year = '2025'
-release_date = f'{release_year}-02-14'
+release_date = f'{release_year}-07-21'
 copyright = f'{release_year}, {author}'
 
 # -- General configuration ---------------------------------------------------
