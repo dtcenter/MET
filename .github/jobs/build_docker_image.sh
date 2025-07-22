271e9a55920400ad44312ca8c7638c304312c84b
John Halley Gotway <johnhg@ucar.edu>
2025-07-22 19:23:56 +0000

    Per #3191, update GitHub actions logic prior to the MET-12.1.0 release to use consistent indentation, switch to using , update scan_docker_image.sh to fail on critical CVEs, update the cve scanning function to print a summary, and update the versions of MET to be rebuilt on schedule.

--- .github/jobs/build_docker_image.sh
+++ .github/jobs/build_docker_image.sh
@@ -15,11 +15,11 @@ fi
 CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build.log
 
 time_command docker build -t ${DOCKERHUB_TAG} \
-    --build-arg SOURCE_BRANCH \
-    --build-arg MET_BASE_REPO \
-    --build-arg MET_BASE_TAG \
-    --build-arg MET_CONFIG_OPTS \
-    -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}
+  --build-arg SOURCE_BRANCH \
+  --build-arg MET_BASE_REPO \
+  --build-arg MET_BASE_TAG \
+  --build-arg MET_CONFIG_OPTS \
+  -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}
 if [ $? != 0 ]; then
   cat ${CMD_LOGFILE}
   exit 1
