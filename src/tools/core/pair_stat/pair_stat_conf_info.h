7cfae1be1b41c1a7e6154087564a3b5470787342
John Halley Gotway <johnhg@ucar.edu>
2025-09-25 13:53:24 -0600

    Feature #3186 pair_stat_censoring_conversion (#3242)
    * Proof-of-concept applying data censoring and conversion logic to the Pair-Stat tool. Note that this impacts the FCST and OBS values for MPR and IODA inputs. It notably does not modify climatology values, which may already be present and pre-computed in the input MPR lines.
    
    * Per #3186, update config file comments, docs, and revision history for this issue.
    
    * Per #3186, whitespace only.
    
    * Per #3186, reduce some SQ code smells.

--- src/tools/core/pair_stat/pair_stat_conf_info.h
+++ src/tools/core/pair_stat/pair_stat_conf_info.h
@@ -129,6 +129,7 @@ class PairStatVxOpt {
 
       VxPairDataPoint vx_pd;              // Matched pair data [n_mask]
       std::vector<StatHdrInfo> vx_hdr;    // Track header inputs [n_mask]
+      bool convert_censor_flag;           // Conversion and/or censoring requested
 
       Grid grid_climo;                    // Grid for climatology data
 
@@ -239,8 +240,13 @@ class PairStatVxOpt {
       bool is_keeper_obs_init_time  (const unixtime, const unixtime) const;
 
       // Add paired data
-      bool add_mpr_line(const STATLine &);
-      bool add_ioda_pair(const point_pair_t &);
+      bool add_mpr_line(STATLine);
+      bool add_ioda_pair(point_pair_t);
+
+      // Apply conversion and censoring logic
+      void apply_convert_censor(STATLine &) const;
+      void apply_convert_censor(point_pair_t &) const;
+      void apply_convert_censor(const VarInfo *, double &) const;
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -323,12 +329,12 @@ class PairStatConfInfo {
       bool get_vflag() const;
 
       // Add paired data
-      bool add_mpr_line(const STATLine &);
+      bool add_mpr_line(STATLine);
 };
 
 ////////////////////////////////////////////////////////////////////////
 
-inline int PairStatConfInfo::get_n_vx() const { return(n_vx); }
+inline int PairStatConfInfo::get_n_vx() const { return n_vx; }
 
 ////////////////////////////////////////////////////////////////////////
 
