d4816e111562edb58c462ca853c2416b3c8389c3
John Halley Gotway <johnhg@ucar.edu>
2025-09-26 06:17:06 -0600

    Feature #3243 v12.2.0-rc1 (#3244)
    * Per #3243, add 12.2.0 RC1 release notes
    
    * Per #3243 plan to cut the MET-12.2.0-rc1 tomorrow, 9/26/25.
    
    * Per #3243, needed to update Makefiles.
    
    ---------
    
    Co-authored-by: MET Tools Test Account <met_test@seneca.rap.ucar.edu>

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,12 @@ print(sys.path)
 
 project = 'MET'
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
-author_list = 'Prestopnik, J., H. Soh, L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz, T. Jensen'
-version = '12.1.0-rc1'
+author_list = 'Soh, H., L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz, T. Jensen, J. Prestopnik'
+version = '12.2.0-rc1'
 verinfo = version
 release = f'{version}'
 release_year = '2025'
-release_date = f'{release_year}-05-22'
+release_date = f'{release_year}-09-26'
 copyright = f'{release_year}, {author}'
 
 # -- General configuration ---------------------------------------------------
