1fbb9791deedbe013bf50a7ad85aea150e01b388
John Halley Gotway <johnhg@ucar.edu>
2025-10-27 22:17:30 +0000

    Per #3270, fix pair_data_ensemble.cc logic.

--- src/libcode/vx_statistics/pair_data_ensemble.cc
+++ src/libcode/vx_statistics/pair_data_ensemble.cc
@@ -1444,7 +1444,7 @@ void VxPairDataEnsemble::add_ens(int member, bool mn, const Grid &gr) {
 
                // MET #3174 Apply lapse rate surface temperature correction
                if(sfc_info.lapse_rate_correction_apply_to != FieldType::None &&
-                  (msg_typ_lapsert.n() == 0 ||
+                  (msg_typ_lapsert.all_empty() ||
                    msg_typ_lapsert.reg_exp_match(it->typ_sa[i_obs].c_str())) &&
                   !correct_lapse_rate(topo_elv, it->elv_na[i_obs], fcst_v, obs_v)) {
 
@@ -1455,7 +1455,7 @@ void VxPairDataEnsemble::add_ens(int member, bool mn, const Grid &gr) {
 
                // MET #3174 Apply MSL/AGL height conversion
                if(sfc_info.msl_agl_conversion_apply_to != FieldType::None &&
-                  (msg_typ_mslagl.n() == 0 ||
+                  (msg_typ_mslagl.all_empty() ||
                    msg_typ_mslagl.reg_exp_match(it->typ_sa[i_obs].c_str())) &&
                   !convert_msl_agl(topo_elv, it->elv_na[i_obs], fcst_v, obs_v, member == 0)) {
 
