1c6798ec096387212563fe1d6f61fa8a8b93b2c2
John Halley Gotway <johnhg@ucar.edu>
2026-02-27 09:44:21 -0700

    Feature #3347 v12.1.2 (#3351)
    * Per #3347, update MET's Docker build environment and script to use the MET_PYTHON environment settings defined in METbaseimage version 3.4.9.
    
    * Per #3347, update to version 12.1.2 and add release notes.
    
    * Per #3347, tweak wording
    
    * Per #3347, MET_PYTHON is not actually required by build_met_docker.sh

--- docs/conf.py
+++ docs/conf.py
@@ -20,11 +20,11 @@ print(sys.path)
 project = 'MET'
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Prestopnik, J., H. Soh, L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz, T. Jensen'
-version = '12.1.1'
+version = '12.1.2'
 verinfo = version
 release = f'{version}'
-release_year = '2025'
-release_date = f'{release_year}-08-27'
+release_year = '2026'
+release_date = f'{release_year}-02-27'
 copyright = f'{release_year}, {author}'
 
 # -- General configuration ---------------------------------------------------
