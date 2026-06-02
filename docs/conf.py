e59a8e42d930ecc392a8ce86833b179e54b667ad
Lindsay Blank <lindsay.blank@fiqas-vps1.gsd.esrl.noaa.gov>
2026-06-02 21:24:41 +0000

    Added name to  User's Guide author list

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ print(sys.path)
 
 project = 'MET'
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
-author_list = 'Soh, H., L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz, T. Jensen, J. Prestopnik'
+author_list = 'Soh, H., L. Goodrich, B. Brown, R. Bullock, J. Halley Gotway, K. Newman, J. Opatz, T. Jensen, J. Prestopnik, L. Blank'
 version = '12.2.1'
 verinfo = version
 release = f'{version}'
