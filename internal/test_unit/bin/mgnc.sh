a3cab967c1f1306196d7d0947074b69e670dfc30
Howard Soh <hsoh@seneca.rap.ucar.edu>
2026-06-25 21:22:37 +0000

    #3395 Changed MET_PYTHON_BIN_EXE to MET_TEST_MET_PYTHON_EXE

--- internal/test_unit/bin/mgnc.sh
+++ internal/test_unit/bin/mgnc.sh
@@ -2,10 +2,10 @@
 
 # check for environment variables, use defaults if necessary
 export MET_TEST_BASE=${MET_TEST_BASE}
-[[ -z $MET_PYTHON_BIN_EXE ]] && MET_PYTHON_BIN_EXE=python3
+[[ -z $MET_TEST_MET_PYTHON_EXE ]] && MET_TEST_MET_PYTHON_EXE=python3
 MGNC=$MET_TEST_BASE/python/mgnc.py
 
 # get environment settings
 . ${MET_TEST_BASE}/bin/set_env.sh
 
-$MET_PYTHON_BIN_EXE $MGNC $@ 2>&1
+$MET_TEST_MET_PYTHON_EXE $MGNC $@ 2>&1
--- internal/test_unit/bin/mpnc.sh
+++ internal/test_unit/bin/mpnc.sh
@@ -2,10 +2,10 @@
 
 # check for environment variables, use defaults if necessary
 export MET_TEST_BASE=${MET_TEST_BASE}
-[[ -z $MET_PYTHON_BIN_EXE ]] && MET_PYTHON_BIN_EXE=python3
+[[ -z $MET_TEST_MET_PYTHON_EXE ]] && MET_TEST_MET_PYTHON_EXE=python3
 MPNC=$MET_TEST_BASE/python/mpnc.py
 
 # get environment settings
 . ${MET_TEST_BASE}/bin/set_env.sh
 
-$MET_PYTHON_BIN_EXE $MPNC $@ 2>&1
+$MET_TEST_MET_PYTHON_EXE $MPNC $@ 2>&1
